b6fcfdfd470f4aad49c0728f953403528893ac03
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-04 20:07:27 +0800

    Update fractalsandart.py

--- Python_BMP/fractalsandart.py
+++ Python_BMP/fractalsandart.py
@@ -1,4 +1,4 @@
-from Python_BMP.BITMAPlib import getmaxcolors,plotxybit,sortrecpoints,bmpcolorbits,colormix,random,iif,isinBMPrectbnd,isinrectbnd,radians,cos,sin,range2baseanddelta,setmax,distance
+from BITMAPlib import getmaxcolors,plotxybit,sortrecpoints,bmpcolorbits,colormix,random,iif,isinBMPrectbnd,isinrectbnd,radians,cos,sin,range2baseanddelta,setmax,distance
 
 def getIFSparams(): return {'fern':(((0,0,0,.16,0,0),(.2,-.26,.23,.22,0,1.6),(-.15,.28,.26,.24,0,.44),(.85,.04,-.04,.85,0,1.6)),(.009,.073,.137,1)),'tree':(((0,.2,0,.5,0,0),(.1,0,0,.1,0,.2),(.42,-.42,.42,.42,0,.2),(.42,.42,-.42,.42,0,.2)),(.05,.2,.6,1)),'cantortree':(((1/3,0,0,1/3,0,0),(1/3,0,0,1/3,1,0),(2/3,0,0,2/3,0.5,0.5),(0,0,0,0,0,0)),(1/3,2/3,1,1)),'sierpinskitriamgle':(((.5,0,0,.5,0,0),(.5,0,0,.5,1,0),(.5,0,0,.5,.5,.5),(0,0,0,0,0,0)),(1/3,2/3,1,1))}
 
