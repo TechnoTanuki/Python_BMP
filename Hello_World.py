cc1a26ca89719ac81514d757394dd6572650bab9
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-07 16:47:33 +0800

    Add files via upload

---
+++ Hello_World.py
@@ -0,0 +1,20 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,getcwd,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        mx,my=500,80 #bitmap size
+        bmp=b.newBMP(mx,my,24)
+        b.plotstring(bmp,50,10,'Hello World!!',4,1,0,b.getcolorname2RGBdict()['lightgray'],b.font8x14)
+        file='HelloWorld.bmp' #file name
+        b.saveBMP(file,bmp)
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
