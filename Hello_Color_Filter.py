eee03ac4aa0ba25f9c12c9f4204c807dc193016b
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-08 15:11:15 +0800

    Add files via upload
    How to do color filters

---
+++ Hello_Color_Filter.py
@@ -0,0 +1,20 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') #load to memory
+        rf,gf,bf=1,0.5,0 #RGB factors 0 to 1 float
+        b.colorfilterto24bitimage(bmp,[rf,gf,bf]) 
+        file='HelloColorFilter.bmp' #file name
+        b.saveBMP(file,bmp)
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
