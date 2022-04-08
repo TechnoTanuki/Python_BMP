b7c303d71ee1cc1ace22cdea9adbd7f2cce5a817
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-08 21:01:54 +0800

    Add files via upload

---
+++ Hello_Lines.py
@@ -0,0 +1,31 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        mx,my=75,75 #bitmap size
+        bmp=b.newBMP(mx,my,4) # 16 color
+        mx,my=mx-1,my-1 #max-1 for screen
+        cx,cy=mx>>1,my>>1 #div by 2
+        b.line(bmp,0,0,mx,my,15)
+        b.line(bmp,mx,0,0,my,14)
+        b.line(bmp,0,0,mx,0,13)
+        b.line(bmp,0,0,0,my,11)
+        b.line(bmp,mx,my,mx,0,10)
+        b.line(bmp,mx,my,0,my,9)
+        b.line(bmp,cx,cy,cx,0,8)
+        b.line(bmp,cx,cy,mx,cy,7)
+        b.line(bmp,cx,cy,cx,my,6)
+        b.line(bmp,cx,cy,0,cy,5)
+        file='HelloLines.bmp' #file name
+        b.saveBMP(file,bmp)
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
