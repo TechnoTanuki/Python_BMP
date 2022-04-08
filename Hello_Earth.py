1ad1098be9549c7e6eb2475eea7aed5006b03bbd
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-08 11:25:45 +0800

    Update Hello_Earth.py

--- Hello_Earth.py
+++ Hello_Earth.py
@@ -7,8 +7,9 @@ from os import path,sys
 def main():
         rootdir=path.abspath(sys.path[0])
         bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
-        b.plotstring(bmp,5,25,'Hello',4,0,0,b.getcolorname2RGBdict()['brightred'],b.font8x14)
-        b.plotstring(bmp,5,85,'World',4,0,0,b.getcolorname2RGBdict()['brightyellow'],b.font8x8)
+        c=b.getcolorname2RGBdict() #friendly color names 2 rgb
+        b.plotstring(bmp,5,25,'Hello',4,0,0,c['brightred'],b.font8x14)
+        b.plotstring(bmp,5,85,'World',4,0,0,c['brightyellow'],b.font8x8)
         file='HelloEarth.bmp' #file name
         b.saveBMP(file,bmp)
         print('\nAll done close mspaint to finish')
