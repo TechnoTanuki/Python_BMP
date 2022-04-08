90edea20894b9d40bb22173525151a4667afe36a
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-08 13:39:41 +0800

    Update Hello_Mirror_Bottom.py

--- Hello_Mirror_Bottom.py
+++ Hello_Mirror_Bottom.py
@@ -6,7 +6,7 @@ from os import path,sys
         
 def main():
         rootdir=path.abspath(sys.path[0])
-        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') #load earth to memory
+        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') #load to memory
         b.mirrorbottom(bmp)
         file='HelloMirrorBottom.bmp' #file name
         b.saveBMP(file,bmp)
