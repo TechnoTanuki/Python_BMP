f5bb5279a660a9d663a0cba86905258159d86519
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-08 14:55:01 +0800

    Update Hello_Darkness.py

--- Hello_Darkness.py
+++ Hello_Darkness.py
@@ -9,6 +9,7 @@ def main():
         mx,my=500,80 #bitmap size
         bmp=b.newBMP(mx,my,24)
         file='HelloDarkness.bmp' #file name
+        #insert something here
         b.saveBMP(file,bmp)
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
