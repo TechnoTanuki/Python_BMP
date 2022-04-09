46981b1175ba56a64579c72bcdca97ed98b9cea8
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-09 15:16:52 +0800

    Add files via upload

---
+++ Hello_FilledRectangle.py
@@ -0,0 +1,21 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        mx,my=200,100 #bitmap size
+        bmp=b.newBMP(mx,my,4) # 16 color
+        j=19
+        b.filledrect(bmp,j,j,mx-j,my-j,12)
+        file='HelloFilledRectangle.bmp' #file name
+        b.saveBMP(file,bmp)
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
