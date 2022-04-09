9d3163d538ea5f89d9649ce1eddc77eca16bdbd6
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-09 18:09:18 +0800

    Add files via upload

---
+++ Hello_Thick_Round_Line.py
@@ -0,0 +1,22 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        mx,my=200,200 #bitmap size
+        bmp=b.newBMP(mx,my,4) # 16 color
+        mx,my=mx-1,my-1 #max-1 for screen
+        cx,cy=mx>>1,my>>1 #div by 2
+        b.thickroundline(bmp,[12,12],[mx-12,my-12],10,14)
+        file='HelloThickroundLine.bmp' #file name
+        b.saveBMP(file,bmp)
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
