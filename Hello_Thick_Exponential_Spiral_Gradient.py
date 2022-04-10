17858677a8093d83fa389220f2888bc88360cb97
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-11 06:36:15 +0800

    Update Hello_Thick_Exponential_Spiral_Gradient.py

--- Hello_Thick_Exponential_Spiral_Gradient.py
+++ Hello_Thick_Exponential_Spiral_Gradient.py
@@ -8,7 +8,7 @@ def main():
         rootdir=path.abspath(sys.path[0])
         mx,my=300,300 #bitmap size
         bmp=b.newBMP(mx,my,24)
-        b.gradvert(bmp,b.bsplinevert(b.spiralcontrolpointsvert(150,150,7,1.5,5),False,False),5,[255,0],[.9,.5,.9])
+        b.gradvert(bmp,b.bsplinevert(b.spiralcontrolpointsvert(150,150,7,1.618,5),False,False),5,[255,0],[.9,.5,.9])
         file='HelloThickExpGradSpiral.bmp' #file name
         b.saveBMP(file,bmp)
         print('\nAll done close mspaint to finish')
