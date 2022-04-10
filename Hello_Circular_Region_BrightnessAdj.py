2bfbd48c3bbad2d095c3a0031e30b71bec395f58
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-10 21:32:32 +0800

    Add files via upload

---
+++ Hello_Circular_Region_BrightnessAdj.py
@@ -0,0 +1,21 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
+        cx,cy= b.getmaxx(bmp)<<1, b.getmaxy(bmp)<<1# center 
+        b.brightnessadjcircregion(bmp,85,85,60,-50) #last param is brightness can be + or -
+        b.brightnessadjcircregion(bmp,85,85,40,100) #last param is brightness can be + or -
+        file='HelloCircularRegionBrightnessAdj.bmp' #file name
+        b.saveBMP(file,bmp)
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
