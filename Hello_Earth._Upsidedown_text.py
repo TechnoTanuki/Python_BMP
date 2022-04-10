5c1dc8a17cb9fa0b84bb374e804f08cacc7ef634
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-10 10:55:50 +0800

    Add files via upload

---
+++ Hello_Earth._Upsidedown_text.py
@@ -0,0 +1,21 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
+        c=b.getcolorname2RGBdict() #friendly color names 2 rgb
+        b.plotstring(bmp,5,25,'Hello',4,1,0,c['brightred'],b.font8x14)
+        b.plotstringupsidedown(bmp,5,85,'World',4,0,0,c['brightyellow'],b.font8x8)
+        file='HelloEarthDownUpsidedowntext.bmp' #file name
+        b.saveBMP(file,bmp)
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
