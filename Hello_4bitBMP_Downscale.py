d0a8f26aae75de0cb970c13f29314b65079a4b52
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-10 14:59:54 +0800

    Add files via upload

---
+++ Hello_4bitBMP_Downscale.py
@@ -0,0 +1,19 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        file=rootdir+'/assets/somebody.bmp'
+        newfile='4bitBMP.bmp'
+        print(file)
+        b.reduce24bitimagebits(file,newfile,4,24,False,[0.25,.5,1])
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+newfile) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
