b1dd39401963857cbfbc3e44af5a1cc1992e10d4
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-11 20:33:29 +0800

    Add files via upload

---
+++ Hello_Save_Selection.py
@@ -0,0 +1,20 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
+        buff=b.copyrect(bmp,5,5,63,63)
+        nbmp=b.convertselection2BMP(buff)
+        file='HelloSaveSelection.bmp' #file name
+        b.saveBMP(file,nbmp)
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
