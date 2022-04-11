b2474cc6defd9899026dc3d8ea1056ac90eaba00
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-11 20:10:46 +0800

    Add files via upload

---
+++ Hello_Vector.py
@@ -0,0 +1,20 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        mx,my=200,200 #bitmap size
+        bmp=b.newBMP(mx,my,1) # mono
+        b.drawvec(bmp,[5,5],[mx-5,my-5],30,1)
+        file='HelloVector.bmp' #file name
+        b.saveBMP(file,bmp)
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
