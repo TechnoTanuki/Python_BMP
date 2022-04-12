e46289f2878beae48296bb8d628981fb2d25094f
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-12 15:34:11 +0800

    Add files via upload

---
+++ Hello_Vertical_Brightness_Gradient.py
@@ -0,0 +1,19 @@
+#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
+#This graphics library outputs to a bitmap file
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0])
+        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp')
+        b.verticalbrightnessgradto24bitimage(bmp,[-50,100])
+        file='HelloVertBrightGrad.bmp' #file name
+        b.saveBMP(file,bmp)
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
