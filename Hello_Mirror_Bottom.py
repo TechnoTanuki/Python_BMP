2db4d9286cc5981d56531942064c76a64834541b
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-14 01:14:14 +0800

    Update Hello_Mirror_Bottom.py

--- Hello_Mirror_Bottom.py
+++ Hello_Mirror_Bottom.py
@@ -1,15 +1,17 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
+        rootdir=path.abspath(sys.path[0]) # get path of this script
         bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') #load to memory
         b.mirrorbottom(bmp)
         file='HelloMirrorBottom.bmp' #file name
-        b.saveBMP(file,bmp)
+        b.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
