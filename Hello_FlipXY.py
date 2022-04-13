995a789be98833eb82e72b534d063c41cd9613a9
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-14 01:32:05 +0800

    Update Hello_FlipXY.py

--- Hello_FlipXY.py
+++ Hello_FlipXY.py
@@ -1,15 +1,17 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') #load earth to memory
-        bmp=b.flipXY(bmp) #need to return XYflipped img
-        file='HelloFlipXY.bmp' #file name
-        b.saveBMP(file,bmp)
+        rootdir=path.abspath(sys.path[0]) # get path of this script
+        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') #load bitmap to byte array-> bmp
+        bmp=b.flipXY(bmp) # need to return new XY flipped image
+        file='HelloFlipXY.bmp' # file name
+        b.saveBMP(file,bmp) # save XY flipped image
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
