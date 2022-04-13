3fff14a5b0be1064ec74213a082857fd17a3b92d
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-13 22:29:07 +0800

    Update Hello_Lines.py

--- Hello_Lines.py
+++ Hello_Lines.py
@@ -1,15 +1,18 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        mx,my=75,75 #bitmap size
-        bmp=b.newBMP(mx,my,4) # 16 color
+        rootdir=path.abspath(sys.path[0]) # Get directory of this script
+        mx,my=75,75 # bitmap size
+        bmp=b.newBMP(mx,my,4) # 4 bit = 16 color
         mx,my=mx-1,my-1 #max-1 for screen
-        cx,cy=mx>>1,my>>1 #div by 2
+        (cx,cy)=b.centercoord(bmp) # How to get center of the bitmap
+        #Python_BMP.BITMAPlib.line(bmp bytearray,x1 int,y1 int,x2 int, y2 int,color int) all unsigned
         b.line(bmp,0,0,mx,my,15)
         b.line(bmp,mx,0,0,my,14)
         b.line(bmp,0,0,mx,0,13)
@@ -20,8 +23,8 @@ def main():
         b.line(bmp,cx,cy,mx,cy,7)
         b.line(bmp,cx,cy,cx,my,6)
         b.line(bmp,cx,cy,0,cy,5)
-        file='HelloLines.bmp' #file name
-        b.saveBMP(file,bmp)
+        file='HelloLines.bmp' # file name
+        b.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
