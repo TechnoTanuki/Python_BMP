350b6a14f3a0c65883e192f8c94c6ec31155b5c3
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-14 01:28:41 +0800

    Update Hello_Vertical_Brightness_Gradient.py

--- Hello_Vertical_Brightness_Gradient.py
+++ Hello_Vertical_Brightness_Gradient.py
@@ -1,15 +1,17 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp')
-        b.verticalbrightnessgradto24bitimage(bmp,[-50,100])
-        file='HelloVertBrightGrad.bmp' #file name
-        b.saveBMP(file,bmp)
+        rootdir=path.abspath(sys.path[0]) # get path of this script
+        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') # load a bitmap to byte array->bmp
+        b.verticalbrightnessgradto24bitimage(bmp,[-50,100]) # [-50,100] is range of brightness
+        file='HelloVertBrightGrad.bmp' # file name
+        b.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
