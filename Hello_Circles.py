3bf90c02abb2ff546c6cbd220c4c89ab5ba274f3
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-13 19:13:17 +0800

    Update Hello_Circles.py

--- Hello_Circles.py
+++ Hello_Circles.py
@@ -1,20 +1,21 @@
 #Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
 #This graphics library outputs to a bitmap file
+#-------------------------------------------------------------
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
         rootdir=path.abspath(sys.path[0]) # get path of running script
-        mx=512 #bitmap size
-        my=mx #square bitmap
+        mx=512 # bitmap size x
+        my=mx # bitmap size y = x square bitmap
         bmp=b.newBMP(mx,my,8) #8 bit = 256 color
-        (x,y)=b.centercoord(bmp) #How to get center of the bitmap
-        for r in range(0,x):
+        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
+        for r in range(0,x): # increase the radius of the circle from 0 to x
                 b.circle(bmp,x,y,r,r,False) 
-                # circle(bmp bytearray,x int ,y int, radius int, color int,isfilled bool)
-        file='HelloCircles.bmp' #file name
-        b.saveBMP(file,bmp) #save file 
+                # Python_BMP.circle(bmp bytearray,x int ,y int, radius int, color int,isfilled bool)
+        file='HelloCircles.bmp' # file name 
+        b.saveBMP(file,bmp) # save file 
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
