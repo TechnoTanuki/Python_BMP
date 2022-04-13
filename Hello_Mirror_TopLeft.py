f258d1f762d26f925f7b6a5ccbfbd20ea0adaaa1
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-14 01:17:40 +0800

    Update Hello_Mirror_TopLeft.py

--- Hello_Mirror_TopLeft.py
+++ Hello_Mirror_TopLeft.py
@@ -1,15 +1,17 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') #load to memory
+        rootdir=path.abspath(sys.path[0]) # get path of this script
+        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') # load to memory
         b.mirrortopleft(bmp)
         file='HelloMirrorTopLeft.bmp' #file name
-        b.saveBMP(file,bmp)
+        b.saveBMP(file,bmp) #save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
