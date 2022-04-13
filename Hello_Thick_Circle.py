e6a29c364fc512aa14c351593e6c25bcddb26c00
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-13 22:59:28 +0800

    Update Hello_Thick_Circle.py

--- Hello_Thick_Circle.py
+++ Hello_Thick_Circle.py
@@ -1,20 +1,24 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        mx=256 #bitmap size
-        my=mx
-        bmp=b.newBMP(mx,my,4) #16 color
-        mx,my=mx-1,my-1 #max-1 for screen
-        cx,cy=mx>>1,my>>1 #div by 2
-        r=cx-12
-        b.thickcircle(bmp,cx,cy,r,12,14)
+        rootdir=path.abspath(sys.path[0]) # get path of running script
+        mx=128 # bitmap size x
+        my=mx # bitmap size y = x square bitmap        
+        bmp=b.newBMP(mx,my,4) # 4 bit = 16 color
+        (x,y)=b.centercoord(bmp) # How to get center of the bitmap        
+        r=x-12  # set radius = x - 12
+        color=14
+        penradius=12
+        b.thickcircle(bmp,x,y,r,penradius,color) # all unsigned
+        #Python_BMP.BITMAPlib.thickcircle(bmp bytyearray,x int,y int ,r int,penradius int,color int)
         file='HelloThickCirle.bmp' #file name
-        b.saveBMP(file,bmp)
+        b.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
