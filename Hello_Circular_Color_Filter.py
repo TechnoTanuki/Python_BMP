f9729c2ed8def63adbdac162cf9ef3acca5f287e
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-13 19:46:22 +0800

    Update Hello_Circular_Color_Filter.py

--- Hello_Circular_Color_Filter.py
+++ Hello_Circular_Color_Filter.py
@@ -1,17 +1,22 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
+        rootdir=path.abspath(sys.path[0])  # get path of running script
         bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
-        cx,cy= b.getmaxx(bmp)<<1, b.getmaxy(bmp)<<1# center 
-        rf,gf,bf=1,.7,.3 #RGB factors 0 to 1 float
-        b.colorfiltercircregion(bmp,85,85,60,[rf,gf,bf])
+        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
+        r=x-20 # radius set to x - 20
+        rf,gf,bf=1,.7,.3 # RGB color factors (0 to 1) float
+        cf=(rf,gf,bf) # RGB color factor 0..1
+        b.colorfiltercircregion(bmp,x,y,r,cf)
+        # Python_BMP.colorfiltercircregion(bmp bytearray,x int ,y int ,r int,cf tuple of RGB 0..1 )
         file='HelloCircularColorFilter.bmp' #file name
-        b.saveBMP(file,bmp)
+        b.saveBMP(file,bmp) #save to file 
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
