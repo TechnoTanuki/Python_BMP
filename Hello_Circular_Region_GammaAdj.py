c87fe10b05a4e3056897b379a612c0ab4215d233
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-13 21:47:11 +0800

    Update Hello_Circular_Region_GammaAdj.py

--- Hello_Circular_Region_GammaAdj.py
+++ Hello_Circular_Region_GammaAdj.py
@@ -1,16 +1,21 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
-        cx,cy= b.getmaxx(bmp)<<1, b.getmaxy(bmp)<<1# center 
-        b.gammacorrectcircregion(bmp,85,85,60,.1)
+        rootdir=path.abspath(sys.path[0]) # Get script directory
+        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') # load earth to memory
+        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
+        r=60 # radius
+        gc=.1 # gamma correction
+        b.gammacorrectcircregion(bmp,x,y,r,gc)
+        #Python_BMP.BITMAPlib.gammacorrectcircregion(bmp bytearray,x int,y int,r int,gc signed float)
         file='HelloCircularRegionGammaAdj.bmp' #file name
-        b.saveBMP(file,bmp)
+        b.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
