a4fad04e58242bb3f07080eb88f929a937983349
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-13 20:13:09 +0800

    Update Hello_Circular_Pixellate.py

--- Hello_Circular_Pixellate.py
+++ Hello_Circular_Pixellate.py
@@ -1,15 +1,21 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
+        rootdir=path.abspath(sys.path[0])  # get path of running script
         bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
-        b.pixelizenxncircregion(bmp,85,85,50,5)
+        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
+        r=x-30 # radius set to x - 30
+        n=5 # blursize n = 5x5 pix
+        b.pixelizenxncircregion(bmp,x,y,r,n)
+        # Python_BMP.pixelizenxncircregion(bmp bytarray,x int ,y int ,r int ,n int)
         file='HelloCircularPixellate.bmp' #file name
-        b.saveBMP(file,bmp)
+        b.saveBMP(file,bmp) # save the image
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
