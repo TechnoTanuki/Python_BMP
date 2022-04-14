c2984b22c6a2576cd079476f36af5396aeefd1c4
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-14 23:34:52 +0800

    Update Hello_GammaAdj.py

--- Hello_GammaAdj.py
+++ Hello_GammaAdj.py
@@ -1,15 +1,17 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') #load to memory
-        b.gammaadjto24bitimage(bmp,.5)
-        file='HelloGammaAdj.bmp' #file name
-        b.saveBMP(file,bmp)
+        rootdir=path.abspath(sys.path[0]) # get path of this script
+        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') # load to memory
+        b.gammaadjto24bitimage(bmp,.5)  # 2nd param for gamma adj unsigned float
+        file='HelloGammaAdj.bmp' # file name
+        b.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
