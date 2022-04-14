bcea9094abfd8f8f980d7e718e9223f020dd0082
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-14 08:04:48 +0800

    Update Hello_Earth_Reverse_text.py

--- Hello_Earth_Reverse_text.py
+++ Hello_Earth_Reverse_text.py
@@ -1,17 +1,22 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
+        rootdir=path.abspath(sys.path[0]) # get path of thisscripy
+        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') # load earth to memory 
         c=b.getcolorname2RGBdict() #friendly color names 2 rgb
-        b.plotreversestring(bmp,5,25,'Hello',4,0,0,c['brightred'],b.font8x14)
-        b.plotreversestring(bmp,5,85,'World',4,0,0,c['brightyellow'],b.font8x8)
+        fontsize=4 # font size (unsigned int)
+        pixspace=1 # space between bitmap font pixels (0 = default) (unsigned int)
+        charspace=0 # space bitmap font characters (0 = default) (unsigned int)
+        b.plotreversestring(bmp,5,25,'Hello',fontsize,pixspace,charspace,c['brightred'],b.font8x14)
+        b.plotreversestring(bmp,5,85,'World',fontsize,pixspace,charspace,c['brightyellow'],b.font8x8)
         file='HelloEarthReverse.bmp' #file name
-        b.saveBMP(file,bmp)
+        b.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
