82c53cbfdfa097a9effe7b0bdf862fd16e48ac8f
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-14 13:10:45 +0800

    Update Hello_Ellipse.py

--- Hello_Ellipse.py
+++ Hello_Ellipse.py
@@ -1,24 +1,23 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
+import Python_BMP.BITMAPlib as bm,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        mx=512 #bitmap size
-        my=256
-        bmp=b.newBMP(mx,my,4) # 16 color
-        mx,my=mx-1,my-1 #max-1 for screen
-        cx,cy=mx>>1,my>>1 #div by 2
-        r=cx-12
-        b.ellipse(bmp,cx,cy,cy,cx,12)
+        rootdir=path.abspath(sys.path[0]) # get pathof this script
+        bmp=bm.newBMP(150,85,4) # 16 color 80 x 50 bmp
+        (x,y)=bm.centercoord(bmp) # How to get center of the bitmap
+        b=y # b axis = y
+        a=x # a axis = x
+        color=13
+        bm.ellipse(bmp,x,y,b,a,color) # all unsigned ints
         file='HelloEllipse.bmp' #file name
-        b.saveBMP(file,bmp)
+        bm.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
 if __name__=="__main__": 
         main()
-
-
