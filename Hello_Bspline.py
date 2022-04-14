073104be1f5ce923947ac58e1aabfd247d782e11
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-14 08:59:05 +0800

    Update Hello_Bspline.py

--- Hello_Bspline.py
+++ Hello_Bspline.py
@@ -1,16 +1,27 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        mx,my=250,250 #bitmap size
-        bmp=b.newBMP(mx,my,4)
-        file='HelloBspline.bmp' #file name
-        b.bspline(bmp,b.spiralcontrolpointsvert(124,124,5,1,13),0,14,True,True)
-        b.saveBMP(file,bmp)
+        rootdir=path.abspath(sys.path[0]) # get path of this script
+        mx,my=250,250 # bitmap size
+        bmp=b.newBMP(mx,my,4) # 4 bit = 2^4 = 16 bits
+        file='HelloBspline.bmp' # file name
+        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
+        step=5 # pixel interval between spiral turn
+        growthfactor=1 # greater than 1 means exponential spiral
+        turns=13 # number of turns of spiral
+        vertlist = b.spiralcontrolpointsvert(x,y,step,growthfactor,turns) # list of (x,y) control points
+        penradius=2 # radius of pen
+        color=11 
+        isclosed=True # closed curve
+        curveback=True # do extra computation to curve back to origin
+        b.bspline(bmp,vertlist,penradius,color,isclosed,curveback)
+        b.saveBMP(file,bmp) # save bitmap
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
