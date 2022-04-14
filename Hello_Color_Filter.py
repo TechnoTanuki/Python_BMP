3573f3d559f81007a1cd26bd2ae0d2240c093030
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-14 23:45:49 +0800

    Update Hello_Color_Filter.py

--- Hello_Color_Filter.py
+++ Hello_Color_Filter.py
@@ -1,16 +1,18 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
+        rootdir=path.abspath(sys.path[0]) # get path of this script
         bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') #load to memory
-        rf,gf,bf=1,0.5,0 #RGB factors 0 to 1 float
+        rf,gf,bf=1,0.5,0 #RGB factors 0 to 1 float unsigned
         b.colorfilterto24bitimage(bmp,[rf,gf,bf]) 
         file='HelloColorFilter.bmp' #file name
-        b.saveBMP(file,bmp)
+        b.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
