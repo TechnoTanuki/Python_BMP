8d374ff079bb6ec2b46446a02e0d039f9ffe6d87
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-14 23:41:17 +0800

    Update Hello_Monochrome.py

--- Hello_Monochrome.py
+++ Hello_Monochrome.py
@@ -1,19 +1,19 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
+        rootdir=path.abspath(sys.path[0]) # get path of this script
         bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
         b.monofilterto24bitimage(bmp)
-        file='HelloMonoEarth.bmp' #file name
-        b.saveBMP(file,bmp)
+        file='HelloMonoEarth.bmp' # file name
+        b.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
 if __name__=="__main__": 
         main()
-
-
