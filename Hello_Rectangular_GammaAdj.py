2684be28355fc5983bcdfa5848b5272a5e423d18
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-15 17:46:06 +0800

    Update Hello_Rectangular_GammaAdj.py

--- Hello_Rectangular_GammaAdj.py
+++ Hello_Rectangular_GammaAdj.py
@@ -1,17 +1,19 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
-        cx,cy= b.getmaxx(bmp)<<1, b.getmaxy(bmp)<<1# center 
+        rootdir=path.abspath(sys.path[0]) # get path of this script
+        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') # load earth to memory
+        #gammaadjto24bitregion(bmp,x1,y1,x2,y2,gammadj <ufloat>)
         b.gammaadjto24bitregion(bmp,30,30,138,138,4)
         b.gammaadjto24bitregion(bmp,50,50,118,118,.1)
         file='HelloRectangularGammaAdj.bmp' #file name
-        b.saveBMP(file,bmp)
+        b.saveBMP(file,bmp) # bmp<byte array in bitmap format> --> file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
