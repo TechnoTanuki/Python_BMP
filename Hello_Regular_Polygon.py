e4ac877709961e14b2342344bfd06ee85a61e0b9
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-16 12:17:55 +0800

    Update Hello_Regular_Polygon.py

--- Hello_Regular_Polygon.py
+++ Hello_Regular_Polygon.py
@@ -1,16 +1,24 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        mx,my=160,160 #bitmap size
-        bmp=b.newBMP(mx,my,4)
-        b.plotpoly(bmp,b.regpolygonvert(80,80,60,6,0),13)
-        file='HelloRegularPolygon.bmp' #file name
-        b.saveBMP(file,bmp)
+        rootdir=path.abspath(sys.path[0]) # get current script path
+        mx=my=200 # bitmap size
+        bmp=b.newBMP(mx,my,4) # 2^4=16 color bitmap black background
+        x=y=100 # centerpoint
+        r=x-20 # radius of a circle that contains all the vertices 
+        sides=5 # for a pentagon
+        angle=30 # for rotation in degrees
+        polygonvertexlist=b.regpolygonvert(x,y,r,sides,angle) # generate vertices
+        color=11 # color in 4 bit mode (min 0 - max 15)
+        b.plotpoly(bmp,polygonvertexlist,color) # plot the polygon
+        file='HelloRegularPolygon.bmp' # file name
+        b.saveBMP(file,bmp) # save the bitmap
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
