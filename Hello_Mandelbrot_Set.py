a1d5ac0084cc6e24a39a7860274ed5e8108d6c31
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-16 12:00:31 +0800

    Update Hello_Mandelbrot_Set.py

--- Hello_Mandelbrot_Set.py
+++ Hello_Mandelbrot_Set.py
@@ -1,23 +1,24 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.c
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        mx=500
-        my=500
-        bmp=b.newBMP(mx,my,24)
+        rootdir=path.abspath(sys.path[0]) # get current script path
+        mx=my=500 # square canvas
+        bmp=b.newBMP(mx,my,24) # RGB bitmap 500 x 500
         cf=b.getRGBfactors() #color info
-        mandelpar=b.mandelparamdict()#seems preferable to store common params
-        b.mandelbrot(bmp,0,0,500,500,mandelpar['maxeqdim'],cf['brightgreen'],255)
-        file='fern.bmp'
-        b.saveBMP(file,bmp)
+        mandelpar=b.mandelparamdict() # common parameters 
+        #mandelbrot(bmp,x1,y1,x2,y2,mandelpar[],RGBfactors,maxiter)
+        b.mandelbrot(bmp,0,0,mx,my,mandelpar['maxeqdim'],cf['brightgreen'],255)
+        file='mandelbrot.bmp'
+        b.saveBMP(file,bmp) # save file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
 if __name__=="__main__": 
         main()
-
 
