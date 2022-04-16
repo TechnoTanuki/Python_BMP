6f3d6b59e067935f8bdafd30ea3b3a4ab246edb4
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-16 16:26:42 +0800

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,14 +1,19 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        mx,my=500,80 #bitmap size
-        bmp=b.newBMP(mx,my,24)
-        b.plotstring(bmp,50,10,'Hello World!!',4,1,0,b.getcolorname2RGBdict()['lightgray'],b.font8x14)
+        rootdir=path.abspath(sys.path[0]) # get path of this script
+        mx,my=500,80 # bitmap size
+        bmp=b.newBMP(mx,my,24) # RGB bitmap
+        fontsize=4 # font size multiplier
+        pixspace=1 # space between pixels
+        charspace=0 # default space between char
+        b.plotstring(bmp,50,10,'Hello World!!',fontsize,pixspace,charspace,b.getcolorname2RGBdict()['yellow'],b.font8x14)
         file='HelloWorld.bmp' #file name
         b.saveBMP(file,bmp)
         print('\nAll done close mspaint to finish')
@@ -16,5 +21,3 @@ def main():
 
 if __name__=="__main__": 
         main()
-
-
