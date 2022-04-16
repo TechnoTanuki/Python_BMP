8c4c250d9bdc167d53379ec4883ac21ffb32a052
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-16 19:55:28 +0800

    Add files via upload

---
+++ Hello_Mirror_Top.py
@@ -0,0 +1,21 @@
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0]) # get path of this script
+        bmp=b.loadBMP(rootdir+'/assets/tanuki.bmp') # load to memory
+        b.mirrortop(bmp)
+        file='HelloMirrorTop.bmp' # file name
+        b.saveBMP(file,bmp) # save file
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
