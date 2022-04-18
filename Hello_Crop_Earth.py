33c834dd9679d5962501c7a7eae204d0f373b0ae
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-19 05:15:05 +0800

    Update Hello_Crop_Earth.py

--- Hello_Crop_Earth.py
+++ Hello_Crop_Earth.py
@@ -9,7 +9,7 @@ from os import path,sys
 def main():
         rootdir=path.abspath(sys.path[0]) # get script path
         existingfile=rootdir+'/assets/earth.bmp'
-        newfile='HelloEarthCopyPaste.bmp' #file name
+        newfile='HelloCropEarth.bmp' #file name
         b.cropBMPandsave(existingfile,newfile,5,5,60,60) # crop a rectangular region (x1,y1,x2,y2) to a file
         print('\nAll done close mspaint to finish')
         ret =proc.call('mspaint '+newfile) # replace with another editor if Unix
