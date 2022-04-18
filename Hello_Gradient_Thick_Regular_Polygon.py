e76c44b1c32cba8b905a0cecdbff46c008005d1c
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-18 20:24:21 +0800

    Add files via upload

---
+++ Hello_Gradient_Thick_Regular_Polygon.py
@@ -0,0 +1,31 @@
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
+
+import Python_BMP.BITMAPlib as b,subprocess as proc
+from os import path,sys
+        
+def main():
+        rootdir=path.abspath(sys.path[0]) # get current script path
+        mx=my=200 # bitmap size
+        bmp=b.newBMP(mx,my,24) # RGB bitmap black background
+        x=y=100 # centerpoint
+        r=x-20 # radius of a circle that contains all the vertices 
+        sides=5 # for a pentagon
+        angle=30 # for rotation in degrees
+        polygonvertexlist=b.regpolygonvert(x,y,r,sides,angle) # generate vertices
+        color=11 # color in 4 bit mode (min 0 - max 15)
+        penradius=5 # radius of pen
+        b.gradthickplotpoly(bmp,polygonvertexlist,penradius,color)  # plot the polygon
+        file='HelloThickRegularPolygon.bmp' # file name
+        b.saveBMP(file,bmp) # save the bitmap
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
+
+
+
+
