ec2ca1162e4d5a232cd0e7b950aac70dbe431233
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-19 10:02:48 +0800

    Update Hello_XYScatterplot.py

--- Hello_XYScatterplot.py
+++ Hello_XYScatterplot.py
@@ -11,19 +11,19 @@ def main():
         x=y=600 # bitmap size
         bmp=b.newBMP(x,y,4) # 16 color bitmap
         file='HelloXYScatterplot.bmp' # random file name
-        XYdata=[[20,80,5,15,True], # [[x,y,radius,color,filled],[]...]
+        XYdata=[[20,80,5,15,True], #[[x,y,radius,color,filled]
         [40,110,5,15,True],
         [50,10,5,15,True]]
         origin=(80,570) # uint x,y tuple origin point of the graph (bottom left)
-        xylimits=(570,18) # uint x,y tuple end point of graph (top right)
+        xylimits=(570,18) # uint x,y tuple end poimt of graph (top right)
         steps=(40,40)  # uint x,y tuple x and y step for increment in screen coords
         xyvalstarts=(0,0) # int x,y tuple for starting values in graph coords
         xysteps=(10,10) # int x,y tuple for increment values in graph coords  
-        axiscolor=14 # color of axis
+        color=14 # color of axis
         gridcolor=5 # color of grid
         showgrid=True # bool value to toggle grid visibility
-        color= 15 # color of text
-        XYcoordinfo=b.XYaxis(bmp,origin,steps,xylimits,xyvalstarts,xysteps,axiscolor,color,showgrid,gridcolor)
+        textcolor= 15 # color of text
+        XYcoordinfo=b.XYaxis(bmp,origin,steps,xylimits,xyvalstarts,xysteps,color,textcolor,showgrid,gridcolor)
         showLinearRegLine=True # toggle computation and visibility of linear reg line
         reglinecolor=8 # color of the linear regression line
         b.XYscatterplot(bmp,XYdata,XYcoordinfo,showLinearRegLine,reglinecolor) # do coordinate transform and plot
@@ -33,6 +33,8 @@ def main():
 
 if __name__=="__main__": 
         main()
+
+
 
 
 
