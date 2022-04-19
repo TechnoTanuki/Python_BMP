69f9efeee8bbd45652e095540c9909e8afafd7a3
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-19 20:42:00 +0800

    Update Hello_Fern.py

--- Hello_Fern.py
+++ Hello_Fern.py
@@ -1,16 +1,19 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.c
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
-        mx=250
-        my=500
-        bmp=b.newBMP(mx,my,24)
-        c=b.getcolorname2RGBdict() #color info
-        b.IFS(bmp,b.getIFSparams()['fern'],0,0,250,500,40,55,125,80,c['lightgreen'],80000)
+        rootdir=path.abspath(sys.path[0]) # get path of this script
+        x=250
+        y=500
+        bmp=b.newBMP(x,y,4) # 16 color
+        color=10 # color info
+        #IFS(bmp,b.getIFSparams()['fern'],x1,y1,x2,y2,xscale,yscale,xoffset,yoffset,color,maxiter)
+        b.IFS(bmp,b.getIFSparams()['fern'],0,0,x,y,40,55,125,80,color,80000)
         file='fern.bmp'
         b.saveBMP(file,bmp)
         print('\nAll done close mspaint to finish')
@@ -18,5 +21,3 @@ def main():
 
 if __name__=="__main__": 
         main()
-
-
