208df104ffb6d60284e004bc25e16d2fcb16f104
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-20 11:51:27 +0800

    Update Hello_TV_Scanlines.py

--- Hello_TV_Scanlines.py
+++ Hello_TV_Scanlines.py
@@ -1,13 +1,18 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
 
 import Python_BMP.BITMAPlib as b,subprocess as proc
 from os import path,sys
         
 def main():
-        rootdir=path.abspath(sys.path[0])
+        rootdir=path.abspath(sys.path[0]) # get path of script
         bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
-        b.erasealternatehorizontallines(bmp,3,1,0)
+        eraseeverynline,eraseNthline=4,3 # control line erase
+        #if i%int_eraseeverynline==int_eraseNthline: bmp[s2:s2+bufsize]=blank
+        bytepattern=0 # byte pattern for line erase
+        b.erasealternatehorizontallines(bmp,eraseeverynline,eraseNthline,bytepattern)
         file='HelloTVEarth.bmp' #file name
         b.saveBMP(file,bmp)
         print('\nAll done close mspaint to finish')
