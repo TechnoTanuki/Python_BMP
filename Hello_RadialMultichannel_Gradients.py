0076e74815b57d814c6d2c8f74506ea4c9631158
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-23 19:01:17 +0800

    Add files via upload

---
+++ Hello_RadialMultichannel_Gradients.py
@@ -0,0 +1,27 @@
+#/--------------------------------------------------------------\
+#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
+#| This graphics library outputs to a bitmap file               |
+#\--------------------------------------------------------------/
+
+import Python_BMP.BITMAPlib as bm,subprocess as proc
+from os import path,sys
+
+def main():
+        rootdir=path.abspath(sys.path[0]) # get path of this script
+        mx,my=640,480 # x and y dim of bmp
+        bmp=bm.newBMP(mx,my,24) # (x,y,bit depth) 24 bit BMP
+        cen=bm.centercoord(bmp) # get x,y of center
+        f=lambda d:d*4 # simple linear func
+        for y in range(0,my):
+                for x in range(0,mx):
+                        b=int(f(bm.distance(cen,(x,y))))%256 # blue gradient
+                        r=255-b # green gradient 
+                        g=(abs(r-b))%256 # red gradient 
+                        bm.plotRGBxybit(bmp,x,y,(r,g,b)) # make a rainbow
+        file='HelloRadialMultichannelGradients.bmp' # file name
+        bm.saveBMP(file,bmp) # dump bytearray to file
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+
+if __name__=="__main__": 
+        main()
