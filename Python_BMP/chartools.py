e557ebc6d4824a0beb4d67acfa719b55562e3817
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-27 02:51:58 +0800

    Update chartools.py

--- Python_BMP/chartools.py
+++ Python_BMP/chartools.py
@@ -1,3 +1,8 @@
+#/--------------------------------------------------------------------------\
+#|    Copyright 2022 by Joel C. Alcarez    [joelalcarez1975@gmail.com]      |
+#|    We make absolutely no warranty of any kind, expressed or implied.     |
+#\--------------------------------------------------------------------------/
+
 def enumletters(st:str) -> str:
     c,i=len(st),0
     while i<c:
