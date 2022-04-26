72695ff277091cf124ebe97e5615587b91b93350
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-04-26 23:23:30 +0800

    Update primitives2D.py

--- Python_BMP/primitives2D.py
+++ Python_BMP/primitives2D.py
@@ -17,9 +17,9 @@
 #|        |  Note: This graphics library outputs to a bitmap file. |        |
 #\--------#--------------------------------------------------------#--------/
 
-from .mathlib import sign,subvect,addvect,mirror1stquad,computerotvec,roundvect,rotvec2D,combination,scalarmulvect,iif,setmax,radians,sin,cos,roundvectlist,rect2polarcoord2Dwithcenter,pivotlist
+from .mathlib import sign,subvect,addvect,mirror1stquad,computerotvec,roundvect,rotvec2D,combination,scalarmulvect,iif,setmax,radians,sin,cos,roundvectlist,rect2polarcoord2Dwithcenter,pivotlist,swapif
 
-def itercirclepart(r):
+def itercirclepart(r:int)->list:
     row,col,r_sqr=r,0,r*r
     two_r_sqr,four_r_sqr=r_sqr<<1,r_sqr<<2
     d=two_r_sqr*((row-1)*(row))+r_sqr+two_r_sqr*(1-r_sqr)
@@ -32,7 +32,7 @@ def itercirclepart(r):
         d+=two_r_sqr*(3+(col<<1))
         col+=1
 
-def itercirclepart1(r):
+def itercirclepart1(r:int)->list:
     row,col,r_sqr=r,0,r*r
     two_r_sqr,four_r_sqr=r_sqr<<1,r_sqr<<2
     d=two_r_sqr*((row-1)*(row))+r_sqr+two_r_sqr*(1-r_sqr)
@@ -44,7 +44,7 @@ def itercirclepart1(r):
             d+=two_r_sqr*(3+(col<<1))
             col+=1
 
-def itercirclepartlineedge(r):
+def itercirclepartlineedge(r:int)->list:
     row,col,r_sqr=r,0,r*r
     two_r_sqr,four_r_sqr=r_sqr<<1,r_sqr<<2
     d=two_r_sqr*((row-1)*(row))+r_sqr+two_r_sqr*(1-r_sqr)
@@ -62,7 +62,7 @@ def itercirclepartlineedge(r):
         d+=two_r_sqr*(3+(col<<1))
         col+=1
 
-def itercirclepartvertlineedge(r):
+def itercirclepartvertlineedge(r:int)->list:
     row,col,r_sqr=r,0,r*r
     two_r_sqr,four_r_sqr=r_sqr<<1,r_sqr<<2
     d=two_r_sqr*((row-1)*(row))+r_sqr+two_r_sqr*(1-r_sqr)
@@ -80,7 +80,7 @@ def itercirclepartvertlineedge(r):
         d+=two_r_sqr*(3+(col<<1))
         col+=1
 
-def iterline(p1,p2):
+def iterline(p1:list,p2:list)->list:
     p3=subvect(p2,p1)
     dx,dy=p3[0],p3[1]
     sdx,sdy,dxabs,dyabs=sign(dx),sign(dy),abs(dx),abs(dy)
@@ -105,13 +105,13 @@ def iterline(p1,p2):
             py+=sdy
     yield p2
 
-def iterparallelogram(p1,p2,p3):
+def iterparallelogram(p1:list,p2:list,p3:list)->list:
     p,q=lineseg(p1,p3),lineseg(p2,addvect(p3,subvect(p2,p1)))
     for u,v in zip(p,q): yield [u,v]
 
 def lineseg(p1,p2): return [p for p in iterline(p1,p2)]
 
-def iterellipsepart(b,a):
+def iterellipsepart(b:int,a:int):
     row,col,a_sqr,b_sqr=b,0,a*a,b*b
     two_a_sqr,four_a_sqr=a_sqr<<1,a_sqr<<2
     two_b_sqr,four_b_sqr=b_sqr<<1,b_sqr<<2
@@ -136,18 +136,18 @@ def iterellipse(x,y,b,a):
     for p in iterellipsepart(b,a):
          for v in mirror1stquad(x,y,p): yield v
 
-def iterellipserot(x,y,b,a,degrot):
+def iterellipserot(x:int,y:int,b:int,a:int,degrot:float):
     rotvec,c=computerotvec(degrot),[x,y]
     for p in iterellipsepart(b,a):
          for v in mirror1stquad(x,y,p): yield roundvect(addvect(rotvec2D(subvect(v,c),rotvec),c))
 
-def itercircle(x,y,r):
+def itercircle(x:int,y:int,r:int)->list:
     for p in itercirclepart(r):
          for v in mirror1stquad(x,y,p): yield v
 
 def bezierblend(i,n,u): return combination(n,i)*(u**i)*((1-u)**(n-i))
 
-def iterbeziercurve(pntlist):
+def iterbeziercurve(pntlist:list)->list:
     i,cnt,v=0,len(pntlist),pntlist[0]
     w,klim=v,cnt<<2
     while i<cnt:
@@ -161,9 +161,10 @@ def iterbeziercurve(pntlist):
                 w=v
         i+=1
 
-def beziercurvevert(pntlist,isclosed,curveback): return [v for v in iterbeziercurve(pntlist)]
+def beziercurvevert(pntlist:list,isclosed:bool,curveback:bool)->list: 
+    return [v for v in iterbeziercurve(pntlist)]
 
-def iterbspline(pntlist,isclosed,curveback):
+def iterbspline(pntlist:list,isclosed:bool,curveback:bool)->list:
     i,cnt,v=0,len(pntlist),pntlist[0]
     w,klim,ilim,mx=v,cnt<<2,cnt+iif(isclosed,2,0),cnt-1
     while i<ilim:
@@ -178,9 +179,10 @@ def iterbspline(pntlist,isclosed,curveback):
             w=v
         i+=1
 
-def bsplinevert(pntlist,isclosed,curveback): return [v for v in iterbspline(pntlist,isclosed,curveback)]
+def bsplinevert(pntlist:list,isclosed:bool,curveback:bool)->list: 
+    return [v for v in iterbspline(pntlist,isclosed,curveback)]
 
-def bsplineblend(u):#dont edit this square shaped code
+def bsplineblend(u:float)->list:#dont edit this square shaped code
     u2,u3= u*u,u*u*u
     d,f = u3/6 , 1/6
     a=-d +u2/2-u/2+f
@@ -188,24 +190,25 @@ def bsplineblend(u):#dont edit this square shaped code
     c=(-u3+u2+u)/2+f
     return (a,b,c,d)
 
-def recvert(x1,y1,x2,y2): return [(x1,y1),(x2,y1),(x2,y2),(x1,y2)]
+def recvert(x1:int,y1:int,x2:int,y2:int)->list: 
+    return [(x1,y1),(x2,y1),(x2,y2),(x1,y2)]
 
-def floatregpolygonvert(cx,cy,r,sides,angle):
+def floatregpolygonvert(cx:int,cy:int,r:int,sides:int,angle:int)->list:
     v,angle,anginc,maxang=[],radians(angle),360//sides,360
     for a in range(0,maxang,anginc):
         ang=angle+radians(a)
         v.append([int(cx-r*sin(ang)),int(cy-r*cos(ang))])
     return v
 
-def regpolygonvert(cx,cy,r,sides,angle): return roundvectlist(floatregpolygonvert(cx,cy,r,sides,angle))
+def regpolygonvert(cx:int,cy:int,r:int,sides:int,angle:float)->list: return roundvectlist(floatregpolygonvert(cx,cy,r,sides,angle))
 
-def horizontalvert(y,x1,x2,dx): return [[x,y] for x in range(x1,x2,dx)]
+def horizontalvert(y:int,x1:int,x2:int,dx:int)->list: return [[x,y] for x in range(x1,x2,dx)]
 
-def verticalvert(x,y1,y2,dy): return [[x,y] for y in range(y1,y2,dy)]
+def verticalvert(x:int,y1:int,y2:int,dy:int)->list: return [[x,y] for y in range(y1,y2,dy)]
 
-def circlevert(x,y,r): return [v for v in itercircle(x,y,r)]
+def circlevert(x:int,y:int,r:int)->list: return [v for v in itercircle(x,y,r)]
 
-def arcvert(x,y,r,startdegangle,enddegangle,showoutline):
+def arcvert(x:int,y:int,r:int,startdegangle:float,enddegangle:float,showoutline:bool):
     v,tol,c,sa,ea=[],0.03,(x,y),radians(startdegangle),radians(enddegangle)
     for p in itercircle(x,y,r):
         pc=rect2polarcoord2Dwithcenter(c,p)
@@ -216,12 +219,12 @@ def arcvert(x,y,r,startdegangle,enddegangle,showoutline):
                 for np in iterline(c,p): v.append(np)
     return v
 
-def rectboundarycoords(vlist):
+def rectboundarycoords(vlist:list)->list:
     u=pivotlist(vlist)
     x,y=u[0],u[1]
     return ((min(x),min(y)),(max(x),max(y)))
 
-def itergetneighbors(v,mx,my,includecenter):
+def itergetneighbors(v:list,mx:int,my:int,includecenter:bool)->list:
     x,y=v[0],v[1]
     if x>-1 and y>-1:
         lx,ty,rx,by=x-1,y-1,x+1,y+1
@@ -237,7 +240,7 @@ def itergetneighbors(v,mx,my,includecenter):
             yield [rx,y]
             if by<my: yield [rx,by]
 
-def spiralcontrolpointsvert(x,y,step,growthfactor,turns):
+def spiralcontrolpointsvert(x:int,y:int,step:int,growthfactor:int,turns:int):
     v=[[x,y]]
     inc=step
     while turns>0:
@@ -256,3 +259,17 @@ def spiralcontrolpointsvert(x,y,step,growthfactor,turns):
         step+=inc
         inc*=growthfactor
     return v
+
+def sortrecpoints(x1:int,y1:int,x2:int,y2:int):
+    x1,x2=swapif(x1,x2,x1>x2)
+    y1,y2=swapif(y1,y2,y1>y2)
+    return x1,y1,x2,y2
+
+def isinrectbnd(x:int,y:int,xmin:int,ymin:int,xmax:int,ymax:int) -> bool: 
+    return (x<xmax and y<ymax) and (x>xmin and y>ymin)
+
+def listinrecbnd(xylist:list,xmin:int,ymin:int,xmax:int,ymax:int) -> bool:
+    retval=True
+    for v in xylist:
+        if isinrectbnd(v[0],v[1],xmin,ymin,xmax,ymax)==False: break
+    return retval
