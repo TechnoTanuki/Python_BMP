d6e014021fb5c7954510dc564a678264e3224a8f
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-02 01:06:34 +0800

    add docstrings

--- Python_BMP/buffersplit.py
+++ Python_BMP/buffersplit.py
@@ -3,17 +3,44 @@
 #|    We make absolutely no warranty of any kind, expressed or implied.     |
 #\--------------------------------------------------------------------------/
 
-def altsplitbuf(buf):
+def altsplitbuf(buf:list) -> list:
+    """Split a list into two with alternating items in different list
+
+    Args:
+        buf: list,array or tuple
+        
+    Returns:
+        [[odd list],[even list]]
+
+    """
     m=len(buf)
     if m%2==0: m-=m&1
     return [buf[0:m-1:2],buf[1:m:2]]
 
-def altsplitbuf3way(buf):
+def altsplitbuf3way(buf:list) -> list:
+    """Split a list into three with alternating items in different list
+
+    Args:
+        buf: list,array or tuple [red,green,blue,....]
+        
+    Returns:
+        [[red list],[green list],[blue list]]
+
+    """
     m=len(buf)
     if m%3==0: m-=m&1
     return [buf[0:m-2:3],buf[1:m-1:3],buf[2:m:3]]
 
-def altsplitbufnway(buf,n):
+def altsplitbufnway(buf:list,n:int) -> list:
+    """Split a list into n with alternating items in different list
+
+    Args:
+        buf: list,array or tuple [red,green,blue,....]
+        
+    Returns:
+        [[red list],[green list],[blue list]]
+
+    """
     retval,m,j,i=[],len(buf),n-1,0
     if m%n==0: m-=m&1
     while i<n:
