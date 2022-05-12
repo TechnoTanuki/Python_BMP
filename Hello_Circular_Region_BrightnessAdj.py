fb0fd9f49173738ce1ba13a6ea576517a61aed3c
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 17:33:46 +0800

    PEP8 + notice

--- Hello_Circular_Region_BrightnessAdj.py
+++ Hello_Circular_Region_BrightnessAdj.py
@@ -1,26 +1,43 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+Circular region brightness adjustment
+                demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        centercoord,
+        brightnessadjcircregion,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) #get path of running script
-        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
-        r1=60 # radius 1 set to 60
-        r2=40 # radius 2 set to 40
-        br1=-50.8 # brightness adj set to -50.8%
-        br2=120 # brightness adj set to 120%
-        b.brightnessadjcircregion(bmp,x,y,r1,br1)
-        b.brightnessadjcircregion(bmp,x,y,r2,br2)
+        rootdir = path.dirname(__file__) #get path of running script
+        bmp = loadBMP(rootdir + '/assets/earth.bmp') #load earth to memory
+        (x,y) = centercoord(bmp) # How to get center of the bitmap
+        r1 = 60 # radius 1 set to 60
+        r2 = 40 # radius 2 set to 40
+        br1 = -50.8 # brightness adj set to -50.8%
+        br2 = 120 # brightness adj set to 120%
+        brightnessadjcircregion(bmp, x, y, r1, br1)
+        brightnessadjcircregion(bmp, x, y, r2, br2)
         # Python_BMP.BITMAPlib.brightnessadjcircregion(bmp bytearray,x int,y int, r int ,br signed float)
         file='HelloCircularRegionBrightnessAdj.bmp' #file name
-        b.saveBMP(file,bmp)
+        saveBMP(file, bmp)
         print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        ret =proc.call('mspaint ' + file) # replace with another editor if Unix
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
