489b2b11d7f84a5eb5a4c1a2386e7fb5fbb43d6e
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 17:44:26 +0800

    PEP8 + notice

--- Hello_GammaAdj.py
+++ Hello_GammaAdj.py
@@ -1,21 +1,38 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+        Gamma Adjustment Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        gammaadjto24bitimage,
+        saveBMP
+)
+
+import subprocess as proc
 from os import path,sys
-        
-def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') # load to memory
-        b.gammaadjto24bitimage(bmp,.5)  # 2nd param for gamma adj unsigned float
-        file='HelloGammaAdj.bmp' # file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
 
-if __name__=="__main__": 
+def main():
+        print(notice)
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = loadBMP(rootdir + '/assets/tanuki.bmp') # load to memory
+        gammaadjto24bitimage(bmp, .5)  # 2nd param for gamma adj unsigned float
+        file='HelloGammaAdj.bmp' # file name
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s' % (file,rootdir) )
+        print('\nAll done close mspaint to finish')
+        ret = proc.call('mspaint ' + file) # replace with another editor if Unix
+
+if __name__=="__main__":
         main()
 
 
