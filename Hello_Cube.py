150fff2b37e1950e3165a6f008dec7ab7b796ded
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 21:42:57 +0800

    make samples consistent

--- Hello_Cube.py
+++ Hello_Cube.py
@@ -1,30 +1,56 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+             Cube Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        getRGBfactors,
+        getshapesidedict,
+        plot3Dsolid,
+        rotvec3D,
+        cubevert,
+        saveBMP
+)
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        mx=my=250 # x=y square bmp
-        file='HelloCube.bmp' # some random file name as string
-        bmp=b.newBMP(mx,my,24) # RGB bmp
-        cenpt=b.centercoord(bmp) # helper method to get center of a bitmap
-        cf=b.getRGBfactors() # color info with presets
-        d,translationvector=400,[0,0,200] # be careful with these variables or object goes offscreen
-        isSolid=True # toggle solid or outline
-        showoutline=False # can show outline even if solid
-        cf=b.getRGBfactors() # color list 
-        color=cf['brightwhite'] # color of solid
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) #get path of running script
+        mx = my = 250 # x=y square bmp
+        file = 'HelloCube.bmp' # some random file name as string
+        bmp = newBMP(mx, my, 24) # RGB bmp
+        cenpt = centercoord(bmp) # helper method to get center of a bitmap
+        cf = getRGBfactors() # color info with presets
+        d, translationvector = 400, [0, 0, 200] # be careful with these variables or object goes offscreen
+        isSolid = True # toggle solid or outline
+        showoutline = False # can show outline even if solid
+        cf = getRGBfactors() # color list
+        color = cf['brightwhite'] # color of solid
         outlinecolor=10 # outline color
-        sd=b.getshapesidedict() # common shape dict for surface definitions
-        rotation=b.rotvec3D(40,80,50) # rotation vector (x,y,z) in degrees
-        b.plot3Dsolid(bmp,[b.cubevert(40),sd["cube"]],isSolid,color,showoutline,outlinecolor,rotation,translationvector,d,cenpt)
-        b.saveBMP(file,bmp) # save file
-        print('Saved '+ file )
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        sd = getshapesidedict() # common shape dict for surface definitions
+        rotation = rotvec3D(40, 80, 50) # rotation vector (x,y,z) in degrees
+        plot3Dsolid(bmp, [cubevert(40), sd["cube"]],
+                isSolid, color,
+                showoutline, outlinecolor,
+                rotation, translationvector,
+                d, cenpt)
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
