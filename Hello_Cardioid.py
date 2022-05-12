27fe15de9546f3690ef17a45d6fb2c32e6408d2f
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 22:04:25 +0800

    making code samples consistent

--- Hello_Cardioid.py
+++ Hello_Cardioid.py
@@ -1,26 +1,45 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+            Cardiod demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        plotflower,
+        saveBMP
+        )
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        x=y=256 # square bitmap
-        bmp=b.newBMP(x,y,24) # (x,y,bit depth) 
-        (x,y)=b.centercoord(bmp) # helper method to get center of a bitmap
-        r=x-10 # radius of flower
-        petals=1# for cardioid
-        angrot=45 # angle of rotation in degrees
-        lumrange=(255,240) # for brightness gradient
-        rgbfactors=(1,1,0) # color as rgb of ufloat 0 to 1
-        file='HelloCardioid.bmp' #file name
-        b.plotflower(bmp,x,y,r,petals,angrot,lumrange,rgbfactors)
-        b.saveBMP(file,bmp) # dump bytearray to file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        x = y = 256 # square bitmap
+        bmp = newBMP(x, y, 24) # (x,y,bit depth)
+        (x, y) = centercoord(bmp) # helper method to get center of a bitmap
+        r = x - 10 # radius of flower
+        petals = 1# for cardioid
+        angrot = 45 # angle of rotation in degrees
+        lumrange = (255, 240) # for brightness gradient
+        rgbfactors = (1, 1, 0) # color as rgb of ufloat 0 to 1
+        file = 'HelloCardioid.bmp' #file name
+        plotflower(bmp, x, y, r, petals, angrot,
+                lumrange, rgbfactors)
+        saveBMP(file, bmp) # dump bytearray to file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
