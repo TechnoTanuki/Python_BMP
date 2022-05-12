874512cf4ce5730222e16af9817e93921230141a
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 17:00:48 +0800

    Explicit imports

--- Hello_Coin.py
+++ Hello_Coin.py
@@ -12,37 +12,47 @@ notice = """
  -----------------------------------
 """
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        getRGBfactors,
+        rotvec3D,
+        cylindervertandsurface,
+        plot3Dsolid,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
 
 def main():
         print(notice)
         mx = my =250 # x=y square bmp
         file = 'HelloCoin.bmp' # some random file name as string
-        bmp = b.newBMP(mx, my, 24) # RGB bmp
-        cenpt = b.centercoord(bmp) # helper method to get center of a bitmap
-        cf = b.getRGBfactors() # color info with presets
-        d, translationvector=400, [0,0,200] # be careful with these variables or object goes offscreen
+        bmp = newBMP(mx, my, 24) # RGB bmp
+        cenpt = centercoord(bmp) # helper method to get center of a bitmap
+        cf = getRGBfactors() # color info with presets
+        d, translationvector=400, [0, 0, 200] # be careful with these variables or object goes offscreen
         isSolid = True # toggle solid or outline
         showoutline = True # can show outline even if solid
-        cf = b.getRGBfactors() # color list
+        cf = getRGBfactors() # color list
         color = cf['brightyellow'] # color of solid
         outlinecolor = 0 # outline color
-        rotation = b.rotvec3D(25,30,40) # rotation vector (x,y,z) in degrees
-        vcen = (1,0,0) # x y z coords
+        rotation = rotvec3D(25, 30, 40) # rotation vector (x,y,z) in degrees
+        vcen = (1, 0, 0) # x y z coords
         r = 40 # radius of cylinder
         zlen = 10 # height of cylinder
         deganglestep = 5 # how finely we tile flat surfaces around the cylinder
-        obj3D = b.cylindervertandsurface(vcen,r,zlen,deganglestep)# A solid is defined by vertices and surfaces
-        b.plot3Dsolid(bmp, obj3D,
+        obj3D = cylindervertandsurface(vcen, r, zlen, deganglestep)# A solid is defined by vertices and surfaces
+        plot3Dsolid(bmp, obj3D,
                 isSolid, color,
                 showoutline, outlinecolor,
                 rotation, translationvector,
                 d, cenpt)
-        b.saveBMP(file, bmp) # save file
+        saveBMP(file, bmp) # save file
         print('Saved '+ file )
         print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        ret =proc.call('mspaint '+ file) # replace with another editor if Unix
 
 if __name__=="__main__":
         main()
