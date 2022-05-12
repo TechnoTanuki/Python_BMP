ad0efa99bc6f2f25751b16085f6f43e422015fa5
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 15:55:36 +0800

    PEP8 + new header

--- Hello_FlipXY.py
+++ Hello_FlipXY.py
@@ -1,21 +1,30 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
-
+notice = """
+     Flip XY or 90 degree demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
-def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') # load bitmap to byte array-> bmp
-        bmp=b.flipXY(bmp) # need to return new XY flipped image
-        file='HelloFlipXY.bmp' # file name
-        b.saveBMP(file,bmp) # save XY flipped image
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+from os import path
 
-if __name__=="__main__": 
+def main():
+        print(notice)
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = b.loadBMP(rootdir + '/assets/somebody.bmp') # load bitmap to byte array-> bmp
+        bmp = b.flipXY(bmp) # need to return new XY flipped image
+        file = 'HelloFlipXY.bmp' # file name
+        b.saveBMP(file, bmp) # save XY flipped image
+        print('\nAll done close mspaint to finish')
+        ret =proc.call('mspaint ' + file) # replace with another editor if Unix
+
+if __name__=="__main__":
         main()
 
 
