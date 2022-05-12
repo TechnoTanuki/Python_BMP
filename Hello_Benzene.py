be0a37cb892c5699795640dd3fd959aaddf9b34a
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 16:56:11 +0800

    explicit imports

--- Hello_Benzene.py
+++ Hello_Benzene.py
@@ -11,29 +11,37 @@ notice = """
 |   to a bitmap file.               |
  -----------------------------------
 """
-import Python_BMP.BITMAPlib as b,subprocess as proc
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        gradvert,
+        regpolygonvert,
+        saveBMP
+)
+
+import subprocess as proc
 
 
 def main():
         print(notice)
         mx = my = 250 #bitmap size
-        bmp = b.newBMP(mx, my, 24) # 24 bit BMP
-        (x, y) = b.centercoord(bmp) # How to get center of the bitmap
+        bmp = newBMP(mx, my, 24) # 24 bit BMP
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
         r = 80 # radius of circle that circumscribes the regular polygon
         sides = 6 # we want a hexagon
         lumrange = (255, 0) # lum intensity decrease from center (byte,byte) unsigned
         rgbfactors = (.4, .7, .3) # color as unsigned tuple of (r,g,b) -> min 0 max 1
         angle = 0 # angle of rotation of regular polygon in degrees
-        polyvertlist = b.regpolygonvert(x, y, r, sides, angle) # generate polygon vertices
+        polyvertlist = regpolygonvert(x, y, r, sides, angle) # generate polygon vertices
         atomrad = 55 # make radius of carbon atoms 55 pixels
-        b.gradvert(bmp, polyvertlist,
+        gradvert(bmp, polyvertlist,
                         atomrad,
                         lumrange,
                         rgbfactors)
         file='HelloBenzene.bmp' #file name
-        b.saveBMP(file, bmp) # save file
+        saveBMP(file, bmp) # save file
         print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint ' + file) # replace with another editor if Unix
+        ret = proc.call('mspaint ' + file) # replace with another editor if Unix
 
 if __name__=="__main__":
         main()
