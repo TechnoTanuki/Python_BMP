4d08a3e958b2e16c5894462e374e1cbf1a9eccb7
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 16:07:04 +0800

    PEP8 + new notice

--- Hello_Icosahedron.py
+++ Hello_Icosahedron.py
@@ -1,30 +1,43 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
-
+notice = """
+         Icosahedron Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+
 def main():
-        mx=my=250 # x=y square bmp
-        file='HelloIcosahedron.bmp' # some random file name as string
-        bmp=b.newBMP(mx,my,24) # RGB bmp
-        cenpt=b.centercoord(bmp) # helper method to get center of a bitmap
-        cf=b.getRGBfactors() # color info with presets
-        d,translationvector=400,[0,0,200] # be careful with these variables or object goes offscreen
-        isSolid=True # toggle solid or outline
-        showoutline=False # can show outline even if solid
-        color=cf['brightwhite'] # color of solid
-        outlinecolor=0 # outline color
-        rotation=b.rotvec3D(70,7,20) # rotation vector (x,y,z) in degrees
-        b.plot3Dsolid(bmp,b.icosahedvertandsurface(40),isSolid,color,showoutline,outlinecolor,rotation,translationvector,d,cenpt)
-        b.saveBMP(file,bmp) # save file
+        print(notice)
+        mx = my = 250 # x=y square bmp
+        file = 'HelloIcosahedron.bmp' # some random file name as string
+        bmp = b.newBMP(mx, my, 24) # RGB bmp
+        cenpt = b.centercoord(bmp) # helper method to get center of a bitmap
+        cf = b.getRGBfactors() # color info with presets
+        d, translationvector = 400, [0, 0, 200] # be careful with these variables or object goes offscreen
+        isSolid = True # toggle solid or outline
+        showoutline = False # can show outline even if solid
+        color = cf['brightwhite'] # color of solid
+        outlinecolor = 0 # outline color
+        rotation = b.rotvec3D(70, 7, 20) # rotation vector (x,y,z) in degrees
+        b.plot3Dsolid(bmp,
+                b.icosahedvertandsurface(40),
+                isSolid, color,
+                showoutline, outlinecolor,
+                rotation, translationvector,
+                d, cenpt)
+        b.saveBMP(file, bmp) # save file
         print('Saved '+ file )
         print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        ret = proc.call('mspaint '+file) # replace with another editor if Unix
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
