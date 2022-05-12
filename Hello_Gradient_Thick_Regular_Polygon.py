9a3270931177a62ef0d4250b7787a468c23032bf
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 22:10:46 +0800

    making  the code samples consistent

--- Hello_Gradient_Thick_Regular_Polygon.py
+++ Hello_Gradient_Thick_Regular_Polygon.py
@@ -1,28 +1,48 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+ Gradient Thick Regular Polygon Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        regpolygonvert,
+        gradthickplotpoly,
+        saveBMP,
+        )
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get current script path
-        mx=my=200 # bitmap size
-        bmp=b.newBMP(mx,my,24) # RGB bitmap black background
-        x=y=100 # centerpoint
-        r=x-20 # radius of a circle that contains all the vertices 
-        sides=5 # for a pentagon
-        angle=30 # for rotation in degrees
-        penradius=15 # radius of pen
-        lumrange=(0,255) # increasing luminosity from center of pen
-        rgbfactors=(.7,.5,.6) # rgb values from 0 to 1 ufloat
-        polygonvertexlist=b.regpolygonvert(x,y,r,sides,angle) # generate vertices
-        b.gradthickplotpoly(bmp,polygonvertexlist,penradius,lumrange,rgbfactors)  # plot the polygon
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = my= 400 # bitmap size
+        bmp =  newBMP(mx, my ,24) # RGB bitmap black background
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        r = x - 20 # radius of a circle that contains all the vertices
+        sides = 5 # for a pentagon
+        angle = 30 # for rotation in degrees
+        penradius = 15 # radius of pen
+        lumrange = (0, 255) # increasing luminosity from center of pen
+        rgbfactors = (.7, .5, .6) # rgb values from 0 to 1 ufloat
+        polygonvertexlist = regpolygonvert(x, y, r, sides, angle) # generate vertices
+        gradthickplotpoly(bmp, polygonvertexlist, penradius,
+                lumrange, rgbfactors)  # plot the polygon
         file='HelloThickGradientRegularPolygon.bmp' # file name
-        b.saveBMP(file,bmp) # save the bitmap
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        saveBMP(file, bmp) # save the bitmap
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
