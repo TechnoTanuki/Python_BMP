8ba6370e006245565c051f0cdb65ceb5c70c079b
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 16:52:44 +0800

    PEP8 + notice

--- Hello_Flip_Horizontal.py
+++ Hello_Flip_Horizontal.py
@@ -1,21 +1,36 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+        Flip Horizontal Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        fliphorizontal,
+        saveBMP
+        )
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0])
-        bmp=b.loadBMP(rootdir+'/assets/tanuki.bmp') #load to memory
-        b.fliphorizontal(bmp)
-        file='HelloFliphorzontal.bmp' #file name
-        b.saveBMP(file,bmp) # save to file
+        print(notice)
+        bmp = loadBMP(path.dirname(__file__) + \
+                '/assets/tanuki.bmp') #load to memory
+        fliphorizontal(bmp)
+        file = 'HelloFliphorzontal.bmp' #file name
+        saveBMP(file, bmp) # save to file
         print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        ret = proc.call('mspaint '+file) # replace with another editor if Unix
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
