3786faab4d5ef9d593dba8adcae524608176b9c1
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 03:13:17 +0800

    making code samples consistent

--- Hello_Copy_Paste_Earth.py
+++ Hello_Copy_Paste_Earth.py
@@ -1,22 +1,40 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+   Hello Earth Copy and Paste Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        copyrect,
+        pasterect,
+        saveBMP
+        )
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get script path
-        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
-        buf=b.copyrect(bmp,3,3,60,60) # copy a rectangular region (x1,y1,x2,y2) to a buffer
-        b.pasterect(bmp,buf,40,40) # paste buffer to a rectangular region at (x1,y1)
-        file='HelloEarthCopyPaste.bmp' #file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp= loadBMP(rootdir + '/assets/earth.bmp') #load earth to memory
+        buf = copyrect(bmp, 3, 3, 60, 60) # copy a rectangular region (x1,y1,x2,y2) to a buffer
+        pasterect(bmp,buf,40,40) # paste buffer to a rectangular region at (x1,y1)
+        file = 'HelloEarthCopyPaste.bmp' #file name
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
