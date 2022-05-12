618a2080a421bc9e6731289f11af72ce459240aa
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 20:21:48 +0800

    making samples consistent

--- Hello_Circles.py
+++ Hello_Circles.py
@@ -1,27 +1,46 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+             Circle Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        circle,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of running script
-        mx=512 # bitmap size x
-        my=mx # bitmap size y = x square bitmap
-        bmp=b.newBMP(mx,my,8) #8 bit = 256 color
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = my = 512 # bitmap size y = x square bitmap
+        bmp = newBMP(mx, my, 8) #8 bit = 256 color
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
         for r in range(0,x): # increase the radius of the circle from 0 to x
-                c=r # set color equal to radius
-                b.circle(bmp,x,y,r,c,False) # all ints are unsigned
+                c = r # set color equal to radius
+                circle(bmp,x,y,r,c,False) # all ints are unsigned
                 # Python_BMP.BITMAPlib.circle(bmp bytearray,x int ,y int, r int, c int,isfilled bool)
-        file='HelloCircles.bmp' # file name 
-        b.saveBMP(file,bmp) # save file 
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        file='HelloCircles.bmp' # file name
+        saveBMP(file,bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file)
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
