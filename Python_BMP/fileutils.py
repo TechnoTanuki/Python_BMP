dd322e947bc17daccfb1f45c2ef98617575c4be0
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 09:46:20 +0800

    mobile friendly header comment + PEP8 + typehint

--- Python_BMP/fileutils.py
+++ Python_BMP/fileutils.py
@@ -1,41 +1,61 @@
-#/--------------------------------------------------------------------------\
-#|    Copyright 2022 by Joel C. Alcarez    [joelalcarez1975@gmail.com]      |
-#|    We make absolutely no warranty of any kind, expressed or implied.     |
-#\--------------------------------------------------------------------------/
+# -----------------------------------
+#| Copyright 2022 by Joel C. Alcarez |
+#| [joelalcarez1975@gmail.com]       |
+#|-----------------------------------|
+#|    We make absolutely no warranty |
+#| of any kind, expressed or implied |
+#|-----------------------------------|
+#|       The primary author and any  |
+#| any subsequent code contributors  |
+#| shall not be liable in any event  |
+#| for  incidental or consequential  |
+#| damages  in connection with,  or  |
+#| arising out from the use of this  |
+#| code in current form or with any  |
+#| modifications.                    |
+# -----------------------------------
 
 from os.path import isfile
+from typing import Callable
 from .messages import sysmsg
 
-def checklink(func):
-    """Decorator to test if first parameter in a function is a valid file
+def checklink(func: Callable):
+    """Decorator to test if
+        the first parameter
+        in a function is
+        a valid file
 
     Args:
         function
-        
+
     Returns:
         caller function
 
     """
-    def callf(*args,**kwargs):
-        if isfile(args[0]):  
-            return(func(*args,**kwargs))
-        else: 
+    def callf(*args, **kwargs):
+        if isfile(args[0]):
+            return(func(*args, **kwargs))
+        else:
             print(sysmsg['filenotexist'])
     return(callf)
 
-def checklinks(func):
-    """Decorator to test if two parameters in a function are valid files
+def checklinks(func: Callable):
+    """Decorator to test if
+        the two parameters
+        in a function
+        are valid files
 
     Args:
         function
-        
+
     Returns:
         caller function
 
     """
     def callf(*args,**kwargs):
-        if isfile(args[0]) and isfile(args[1]):  
+        if isfile(args[0]) and \
+           isfile(args[1]):
             return(func(*args,**kwargs))
-        else: 
+        else:
             print(sysmsg['filenotexist'])
     return(callf)
