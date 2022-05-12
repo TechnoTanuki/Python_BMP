74a0413dabc35fcd97e24824f97981955b6b6c5e
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 03:50:25 +0800

    minor changes + making samples consistent

--- Hello_NonLinearRadialMultichannel_Gradients.py
+++ Hello_NonLinearRadialMultichannel_Gradients.py
@@ -1,27 +1,48 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+Non Linear Radial Multichannel Gradient
+(Make fancy wallpaper with math demo)
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as bm,subprocess as proc
-from os import path,sys
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        plotRGBxybit,
+        distance,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
 
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        mx,my=640,480 # x and y dim of bmp
-        bmp=bm.newBMP(mx,my,24) # (x,y,bit depth) 24 bit BMP
-        cen=bm.centercoord(bmp) # get x,y of center
-        f=lambda d:d*d # simple non linear func
-        for y in range(0,my):
-                for x in range(0,mx):
-                        b=int(f(bm.distance(cen,(x,y))*(1/8)))%256 # blue gradient
-                        g=255-b # green gradient 
-                        r=(abs(g-b))%256 # red gradient 
-                        bm.plotRGBxybit(bmp,x,y,(r,g,b)) # make a rainbow
-        file='HelloNonLinearRadialMultichannelGradients.bmp' # file name
-        bm.saveBMP(file,bmp) # dump bytearray to file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx, my = 1024, 768 # dimensions of the bmp
+        bmp = newBMP(mx, my, 24) # (x,y,bit depth) 24 bit BMP
+        cen = centercoord(bmp) # get x,y of center
+        f = lambda d: d ** 1.8 # simple non linear func try different funcs
+        for y in range(0, my): # row sweep
+                for x in range(0, mx): # column sweep
+                        g = int(f(distance(cen, (x, y)) * (1 / 8))) % 256 # green gradient
+                        b = 255 - g # blue gradient
+                        r = (abs(b - g)) % 256 # red gradient
+                        plotRGBxybit(bmp, x, y, (r, g, b)) # make a rainbow
+        file = 'HelloNonLinearRadialMultichannelGradients.bmp' # file name
+        saveBMP(file, bmp) # dump bytearray to file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
