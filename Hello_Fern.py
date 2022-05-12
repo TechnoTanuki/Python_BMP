48b35f993e99974c7d3ff8c5bed1e9affc48f965
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 21:36:18 +0800

    making the code samples consistent

--- Hello_Fern.py
+++ Hello_Fern.py
@@ -1,19 +1,36 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+        IFS Fern Fractal Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        fern,
+        saveBMP
+)
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) #get path of this script
-        bmp=b.newBMP(250,500,4) #250 x 500 x 16 color
-        b.fern(bmp,0,0,249,499,10) #b.fern(bmp,x1,y1,x2,y2,color)
-        file='fern.bmp' #random filenme here
-        b.saveBMP(file,bmp) #save the fern
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) #get path of running script
+        bmp = newBMP(250, 500, 4) #250 x 500 x 16 color
+        fern(bmp, 0, 0, 249, 499, 10) #b.fern(bmp,x1,y1,x2,y2,color)
+        file = 'Hellofern.bmp' #random filenme here
+        saveBMP(file, bmp) #save the fern
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
