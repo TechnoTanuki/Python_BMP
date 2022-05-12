03351d506e94ab0626743db2e34f42f8b678b0a7
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 21:27:15 +0800

    make code samples consistent

--- Hello_Circular_Region_GammaAdj.py
+++ Hello_Circular_Region_GammaAdj.py
@@ -1,25 +1,44 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+  Circular region gamma adjust demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        centercoord,
+        gammacorrectcircregion,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # Get script directory
-        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') # load earth to memory
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
-        r=60 # radius
-        gc=.1 # gamma correction
-        b.gammacorrectcircregion(bmp,x,y,r,gc)
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) #get path of running script
+        bmp = loadBMP(rootdir+'/assets/earth.bmp') # load earth to memory
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        r = 60 # radius
+        gc = .1 # gamma correction
+        gammacorrectcircregion(bmp, x, y, r, gc)
         #Python_BMP.BITMAPlib.gammacorrectcircregion(bmp bytearray,x int,y int,r int,gc signed float)
         file='HelloCircularRegionGammaAdj.bmp' #file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
