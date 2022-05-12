6ace1d1b9fe6d9d2dc1f8dfdcb69323d10a10780
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-12 21:59:22 +0800

    making code samples consistent

--- Hello_FlipXY.py
+++ Hello_FlipXY.py
@@ -11,17 +11,23 @@ notice = """
 |   to a bitmap file.               |
  -----------------------------------
 """
-import Python_BMP.BITMAPlib as b,subprocess as proc
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        flipXY,
+        saveBMP
+        )
+
+import subprocess as proc
 from os import path
 
 def main():
         print(notice)
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        bmp = b.loadBMP(rootdir + '/assets/tanuki.bmp') # load bitmap to byte array-> bmp
-        bmp = b.flipXY(bmp) # need to return new XY flipped image
+        bmp = loadBMP(rootdir + '/assets/tanuki.bmp') # load bitmap to byte array-> bmp
+        bmp = flipXY(bmp) # need to return new XY flipped image
         file = 'HelloFlipXY.bmp' # file name
-        b.saveBMP(file, bmp) # save XY flipped image
+        saveBMP(file, bmp) # save XY flipped image
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
         ret = proc.call(imgedt + ' ' + file) # load the image
