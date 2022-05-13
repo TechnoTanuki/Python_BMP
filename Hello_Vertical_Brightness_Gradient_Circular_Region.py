a563bf4c12d307e5082d04ed4bc4ead42788f646
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 18:42:10 +0800

    make the sample code consistent

--- Hello_Vertical_Brightness_Gradient_Circular_Region.py
+++ Hello_Vertical_Brightness_Gradient_Circular_Region.py
@@ -1,20 +1,49 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+notice = """
+ Hello Vertical Brightness Gradient
+ in a circular region Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        vertbrightnessgrad2circregion,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0])
-        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp')
-        r=110
-        b.vertbrightnessgrad2circregion(bmp,10+r,5+r,r,[-80,150])
-        file='HelloVertBrightGradCircReg.bmp' #file name
-        b.saveBMP(file,bmp)
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = loadBMP(rootdir + '/assets/somebody.bmp')
+        r = 110
+        vertbrightnessgrad2circregion(
+                bmp, # unsigned byte array
+                10 + r, # int x
+                5 + r, # int y
+                r, # int radius r
+                [-80, 150]) # the brightness gradient
+        file = 'HelloVertBrightGradCircReg.bmp' #file name
+        saveBMP(file, bmp) #dump the bytearray to disk
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
+
+
 
 
