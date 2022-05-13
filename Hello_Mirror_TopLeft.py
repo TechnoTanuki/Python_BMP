37d977750b4f785d236ef29d6f070b3e4e5c54f2
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 18:50:49 +0800

    make sample code consistent

--- Hello_Mirror_TopLeft.py
+++ Hello_Mirror_TopLeft.py
@@ -1,21 +1,40 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+  Mirror the top-left part of a BMP
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        mirrortopleft,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') # load to memory
-        b.mirrortopleft(bmp)
-        file='HelloMirrorTopLeft.bmp' # file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = loadBMP(rootdir + '/assets/tanuki.bmp') #load to memory
+        mirrortopleft(bmp)
+        file = 'HelloMirrorTopLeft.bmp' # file name
+        saveBMP(file, bmp) #dump the bytearray to disk
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
