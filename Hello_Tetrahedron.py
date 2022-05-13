2c7dce6796a3cfc62a5001b38c54107349745833
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 00:38:17 +0800

    edits for code sample consistency

--- Hello_Tetrahedron.py
+++ Hello_Tetrahedron.py
@@ -1,33 +1,56 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+          Tetrahedron Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        getRGBfactors,
+        rotvec3D,
+        plot3Dsolid,
+        getshapesidedict,
+        tetrahedravert,
+        saveBMP
+        )
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        mx=my=250 # x=y square bmp
-        file='HelloTetrahedra.bmp' # some random file name as string
-        bmp=b.newBMP(mx,my,4) # RGB bmp
-        cenpt=b.centercoord(bmp) # helper method to get center of a bitmap
-        cf=b.getRGBfactors() # color info with presets
-        d,translationvector=400,[0,0,200] # be careful with these variables or object goes offscreen
-        isSolid=True # toggle solid or outline
-        showoutline=False # can show outline even if solid
-        cf=b.getRGBfactors() # color list 
-        color=cf['brightred'] # color of solid
-        outlinecolor=10 # outline color
-        sd=b.getshapesidedict() # common shape dict for surface definitions
-        rotation=b.rotvec3D(20,30,40) # rotation vector (x,y,z) in degrees
-        vertices=b.tetrahedravert(60) # get tetrahedra vetices
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = my = 250 # x=y square bmp
+        file = 'HelloTetrahedra.bmp' # some random file name as string
+        bmp = newBMP(mx, my, 24) # RGB bmp
+        cenpt = centercoord(bmp) # helper method to get center of a bitmap
+        cf = getRGBfactors() # color info with presets
+        d, translationvector= 400, [0, 0, 200] # be careful with these variables or object goes offscreen
+        isSolid = True # toggle solid or outline
+        showoutline = False # can show outline even if solid
+        cf = getRGBfactors() # color list
+        color = cf['brightred'] # color of solid
+        outlinecolor = 10 # outline color
+        sd = getshapesidedict() # common shape dict for surface definitions
+        rotation = rotvec3D(45,60,15) # rotation vector (x,y,z) in degrees
+        vertices = tetrahedravert(60) # get tetrahedra vetices
         surfaces=sd["tetrahedra"] # get tetrahedra surfaces
         obj3D=[vertices,surfaces] # A solid is defined by vertices and surfaces
-        b.plot3Dsolid(bmp,obj3D,isSolid,color,showoutline,outlinecolor,rotation,translationvector,d,cenpt)
-        b.saveBMP(file,bmp) # save file
+        plot3Dsolid(bmp,obj3D,isSolid,color,showoutline,outlinecolor,rotation,translationvector,d,cenpt)
+        saveBMP(file,bmp) # save file
         print('Saved '+ file )
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # replace with another editor if Unix
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
