d9382f68e434c694e3436de4bca829ef44a704ea
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 09:33:26 +0800

    making samples consistent

--- Hello_Color_Filter.py
+++ Hello_Color_Filter.py
@@ -1,22 +1,41 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+          Color Filter Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        colorfilterto24bitimage,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        bmp=b.loadBMP(rootdir+'/assets/somebody.bmp') #load to memory
-        rf,gf,bf=1,0.5,0 #RGB factors 0 to 1 float unsigned
-        b.colorfilterto24bitimage(bmp,[rf,gf,bf]) 
-        file='HelloColorFilter.bmp' #file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) #get path of running script
+        bmp = loadBMP(rootdir+'/assets/tanuki.bmp') #load to memory
+        rf, gf, bf = .3, 0.8, .5 #RGB factors 0 to 1 float unsigned
+        colorfilterto24bitimage(bmp, [rf, gf, bf])
+        file = 'HelloColorFilter.bmp' #file name
+        saveBMP(file,bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
+
 
 
