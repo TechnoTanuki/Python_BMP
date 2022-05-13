55f5dc711ee69e16c6257a53fdef124a95b5dace
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 10:29:54 +0800

    making code samples consistent

--- Hello_Sphere.py
+++ Hello_Sphere.py
@@ -1,26 +1,43 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+         Hello Sphere Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        sphere,
+        saveBMP
+)
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of running script
-        mx=200 # bitmap size x
-        my=mx # bitmap size y = x square bitmap
-        bmp=b.newBMP(mx,my,24) # 24 bit BMP
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
-        r=x-12 # radius = x - 12 
-        rgbfactors=(.4,1,.7) # unsigned floats 0 to 1 -> (r,g,b)
-        b.sphere(bmp,x,y,r,rgbfactors) # bmp is unsigned byte array
-        file='HelloSphere.bmp' # file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) #get path of running script
+        mx = my =320 # bitmap size y = x square bitmap
+        bmp = newBMP(mx, my, 24) # 24 bit BMP
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        r = x - 12 # radius = x - 12
+        rgbfactors=(.4, 1, .7) # unsigned floats 0 to 1 -> (r,g,b)
+        sphere(bmp, x, y, r, rgbfactors) # bmp is unsigned byte array
+        file = 'HelloSphere.bmp' # file name
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user something happened
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
