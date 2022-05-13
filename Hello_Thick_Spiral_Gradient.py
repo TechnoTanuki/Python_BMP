53e3814d5a0bb701281398b677ece811ac243fe6
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 18:33:06 +0800

    make sample code consistent

--- Hello_Thick_Spiral_Gradient.py
+++ Hello_Thick_Spiral_Gradient.py
@@ -1,20 +1,49 @@
-#Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com
-#This graphics library outputs to a bitmap file
+notice = """
+  Hello Thick Spiral Gradient Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        gradvert,
+        bsplinevert,
+        spiralcontrolpointsvert,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0])
-        mx,my=300,300 #bitmap size
-        bmp=b.newBMP(mx,my,24)
-        b.gradvert(bmp,b.bsplinevert(b.spiralcontrolpointsvert(150,150,7,1,10),False,False),5,[255,0],[.9,.5,.9])
-        file='HelloThickGradSpiral.bmp' #file name
-        b.saveBMP(file,bmp)
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = my = 300 #bitmap size
+        bmp = newBMP(mx, my, 24)
+        gradvert(bmp,
+                bsplinevert(
+                        spiralcontrolpointsvert(
+                                150, 150, 7, 1, 10),
+                        False, False),
+                5, [255, 0], [.9, .5, .9])
+        file = 'HelloThickGradSpiral.bmp' # random file name
+        saveBMP(file, bmp) # dump the byterray to disk
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
+
 
 
