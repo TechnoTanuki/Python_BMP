f27f946e2ecdee9a91cbbc6f8a9fd98e2b8428a9
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 09:57:36 +0800

    making samples consistent

--- Hello_Octahedron.py
+++ Hello_Octahedron.py
@@ -1,33 +1,58 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+           Octahedra Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        getRGBfactors,
+        getshapesidedict,
+        plot3Dsolid,
+        octahedravert,
+        rotvec3D,
+        saveBMP
+        )
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
+
 def main():
-        mx=my=250 # x=y square bmp
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) #get path of running script
         file='HelloOctahedra.bmp' # some random file name as string
-        bmp=b.newBMP(mx,my,4) # RGB bmp
-        cenpt=b.centercoord(bmp) # helper method to get center of a bitmap
-        cf=b.getRGBfactors() # color info with presets
-        d,translationvector=400,[0,0,200] # be careful with these variables or object goes offscreen
-        isSolid=True # toggle solid or outline
-        showoutline=True # can show outline even if solid
-        cf=b.getRGBfactors() # color list 
-        color=cf['brightyellow'] # color of solid
-        outlinecolor=10 # outline color
-        sd=b.getshapesidedict() # common shape dict for surface definitions
-        rotation=b.rotvec3D(0,20,30) # rotation vector (x,y,z) in degrees
-        vertices=b.octahedravert(50) # get octahedra vetices
+        mx = my = 400 # bitmap dimensions
+        bmp = newBMP(mx,my,4) # RGB bmp
+        cenpt = centercoord(bmp) # helper method to get center of a bitmap
+        cf = getRGBfactors() # color info with presets
+        d, translationvector=400, [0, 0, 200] # be careful with these variables or object goes offscreen
+        isSolid = True # toggle solid or outline
+        showoutline = True # can show outline even if solid
+        cf = getRGBfactors() # color list
+        color = cf['brightyellow'] # color of solid
+        outlinecolor = 0 # outline color
+        sd = getshapesidedict() # common shape dict for surface definitions
+        rotation = rotvec3D(60, 60, 30) # rotation vector (x,y,z) in degrees
+        vertices = octahedravert(50) # get octahedra vetices
         surfaces=sd["octahedra"] # get octahedra surfaces
-        obj3D=[vertices,surfaces] # A solid is defined by vertices and surfaces
-        b.plot3Dsolid(bmp,obj3D,isSolid,color,showoutline,outlinecolor,rotation,translationvector,d,cenpt)
-        b.saveBMP(file,bmp) # dump byte array to file system as-is
-        print('Saved '+ file ) 
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        obj3D = [vertices, surfaces] # A solid is defined by vertices and surfaces
+        plot3Dsolid(bmp, obj3D, isSolid, color,
+                showoutline, outlinecolor,
+                rotation, translationvector, d, cenpt)
+        saveBMP(file, bmp) # dump byte array to file system as-is
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
