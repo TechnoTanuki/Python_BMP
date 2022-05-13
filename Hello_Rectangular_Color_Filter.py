b91b666a5d1f7f547c1a47c1d2ddb75fa83eaa5c
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 21:49:53 +0800

    make code samples consistent

--- Hello_Rectangular_Color_Filter.py
+++ Hello_Rectangular_Color_Filter.py
@@ -1,21 +1,41 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+     Hello apply a Color filter
+     to rectangular region Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        colorfilterto24bitregion,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') # load earth to memory
-        rf,gf,bf=1,.7,.3 #RGB factors 0 to 1 float
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = loadBMP(rootdir+'/assets/earth.bmp') # load earth to memory
+        rf, gf, bf = 1, .7, .3 #RGB factors 0 to 1 float
         # colorfilterto24bitregion(bmp,x1,y1,x2,y2,[rf,gf,bf])
-        b.colorfilterto24bitregion(bmp,30,30,138,138,[rf,gf,bf])
-        file='HelloRectangularColorFilter.bmp' # file name
-        b.saveBMP(file,bmp) # dump bytearray bmp to file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        colorfilterto24bitregion(bmp, 30, 30, 138, 138,[rf, gf, bf])
+        file = 'HelloRectangularColorFilter.bmp' # file name
+        saveBMP(file,bmp) # dump bytearray bmp to file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
