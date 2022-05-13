062f76f8346e90cd8d56ed8d890914799d4327e7
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 09:42:21 +0800

    making sample code consistent

--- Hello_Elliptical_Gradient.py
+++ Hello_Elliptical_Gradient.py
@@ -1,26 +1,43 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+      Elliptical Gradient Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        gradellipse,
+        saveBMP
+        )
 
-import Python_BMP.BITMAPlib as bm,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get pathof this script
-        bmp=bm.newBMP(300,200,24) # 24 bit 300 x 200 bmp
-        (x,y)=bm.centercoord(bmp) # How to get center of the bitmap
-        b=y # b axis = y
-        a=x # a axis = x
-        lumrange=(255,0) # decreasing gradient from center uint bytes
-        rgbfactors=(1,.4,1) # rgb triplet as values 0 to 1 unsigned float
-        bm.gradellipse(bmp,x,y,b,a,lumrange,rgbfactors)
-        file='HelloEllipticalGradent.bmp' #file name
-        bm.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) #get path of running script
+        bmp = newBMP(640, 480, 24) # 24 bit 300 x 200 bmp
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        b = y # b axis = y
+        a = x # a axis = x
+        lumrange = (255, 0) # decreasing gradient from center uint bytes
+        rgbfactors = (.3, .9, .5) # rgb triplet as values 0 to 1 unsigned float
+        gradellipse(bmp, x, y, b, a, lumrange, rgbfactors)
+        file = 'HelloEllipticalGradent.bmp' #file name
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
-
-
