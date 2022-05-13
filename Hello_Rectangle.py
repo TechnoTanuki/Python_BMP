3301a131b754bc147de772ac03fb21e27f85b166
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 00:52:33 +0800

    make code samples consistent

--- Hello_Rectangle.py
+++ Hello_Rectangle.py
@@ -1,25 +1,42 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+        Hello Rectangle Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        rectangle,
+        saveBMP
+        )
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # obtain the path of this script
-        x,y=200,100 # bitmap size
-        bmp=b.newBMP(x,y,4) # 2^4 = 16 colors -> black background by default
-        j=19 # border
-        color=10 # change this 0 to 15 -> if 4 bits... 0 black thou lmao
-        x1=y1=j # define top left (x,y) 
-        x2,y2=x-j,y-j # define bottom right (x,y) 
-        b.rectangle(bmp,x1,y1,x2,y2,color)
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        x, y = 320, 200 # bitmap size
+        bmp = newBMP(x, y, 4) # 2^4 = 16 colors -> black background by default
+        j = 19 # border
+        color = 10 # change this 0 to 15 -> if 4 bits... 0 black thou lmao
+        x1 = y1 = j # define top left (x,y)
+        x2, y2 = x - j, y - j # define bottom right (x,y)
+        rectangle(bmp, x1, y1, x2, y2, color) # draw a rectangle
         file='HelloRectangle.bmp' #file name
-        b.saveBMP(file,bmp) # we dump the byte array bmp -> file (so simple)
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        saveBMP(file,bmp) # we dump the byte array bmp -> file (so simple)
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt))
+        ret = proc.call(imgedt + ' ' + file) # replace with another editor if Unix
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
