66c64369ea8ddedd6e0623db267ffd1a9335bc44
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 10:10:23 +0800

    making code samples consistent

--- Hello_FilledCircle.py
+++ Hello_FilledCircle.py
@@ -1,27 +1,46 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+         Filled Circle Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        filledcircle,
+        saveBMP
+)
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of running script
-        mx=512 # bitmap size x
-        my=mx # bitmap size y = x square bitmap
-        bmp=b.newBMP(mx,my,4) # 4 bit = 16 color
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
-        r=x-12 # radius
-        c=11 # color
-        b.filledcircle(bmp,x,y,r,c)
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) #get path of running script
+        my = mx = 256 # bitmap size y = x square bitmap
+        bmp = newBMP(mx, my, 4) # 4 bit = 16 color
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        r = x - 12 # radius
+        c = 11 # color
+        filledcircle(bmp, x, y, r, c)
         # Python_BMP.BITMAPlib.filledcircle(bmp bytearray,x int ,y int, r int, c int)
-        file='HelloFilledCirle.bmp' # file name 
-        b.saveBMP(file,bmp) # save file 
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        file = 'HelloFilledCirle.bmp' # some random file name
+        saveBMP(file,bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user something happened
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
+
 
 
