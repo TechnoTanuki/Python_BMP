28872bc78778c432b36f1e320f38e99b3cceb906
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 20:07:29 +0800

    make code samples consistent

--- Hello_Thick_Circle.py
+++ Hello_Thick_Circle.py
@@ -1,28 +1,47 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+       Hello Thick Circle Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        thickcircle,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of running script
-        mx=128 # bitmap size x
-        my=mx # bitmap size y = x square bitmap        
-        bmp=b.newBMP(mx,my,4) # 4 bit = 16 color
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap        
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = my = 256 # bitmap size y = x square bitmap
+        bmp = newBMP(mx, my, 4) # 4 bit = 16 color
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
         r=x-12  # set radius = x - 12
-        color=14
-        penradius=12
-        b.thickcircle(bmp,x,y,r,penradius,color) # all unsigned
+        color = 10 # set the color
+        penradius = 10 # control the thickness
+        thickcircle(bmp, x, y, r, penradius, color) # all unsigned
         #Python_BMP.BITMAPlib.thickcircle(bmp bytyearray,x int,y int ,r int,penradius int,color int)
-        file='HelloThickCirle.bmp' #file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        file = 'HelloThickCircle.bmp' #file name
+        saveBMP(file, bmp) #dump the bytearray to disk
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
