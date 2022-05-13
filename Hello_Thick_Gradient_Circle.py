17f032574033a4715c163ee861ea6af267c7dd18
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 01:32:07 +0800

    making code samples consistent

--- Hello_Thick_Gradient_Circle.py
+++ Hello_Thick_Gradient_Circle.py
@@ -1,29 +1,50 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+  Hello Thick Gradient Circle Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        gradthickcircle,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of running script
-        mx=512 # bitmap size x
-        my=mx # bitmap size y = x square bitmap        
-        bmp=b.newBMP(mx,my,24)
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap        
-        r=x-12  # set radius = x -12
-        lumrange=(255,0) # byte tuple luminosity range
-        rgbfactors=(0,.5,1) # unsigned float tuple RGB 0 to 1 values
-        penradius=12 # Thickness
-        b.gradthickcircle(bmp,x,y,r,penradius,lumrange,rgbfactors) # all unsigned
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = my = 256 # bitmap size y = x square bitmap
+        bmp = newBMP(mx, my, 24)
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        penradius = 12 # Thickness
+        r = x - penradius  # set radius
+        lumrange = (255, 0) # byte tuple luminosity range
+        rgbfactors = (0, .5, 1) # unsigned float tuple RGB 0 to 1 values
+        gradthickcircle(bmp, x, y, r,
+                penradius, lumrange,
+                rgbfactors) # all unsigned
         # Python_BMP.BITMAPlib.gradthickcircle(bmp bytearray,x int,y int,r int,penradius int,lumrange see line 16,rgbfactors see line 17)
-        file='HelloThickGradientCircle.bmp' #file name
-        b.saveBMP(file,bmp) # save bitmap
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        file = 'HelloThickGradientCircle.bmp' #file name
+        saveBMP(file, bmp) # save bitmap
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
