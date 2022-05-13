54b8745a196405237c26da3bb3882bcc9ea46c9e
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 11:02:31 +0800

    make samples consistent

--- Hello_Mandelbrot_Set.py
+++ Hello_Mandelbrot_Set.py
@@ -1,24 +1,46 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+     Mandelbrot Set Fractal Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP, #insert other func
+        getRGBfactors,
+        mandelbrot,
+        mandelparamdict,
+        saveBMP #here
+)
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get current script path
-        mx=my=500 # square canvas
-        bmp=b.newBMP(mx,my,24) # RGB bitmap 500 x 500
-        cf=b.getRGBfactors() #color info
-        mandelpar=b.mandelparamdict() # common parameters 
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) #get path of running script
+        mx = my = 600 # square canvas
+        bmp = newBMP(mx, my, 24) # RGB bitmap 500 x 500
+        cf= getRGBfactors() #color info
+        mandelpar = mandelparamdict() # common parameters
         #mandelbrot(bmp,x1,y1,x2,y2,mandelpar[],RGBfactors,maxiter)
-        b.mandelbrot(bmp,0,0,mx,my,mandelpar['maxeqdim'],cf['brightgreen'],255)
-        file='mandelbrot.bmp'
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        mandelbrot(bmp, 0, 0, mx, my,
+                mandelpar['maxeqdim'],
+                cf['brightgreen'], 255)
+        file = 'mandelbrot.bmp' # random file name
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user something happened
+        ret = proc.call(imgedt + ' ' + file) # load the image
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
+
 
