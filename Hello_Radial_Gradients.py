89d85e7d4f35e099c78bb3f0616f0cb861c8c713
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-13 21:55:45 +0800

    make code samples consistent

--- Hello_Radial_Gradients.py
+++ Hello_Radial_Gradients.py
@@ -1,29 +1,52 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+    Hello radial gradients Demo
+    (make a wallpaper with math)
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as bm,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        distance,
+        centercoord,
+        plotRGBxybit,
+        getmaxxy,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        mx,my=1024,768 # x and y dim of bmp
-        bmp=bm.newBMP(mx,my,24) # (x,y,bit depth)  24 bit BMP
-        end=bm.getmaxxy(bmp) # get max x and y
-        cen=bm.centercoord(bmp) # get x,y of center
-        ori=(0,0) # define as origin
-        for y in range(0,my):
-                for x in range(0,mx):
-                        pt=(x,y) # pacc x,y to tuple
-                        r=int(bm.distance(ori,pt))%256 # red gradient
-                        g=int(bm.distance(end,pt))%256 # green gradient
-                        b=int(bm.distance(cen,pt))%256 # blue gradient
-                        bm.plotRGBxybit(bmp,x,y,(r,g,b)) # make a rainbow
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx, my = 1024, 768 # x and y dim of bmp
+        bmp = newBMP(mx,my,24) # (x,y,bit depth)  24 bit BMP
+        end = getmaxxy(bmp) # get max x and y
+        cen = centercoord(bmp) # get x,y of center
+        ori = (0, 0) # define as origin
+        for y in range(0, my):
+                for x in range(0, mx):
+                        pt = (x, y) # pacc x,y to tuple
+                        r = int(distance(ori, pt)) % 256 # red gradient
+                        g = int(distance(end, pt)) % 256 # green gradient
+                        b = int(distance(cen, pt)) % 256 # blue gradient
+                        plotRGBxybit(bmp, x, y, (r, g, b)) # make a rainbow
         file='HelloRadialGradients.bmp' #file name
-        bm.saveBMP(file,bmp)
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        saveBMP(file, bmp)
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
