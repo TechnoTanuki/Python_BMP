ad4264b65c7b07fc1c8759573682905bf5656262
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 20:22:50 +0800

    make code samples consistent

--- Hello_Thick_Exponential_Spiral_Gradient.py
+++ Hello_Thick_Exponential_Spiral_Gradient.py
@@ -1,30 +1,55 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+Thick Exponential Spiral Gradient Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        gradvert,
+        spiralcontrolpointsvert,
+        bsplinevert,
+        saveBMP
+        )
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+import subprocess as proc
+from os import path
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        bmp=b.newBMP(300,300,24) # RGB bitmap 300 x 300
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
-        step=7 # growth increment of spiral
-        growthfactor=1.61803399 # Golden Ratio... 
-        turns=5 # number of turns of the spiral
-        vertlist=b.spiralcontrolpointsvert(x,y,step,growthfactor,turns) # compute points of control points 
-        isclosed=False # closed or open curve
-        curveback=False # do extra compute to trace back to origin
-        pts=b.bsplinevert(vertlist,isclosed,curveback) # compute points of bspline 
-        penradius=5 # radius of pen in pixel
-        lumrange=(255,0) # brightness decrease from center of pen to edge <ubytes>
-        rgbfactors=(.9,.5,.9) # color as (r,g,b) values between 1 and 0 <ufloat>
-        b.gradvert(bmp,pts,penradius,lumrange,rgbfactors) # render the spiral
-        file='HelloThickExpGradSpiral.bmp' #file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = newBMP(300, 300, 24) # RGB bitmap 300 x 300
+        (x,y) = centercoord(bmp) # How to get center of the bitmap
+        step = 7 # growth increment of spiral
+        growthfactor = 1.61803399 # Golden Ratio...
+        turns = 5 # number of turns of the spiral
+        vertlist = \
+                spiralcontrolpointsvert(x, y,
+                        step,
+                        growthfactor,
+                        turns) # compute points of control points
+        isclosed = False # closed or open curve
+        curveback = False # do extra compute to trace back to origin
+        pts = bsplinevert(vertlist, isclosed, curveback) # compute points of bspline 
+        penradius = 5 # radius of pen in pixel
+        lumrange = (255, 0) # brightness decrease from center of pen to edge <ubytes>
+        rgbfactors = (.9, .5, .9) # color as (r,g,b) values between 1 and 0 <ufloat>
+        gradvert(bmp, pts, penradius,
+                lumrange, rgbfactors) # render the spiral
+        file = 'HelloThickExpGradSpiral.bmp' #file name
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
