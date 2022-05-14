da71e76b85937a23afef347236bd3f0c1f7b6ff2
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 08:31:48 +0800

    edits for consistency in code samples

--- Hello_Circular_Gradient.py
+++ Hello_Circular_Gradient.py
@@ -1,26 +1,45 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+    Hello Circular Gradient Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc 
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        gradcircle,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script 
-        mx=800 # bitmap size x
-        my=600 # bitmap size y
-        bmp=b.newBMP(mx,my,24) # RGB bitmap
-        mx,my=mx-1,my-1 # max-1 for screen
-        x,y=mx>>1,my>>1 # div by 2 via bit ops
-        r=500 # max radius of gradient
-        lumrange=(0,255) # increase in color brightness farther from (x,y)
-        rgbfactors=(.25,.75,.5) # rgb triplet as ufloat 0 to 1
-        b.gradcircle(bmp,x,y,r,lumrange,rgbfactors) # compute gradient
-        file='HelloCircularGradient.bmp' #file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = 800 # bitmap size x
+        my = 600 # bitmap size y
+        bmp = newBMP(mx, my, 24) # RGB bitmap
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        r = 500 # max radius of gradient
+        lumrange = (0, 255) # increase in color brightness farther from (x,y)
+        rgbfactors = (.25, .75, .5) # rgb triplet as ufloat 0 to 1
+        gradcircle(bmp, x, y, r, lumrange, rgbfactors) # compute gradient
+        file = 'HelloCircularGradient.bmp' #file name
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
