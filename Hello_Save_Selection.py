51c3167efa346145f478e9738318ba190e1c876a
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 17:56:36 +0800

    make code samples consistent

--- Hello_Save_Selection.py
+++ Hello_Save_Selection.py
@@ -1,22 +1,41 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+   Save selected image region Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        copyrect,
+        convertselection2BMP,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get script path
-        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') #load earth to memory
-        buff=b.copyrect(bmp,5,5,63,63) # copy rectagular region (x1,y1,x2,y2) uint to buff
-        nbmp=b.convertselection2BMP(buff)# convert buffer to bitmap
-        file='HelloSaveSelection.bmp' #file name
-        b.saveBMP(file,nbmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = loadBMP(rootdir + '/assets/earth.bmp') #load earth to memory
+        buff = copyrect(bmp, 7, 7, 61, 61) # copy rectagular region (x1,y1,x2,y2) uint to buff
+        nbmp = convertselection2BMP(buff)# convert buffer to bitmap
+        file = 'HelloSaveSelection.bmp' #file name
+        saveBMP(file, nbmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
-
 
