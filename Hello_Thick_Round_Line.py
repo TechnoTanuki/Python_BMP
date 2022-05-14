c9040cf98ae1f01ea979b6f4e4a7ebfb261e907a
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 16:21:20 +0800

    making code samples consistent

--- Hello_Thick_Round_Line.py
+++ Hello_Thick_Round_Line.py
@@ -1,27 +1,46 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+          Hello Globe Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        thickroundline,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of running script
-        mx,my=200,200 # bitmap size
-        bmp=b.newBMP(mx,my,4) # 4 bit = 16 color
-        p1=(12,12) # 1st point as (x,y) 
-        p2=(mx-12,my-12) # 2nd point as (x,y) 
-        penradius=10
-        color=13
-        b.thickroundline(bmp,p1,p2,penradius,color) # all unsigned
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = my = 200 # bitmap size
+        bmp = newBMP(mx, my, 4) # 4 bit = 16 color
+        p1 = (12,12) # 1st point as (x,y)
+        p2 = (mx -12, my-12) # 2nd point as (x,y)
+        penradius = 10
+        color = 13
+        thickroundline(bmp, p1, p2, penradius, color) # all unsigned
         #Python_BMP.BITMAPlib.thickroundline(bmp bytearray,p1 xy-tuple,p2 xy-tuple,penradius int,color int)
-        file='HelloThickroundLine.bmp' #file name
-        b.saveBMP(file,bmp) # save file 
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        file = 'HelloThickroundLine.bmp' #file name
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
