5b0f202b61ca171da2e74c49d14d20979a8d6413
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 16:42:23 +0800

    make code samples consistent

--- Hello_Crop_Earth.py
+++ Hello_Crop_Earth.py
@@ -1,20 +1,42 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+   Hello Crop image and save Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        cropBMPandsave
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get script path
-        existingfile=rootdir+'/assets/earth.bmp'
-        newfile='HelloCropEarth.bmp' #file name
-        b.cropBMPandsave(existingfile,newfile,5,5,60,60) # crop a rectangular region (x1,y1,x2,y2) to a file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+newfile) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        existingfile = rootdir + '/assets/earth.bmp'
+        file = 'HelloCropEarth.bmp' #file name
+        # crop a rectangular region (x1,y1,x2,y2) to a file
+        cropBMPandsave(
+                existingfile,
+                file, # new file to save in
+                20, 20, 100, 100 # area to crop
+                )
+        print('All done close %s to finish' % \
+                (imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
