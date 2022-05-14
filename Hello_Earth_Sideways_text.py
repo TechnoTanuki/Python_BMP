afa6c969d3ca2b627741f8d3ee65c49176142d9c
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 17:48:13 +0800

    make code samples consistent

--- Hello_Earth_Sideways_text.py
+++ Hello_Earth_Sideways_text.py
@@ -1,26 +1,50 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+   Hello sideways text Earth Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        loadBMP,
+        getcolorname2RGBdict,
+        plotstringsideway,
+        font8x14,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        bmp=b.loadBMP(rootdir+'/assets/earth.bmp') # load earth to memory wow.., it fits
-        c=b.getcolorname2RGBdict() #friendly color names 2 rgb
-        x,y=150,150
-        fontsize=2
-        pixspace=1 # space between bitmap font pixels (0 = default)
-        charspace=0 # space bitmap font characters (0 = default)
-        b.plotstringsideway(bmp,x,y,'Hello!!!',fontsize,pixspace,charspace,c['brightyellow'],b.font8x14)
-        file='HelloEarthSidewayText.bmp' #file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = loadBMP(rootdir + '/assets/earth.bmp') # load earth to memory wow.., it fits
+        c = getcolorname2RGBdict() #friendly color names 2 rgb
+        x = y = 150
+        fontsize =2
+        pixspace = 1 # space between bitmap font pixels (0 = default)
+        charspace = 0 # space bitmap font characters (0 = default)
+        plotstringsideway(bmp, x, y,
+                'Hello!!!',
+                fontsize, pixspace, charspace,
+                c['brightyellow'], font8x14)
+        file = 'HelloEarthSidewayText.bmp' #file name
+        saveBMP(file,bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
