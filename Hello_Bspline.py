df51fec7873a557b1b20dbbefa7acd161c18440d
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 16:15:09 +0800

    making code samples consistent

--- Hello_Bspline.py
+++ Hello_Bspline.py
@@ -1,31 +1,52 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+         Hello Bspline Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        bspline,
+        spiralcontrolpointsvert,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        mx,my=250,250 # bitmap size
-        bmp=b.newBMP(mx,my,4) # 4 bit = 2^4 = 16 bits
-        file='HelloBspline.bmp' # file name
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
-        step=5 # pixel interval between spiral turn
-        growthfactor=1 # greater than 1 means exponential spiral
-        turns=13 # number of turns of spiral
-        vertlist = b.spiralcontrolpointsvert(x,y,step,growthfactor,turns) # list of (x,y) control points
-        penradius=2 # radius of pen
-        color=11 
-        isclosed=True # closed curve
-        curveback=True # do extra computation to curve back to origin
-        b.bspline(bmp,vertlist,penradius,color,isclosed,curveback)
-        b.saveBMP(file,bmp) # save bitmap
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = my = 250 # bitmap size
+        bmp = newBMP(mx, my, 4) # 4 bit = 2^4 = 16 bits
+        file = 'HelloBspline.bmp' # file name
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        step = 5 # pixel interval between spiral turn
+        growthfactor = 1 # greater than 1 means exponential spiral
+        turns = 13 # number of turns of spiral
+        vertlist = spiralcontrolpointsvert(x,y,step,growthfactor,turns) # list of (x,y) control points
+        penradius = 2 # radius of pen
+        color = 11
+        isclosed = True # closed curve
+        curveback = True # do extra computation to curve back to origin
+        bspline(bmp, vertlist, penradius,
+                color, isclosed, curveback)
+        saveBMP(file, bmp) # save bitmap
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
-
 
