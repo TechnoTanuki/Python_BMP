554bc7d5af336884ca279a9dbfe811caf74da442
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 16:28:20 +0800

    making code samples consistent

--- Hello_BezierCurve.py
+++ Hello_BezierCurve.py
@@ -1,26 +1,51 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+      Hello Bezier Curve Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        regpolygonvert,
+        beziercurve,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get path of this script
-        bmp=b.newBMP(250,250,4) # 250 x 250  16 color bitmap
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
-        polyrad=80 # radius o ciclethat circumscribes the regular polygon
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = newBMP(250, 250, 4) # 250 x 250  16 color bitmap
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        polyrad=80 # radius or circle that circumscribes the regular polygon
         sides=5 # a pentagon
-        degrot=0 # no rotation
-        controlpointlist=b.regpolygonvert(x,y,polyrad,sides,degrot) #list of (x,y) uint
-        penradius=3 # uint radius of pen in pixels
-        color=12 # unit color
-        b.beziercurve(bmp,controlpointlist,penradius,color)
-        file='HelloBezierCurve.bmp' #file name
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        degrot=30 # no rotation
+        controlpointlist = \
+                regpolygonvert(x, y, polyrad,
+                               sides, degrot)
+        penradius = 3 # uint radius of pen in pixels
+        color = 12 # unit color
+        beziercurve(bmp, controlpointlist,
+                penradius, color)
+        file = 'HelloBezierCurve.bmp' #file name
+        saveBMP(file,bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
+
