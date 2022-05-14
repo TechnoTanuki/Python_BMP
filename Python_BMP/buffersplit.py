0915cd0fec3c26e1d50e8cd52193342f6c7c2862
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-15 01:11:49 +0800

    new header and minor edits

--- Python_BMP/buffersplit.py
+++ Python_BMP/buffersplit.py
@@ -1,10 +1,21 @@
-#/--------------------------------------------------------------------------\
-#|    Copyright 2022 by Joel C. Alcarez    [joelalcarez1975@gmail.com]      |
-#|    We make absolutely no warranty of any kind, expressed or implied.     |
-#\--------------------------------------------------------------------------/
+# -----------------------------------
+#| Copyright 2022 by Joel C. Alcarez |
+#| [joelalcarez1975@gmail.com]       |
+#|-----------------------------------|
+#|    We make absolutely no warranty |
+#| of any kind, expressed or implied |
+#|-----------------------------------|
+#|   Contact primary author          |
+#|   if you plan to use this         |
+#|   in a commercial product at      |
+#|   joelalcarez1975@gmail.com       |
+# -----------------------------------
+
 
 def altsplitbuf(buf: list) -> list:
-    """Split a list into two with alternating items in different list
+    """Split a list into two
+    with alternating items
+    in different lists
 
     Args:
         buf: list,array or tuple
@@ -16,40 +27,57 @@ def altsplitbuf(buf: list) -> list:
     m = len(buf)
     if m % 2 == 0:
         m -= m & 1
-    return [buf[0:m-1:2], buf[1:m:2]]
+    return [buf[0: m - 1: 2],
+            buf[1: m: 2]]
 
 
-def altsplitbuf3way(buf: list) -> list:
-    """Split a list into three with alternating items in different list
+def altsplitbuf3way(
+        buf: list) -> list:
+    """Split a list into three
+    with alternating items
+    in different lists
 
     Args:
-        buf: list,array or tuple [red,green,blue,....]
+        buf: list, array or tuple
+             [red,green,blue,....]
 
     Returns:
-        [[red list],[green list],[blue list]]
+        [[red list],[green list],
+         [blue list]]
 
     """
     m = len(buf)
     if m % 3 == 0:
         m -= m & 1
-    return [buf[0:m-2:3], buf[1:m-1:3], buf[2:m:3]]
+    return [buf[0: m - 2: 3],
+            buf[1: m - 1: 3],
+            buf[2: m: 3]]
 
 
-def altsplitbufnway(buf: list, n: int) -> list:
-    """Split a list into n with alternating items in different list
+def altsplitbufnway(
+        buf: list,
+          n: int) -> list:
+    """Split a list into n
+    with alternating items
+    in different lists
 
     Args:
-        buf: list,array or tuple [red,green,blue,....]
+        buf: list, array or tuple
+             [red,green,blue,....]
 
     Returns:
-        [[red list],[green list],[blue list]]
+        [[red list],[green list],
+        [blue list]]
 
     """
-    retval, m, j, i = [], len(buf), n-1, 0
+    retval = []
+    m = len(buf)
+    j = n - 1
+    i = 0
     if m % n == 0:
         m -= m & 1
     while i < n:
-        retval += [buf[i:m-j:n]]
+        retval += [buf[i: m - j: n]]
         i += 1
         j -= 1
     return retval
