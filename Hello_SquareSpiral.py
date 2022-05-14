50c089d30186220a75dc1f32fea800135be35581
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 20:28:05 +0800

    make code samples consistent

--- Hello_SquareSpiral.py
+++ Hello_SquareSpiral.py
@@ -1,28 +1,51 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+      Hello Square Spiral Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as b,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        plotlines,
+        spiralcontrolpointsvert,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get script path
-        mx,my=250,250 # bitmap size
-        bmp=b.newBMP(mx,my,4) # 16 color
-        (x,y)=b.centercoord(bmp) # How to get center of the bitmap
-        file='HelloSquareSpiral.bmp' # file name
-        step=5 # pixel interval between spiral turn
-        growthfactor=1 # greater than 1 means exponential spiral
-        turns=13 # number of turns of spiral
-        vertlist = b.spiralcontrolpointsvert(x,y,step,growthfactor,turns) # list of (x,y) control points
-        color=9
-        b.plotlines(bmp,vertlist,color) # connect the dots with lines
-        b.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = my = 250 # bitmap size
+        bmp = newBMP(mx, my, 4) # 16 color
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        file = 'HelloSquareSpiral.bmp' # file name
+        step = 5 # pixel interval between spiral turn
+        growthfactor = 1 # greater than 1 means exponential spiral
+        turns = 13 # number of turns of spiral
+        vertlist = spiralcontrolpointsvert(x, y,
+                        step, growthfactor,
+                        turns) # list of (x,y) control points
+        color = 9
+        plotlines(bmp, vertlist, color) # connect the dots with lines
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
