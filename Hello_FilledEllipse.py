628375de44f36afa6ab5d0412793979864fbe5c1
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-14 09:28:15 +0800

    sample code consistency

--- Hello_FilledEllipse.py
+++ Hello_FilledEllipse.py
@@ -1,25 +1,43 @@
-#/--------------------------------------------------------------\
-#| Copyright 2022 by Joel C. Alcarez /joelalcarez1975@gmail.com |
-#| This graphics library outputs to a bitmap file               |
-#\--------------------------------------------------------------/
+notice = """
+      Hello Filled Ellipse Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
 
-import Python_BMP.BITMAPlib as bm,subprocess as proc
-from os import path,sys
-        
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        centercoord,
+        filledellipse,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
 def main():
-        rootdir=path.abspath(sys.path[0]) # get pathof this script
-        bmp=bm.newBMP(300,200,4) # 16 color 300 x 200 bmp
-        (x,y)=bm.centercoord(bmp) # How to get center of the bitmap
-        b=y # b axis = y
-        a=x # a axis = x
-        color=13
-        bm.filledellipse(bmp,x,y,b,a,color) # all unsigned ints
-        file='HelloFilledEllipse.bmp' #file name
-        bm.saveBMP(file,bmp) # save file
-        print('\nAll done close mspaint to finish')
-        ret =proc.call('mspaint '+file) # replace with another editor if Unix
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = newBMP(300, 200, 4) # 16 color 300 x 200 bmp
+        (a, b) = (x, y) = centercoord(bmp) # How to get center of the bitmap
+        color = 10 # set color
+        filledellipse(bmp,x, y, b, a, color) # all unsigned ints
+        file = 'HelloFilledEllipse.bmp' #file name
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
-if __name__=="__main__": 
+if __name__=="__main__":
         main()
 
 
