2131ba6153023657f3283d1672e9f1d9fe93a605
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-15 17:29:58 +0800

    new header + delete duplicate functions

--- Python_BMP/fractals.py
+++ Python_BMP/fractals.py
@@ -5,26 +5,10 @@
 #|    We make absolutely no warranty |
 #| of any kind, expressed or implied |
 #|-----------------------------------|
-#|       The primary author and any  |
-#| any subsequent code contributors  |
-#| shall not be liable in any event  |
-#| for  incidental or consequential  |
-#| damages  in connection with,  or  |
-#| arising out from the use of this  |
-#| code in current form or with any  |
-#| modifications.                    |
-#|-----------------------------------|
 #|   Contact primary author          |
 #|   if you plan to use this         |
 #|   in a commercial product at      |
 #|   joelalcarez1975@gmail.com       |
-#|-----------------------------------|
-#|   Educational or hobby use is     |
-#|   highly encouraged...            |
-#|   have fun coding !               |
-#|-----------------------------------|
-#|   This graphics library outputs   |
-#|   to a bitmap file.               |
 # -----------------------------------
 
 
--- Python_BMP/inttools.py
+++ Python_BMP/inttools.py
@@ -1,11 +1,23 @@
-#/--------------------------------------------------------------------------\
-#|    Copyright 2022 by Joel C. Alcarez    [joelalcarez1975@gmail.com]      |
-#|    We make absolutely no warranty of any kind, expressed or implied.     |
-#\--------------------------------------------------------------------------/
+# -----------------------------------
+#| Copyright 2022 by Joel C. Alcarez |
+#| [joelalcarez1975@gmail.com]       |
+#|-----------------------------------|
+#|    We make absolutely no warranty |
+#| of any kind, expressed or implied |
+#|-----------------------------------|
+#|   Contact primary author          |
+#|   if you plan to use this         |
+#|   in a commercial product at      |
+#|   joelalcarez1975@gmail.com       |
+# -----------------------------------
+
 from array import array
 
 
-def writeint(offset: int, cnt: int, arr: array, value: int):
+def writeint(offset: int,
+                cnt: int,
+                arr: array,
+              value: int):
     j = cnt - 1
     for i in range(0, j):
         b = value & 0xff
@@ -13,15 +25,19 @@ def writeint(offset: int, cnt: int, arr: array, value: int):
         value = value >> 8
 
 
-def readint(offset: int, cnt: int, arr: int) -> int:
+def readint(offset: int,
+               cnt: int,
+               arr: int) -> int:
     v, j = 0, cnt-1
     for i in range(0, j):
         v += arr[offset + i] << (i << 3)
     return v
 
 
-def int2buf(cnt: int, value: int) -> array:
-    return array('B', [(value >> (i * 8)) & 0xff for i in range(0, cnt)])
+def int2buf(cnt: int,
+          value: int) -> array:
+    return array('B', [(value >> (i * 8)) & 0xff
+                        for i in range(0, cnt)])
 
 
 def buf2int(buf: array):
@@ -29,39 +45,3 @@ def buf2int(buf: array):
     for i in range(0, j):
         v += buf[i] << (i << 3)
     return v
-
-
-def altsplitbuf3way(buf: list) -> list:
-    """Split a list into three with alternating items in different list
-
-    Args:
-        buf: list,array or tuple [red,green,blue,....]
-
-    Returns:
-        [[red list],[green list],[blue list]]
-
-    """
-    m = len(buf)
-    if m % 3 == 0:
-        m -= m & 1
-    return [buf[0:m - 2:3], buf[1:m - 1:3], buf[2:m:3]]
-
-
-def altsplitbufnway(buf: list, n: int) -> list:
-    """Split a list into n with alternating items in different list
-
-    Args:
-        buf: list,array or tuple [red,green,blue,....]
-
-    Returns:
-        [[red list],[green list],[blue list]]
-
-    """
-    retval, m, j, i = [], len(buf), n-1, 0
-    if m % n == 0:
-        m -= m & 1
-    while i < n:
-        retval += [buf[i:m - j:n]]
-        i += 1
-        j -= 1
-    return retval
