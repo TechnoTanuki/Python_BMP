9c37989e886531bb6dc494f14a69bb76b64190a1
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-16 09:33:08 +0800

    code header + code reformats

--- Python_BMP/proctimer.py
+++ Python_BMP/proctimer.py
@@ -1,19 +1,42 @@
+# -----------------------------------
+#| Copyright 2022 by Joel C. Alcarez |
+#| [joelalcarez1975@gmail.com]       |
+#|-----------------------------------|
+#|    We make absolutely no warranty |
+#| of any kind, expressed or implied |
+#|-----------------------------------|
+#|   Contact primary author          |
+#|   if you plan to use this         |
+#|   in a commercial product at      |
+#|   joelalcarez1975@gmail.com       |
+# -----------------------------------
+
 from time import process_time_ns
 
+
 def elaspedtimeinseconds(inittime):
-    return (process_time_ns()-inittime)/1000000000
-        
+    return (process_time_ns() - inittime) / \
+            1000000000
+
+
 def hhmmsselaspedtime(inittime):
-    secs,ns=divmod((process_time_ns()-inittime),1000000000)
-    mins,secs=divmod(secs,60)
-    hrs,mins=divmod(mins,60)
-    return str(hrs).zfill(2)+':'+str(mins).zfill(2)+':'+str(secs).zfill(2)+ '.'+str(ns)
-        
+    secs, ns = divmod(
+                (process_time_ns() - inittime),
+                1000000000)
+    mins, secs = divmod(secs, 60)
+    hrs, mins = divmod(mins, 60)
+    return  str(hrs).zfill(2) + ':' + \
+           str(mins).zfill(2) + ':' + \
+           str(secs).zfill(2) + '.' + str(ns)
+
+
 def functimer(func):
-    def callf(*args,**kwargs):
-        print('Applying '+func.__name__+ ' please wait...')
+    def callf(*args, **kwargs):
+        print('Applying ' + func.__name__ + \
+              ' please wait...')
         inittime=process_time_ns()
-        r=func(*args,**kwargs)
-        print("Done in: "+hhmmsselaspedtime(inittime)) 
+        r=func(*args, **kwargs)
+        print("Done in: " + \
+               hhmmsselaspedtime(inittime))
         return(r)
     return(callf)
