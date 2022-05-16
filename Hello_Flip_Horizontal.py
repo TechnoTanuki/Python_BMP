12bdaa260757188d44c4d1248c9c05e5e0cd2a49
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-16 20:47:24 +0800

    make sample code consistent

--- Hello_Flip_Horizontal.py
+++ Hello_Flip_Horizontal.py
@@ -22,13 +22,15 @@ from os import path
 
 def main():
         print(notice)
-        bmp = loadBMP(path.dirname(__file__) + \
-                '/assets/tanuki.bmp') #load to memory
+        rootdir = path.dirname(__file__)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        bmp = loadBMP(rootdir + '/assets/tanuki.bmp') #load to memory
         fliphorizontal(bmp)
         file = 'HelloFliphorzontal.bmp' #file name
         saveBMP(file, bmp) # save to file
-        print('\nAll done close mspaint to finish')
-        ret = proc.call('mspaint '+file) # replace with another editor if Unix
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(imgedt + ' ' + file) # load image in editor
 
 if __name__=="__main__":
         main()
