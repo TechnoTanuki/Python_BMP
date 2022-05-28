336dda685bce27f8a9c5d500ea17425ae9dd90d1
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-05-28 19:39:23 +0800

    add docstrings + code header edit

--- Python_BMP/inttools.py
+++ Python_BMP/inttools.py
@@ -1,15 +1,18 @@
-# -----------------------------------
-#| Copyright 2022 by Joel C. Alcarez |
-#| [joelalcarez1975@gmail.com]       |
-#|-----------------------------------|
-#|    We make absolutely no warranty |
-#| of any kind, expressed or implied |
-#|-----------------------------------|
-#|   Contact primary author          |
-#|   if you plan to use this         |
-#|   in a commercial product at      |
-#|   joelalcarez1975@gmail.com       |
-# -----------------------------------
+"""Integer buffer functions module
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   Contact primary author          |
+|   if you plan to use this         |
+|   in a commercial product at      |
+|   joelalcarez1975@gmail.com       |
+ -----------------------------------
+"""
+
 
 from array import array
 
@@ -18,6 +21,24 @@ def writeint(offset: int,
                 cnt: int,
                 arr: array,
               value: int):
+    """Writes an integer value to an
+        unsigned byte array
+
+    Args:
+        offset: uint starting offset in
+                buffer or array
+                to write to
+        cnt   : uint length of int data
+                to write
+        arr   : unsigned byte array
+                to write int data in
+        value : value of uint data to
+                write in buffer or
+                array
+
+    Returns:
+        byref unsigned byte array
+    """
     j = cnt - 1
     for i in range(0, j):
         b = value & 0xff
@@ -25,23 +46,58 @@ def writeint(offset: int,
         value = value >> 8
 
 
-def readint(offset: int,
-               cnt: int,
-               arr: int) -> int:
-    v, j = 0, cnt-1
+def readint(offset: int, cnt: int,
+                 arr: int) -> int:
+    """Reads an integer value in an
+        unsigned byte array
+
+    Args:
+        offset: uint starting offset in
+                buffer or array
+                to read from
+        cnt   : uint length of int data
+                to read
+        arr   : unsigned byte array
+                to read int data from
+
+    Returns:
+        unsigned int value
+    """
+    v = 0
+    j = cnt-1
     for i in range(0, j):
         v += arr[offset + i] << (i << 3)
     return v
 
 
-def int2buf(cnt: int,
-          value: int) -> array:
+def int2buf(cnt: int, value: int
+                     ) -> array:
+    """Converts an integer value to an
+        unsigned byte array
+
+    Args:
+        cnt   : uint length of int data
+        value : value of uint data
+
+    Returns:
+        unsigned byte array
+    """
     return array('B', [(value >> (i * 8)) & 0xff
                         for i in range(0, cnt)])
 
 
-def buf2int(buf: array):
-    v, j = 0, len(buf) - 1
+def buf2int(buf: array) -> int :
+    """Converts an unsigned byte array
+        to an integer value
+
+    Args:
+        buf: unsigned byte array
+
+    Returns:
+        unsigned int value
+    """
+    v = 0
+    j = len(buf) - 1
     for i in range(0, j):
         v += buf[i] << (i << 3)
     return v
