2506153bf29fcf89ba927405e15a8572c17b01ab
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-01 15:11:33 +0800

    another spirograph sample

---
+++ Hello_Spirograph_1.py
@@ -0,0 +1,48 @@
+notice = """
+       Hello Spirograph Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        pi,
+        centercoord,
+        plotlines,
+        spirographvert,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
+def main():
+        print(notice)
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx = my = 500 # bitmap size
+        bmp = newBMP(mx, my, 4) # 16 color
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        file = 'HelloSpirograph1.bmp' # file name
+        d = 1/120
+        lim = pi * 10 + d
+        color = 10
+        plotlines(bmp,
+                spirographvert(x, y, 200, d , lim, 1, .3),
+                color) # connect the dots with lines
+        saveBMP(file, bmp) # save file
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call(f'{imgedt} {file}')
+
+if __name__=="__main__":
+        main()
