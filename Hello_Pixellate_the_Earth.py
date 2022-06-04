eaf78c9b2a8ec7e8806f58cd5cab6bf0bcc8433e
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 20:31:21 +0800

    add docstring printing to code sample

--- Hello_Pixellate_the_Earth.py
+++ Hello_Pixellate_the_Earth.py
@@ -1,5 +1,5 @@
 notice = """
-   Hello Pixellate the world Demo
+   Hello Pixellate the World Demo
  -----------------------------------
 | Copyright 2022 by Joel C. Alcarez |
 | [joelalcarez1975@gmail.com]       |
@@ -14,7 +14,7 @@ notice = """
 
 from Python_BMP.BITMAPlib import(
         loadBMP,
-        pixelizenxn,
+        pixelizenxn as f,
         saveBMP
         )
 
@@ -24,11 +24,13 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         bmp = loadBMP(f'{rootdir}/assets/earth.bmp')
-        bmp = pixelizenxn(bmp, 4) # 4x4 pix unit to blend for blur effect
-        file = 'HelloEarthPixels.bmp' # file name
+        bmp = f(bmp, 4) # 4x4 pix unit to blend for blur effect
+        file = f'Hello{fname}.bmp' # file name
         saveBMP(file, bmp) # save in-memory bitmap to file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
