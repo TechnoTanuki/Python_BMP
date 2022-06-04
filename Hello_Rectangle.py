7c418b1e057fa95a1dfdff25fbd2084649260ef6
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 20:27:16 +0800

    add docstring printing to sample

--- Hello_Rectangle.py
+++ Hello_Rectangle.py
@@ -13,7 +13,7 @@ notice = """
 """
 from Python_BMP.BITMAPlib import(
         newBMP,
-        rectangle,
+        rectangle as f,
         saveBMP
         )
 
@@ -22,17 +22,21 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        x, y = 320, 200 # bitmap size
+        x, y = 220, 200 # bitmap size
         bmp = newBMP(x, y, 4) # 2^4 = 16 colors -> black background by default
         j = 19 # border
-        color = 10 # change this 0 to 15 -> if 4 bits... 0 black thou lmao
         x1 = y1 = j # define top left (x,y)
         x2, y2 = x - j, y - j # define bottom right (x,y)
-        rectangle(bmp, x1, y1, x2, y2, color) # draw a rectangle
-        file='HelloRectangle.bmp' #file name
-        saveBMP(file,bmp) # we dump the byte array bmp -> file (so simple)
+        for c in range(1, 16):
+                d = c * 5
+                f(bmp, x1 + d, y1 + d,
+                       x2 - d, y2 - d, c) # draw a rectangle
+        file = f'Hello{fname}.bmp' #file name
+        saveBMP(file, bmp) # we dump the byte array bmp -> file (so simple)
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
         ret = proc.call([imgedt, file])
