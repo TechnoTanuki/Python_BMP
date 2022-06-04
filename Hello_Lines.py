ce7c7b4cad4531335b3076e74c320e8560f856f6
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 11:46:01 +0800

    print docstring in sample code

--- Hello_Lines.py
+++ Hello_Lines.py
@@ -15,7 +15,7 @@ from Python_BMP.BITMAPlib import(
         newBMP,
         bottomrightcoord,
         centercoord,
-        line,
+        line as f,
         saveBMP
 )
 
@@ -24,6 +24,8 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames[0:6]}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) #get path of running script
         mx = my= 300 # bitmap size
@@ -31,18 +33,18 @@ def main():
         (mx, my) = bottomrightcoord(bmp) #max-1 for screen
         (cx, cy) = centercoord(bmp) # How to get center of the bitmap
         #Python_BMP.BITMAPlib.line(bmp bytearray,x1 int,y1 int,x2 int, y2 int,color int) all unsigned
-        line(bmp,0,0,mx,my,15)
-        line(bmp,mx,0,0,my,14)
-        line(bmp,0,0,mx,0,13)
-        line(bmp,0,0,0,my,11)
-        line(bmp,mx,my,mx,0,10)
-        line(bmp,mx,my,0,my,9)
-        line(bmp,cx,cy,cx,0,8)
-        line(bmp,cx,cy,mx,cy,7)
-        line(bmp,cx,cy,cx,my,6)
-        line(bmp,cx,cy,0,cy,5)
-        file = 'HelloLines.bmp' # file name
-        saveBMP(file,bmp) # save file
+        f(bmp, 0, 0, mx, my, 15)
+        f(bmp, mx, 0, 0, my, 14)
+        f(bmp, 0, 0, mx, 0, 13)
+        f(bmp, 0, 0, 0, my, 11)
+        f(bmp, mx, my, mx, 0, 10)
+        f(bmp, mx, my, 0, my, 9)
+        f(bmp, cx, cy, cx, 0, 8)
+        f(bmp, cx, cy, mx, cy, 7)
+        f(bmp, cx, cy, cx, my, 6)
+        f(bmp, cx, cy, 0, cy, 5)
+        file = f'Hello{fname}.bmp' # file name
+        saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user something happened
         ret = proc.call([imgedt, file])
