f7acfb8860fa411a4f9e5d776afcd9b3701cb036
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 18:11:32 +0800

    add docstring to sample code

--- Hello_Mirror_Bottom.py
+++ Hello_Mirror_Bottom.py
@@ -14,7 +14,7 @@ Mirror the bottom part of a BMP Demo
 
 from Python_BMP.BITMAPlib import(
         loadBMP,
-        mirrorbottom,
+        mirrorbottom as f,
         saveBMP
         )
 
@@ -24,11 +24,13 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         bmp = loadBMP(f'{rootdir}/assets/tanuki.bmp')
-        mirrorbottom(bmp)
-        file = 'HelloMirrorBottom.bmp' #file name
+        f(bmp) # call mirrorbottom
+        file = f'Hello{fname}.bmp' #file name
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
