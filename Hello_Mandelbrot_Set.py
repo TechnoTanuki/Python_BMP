2dc6dc61b03a7da5786f1caa68c479db434f6423
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 18:41:39 +0800

    add docstring printing  to sample code

--- Hello_Mandelbrot_Set.py
+++ Hello_Mandelbrot_Set.py
@@ -14,7 +14,7 @@ notice = """
 from Python_BMP.BITMAPlib import(
         newBMP, #insert other func
         getRGBfactors,
-        mandelbrot,
+        mandelbrot as f,
         mandelparamdict,
         saveBMP #here
 )
@@ -24,17 +24,19 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames[0:8]}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) #get path of running script
-        mx = my = 600 # square canvas
+        mx = my = 300 # square canvas
         bmp = newBMP(mx, my, 24) # RGB bitmap 500 x 500
-        cf= getRGBfactors() #color info
+        cf = getRGBfactors() #color info
         mandelpar = mandelparamdict() # common parameters
         #mandelbrot(bmp,x1,y1,x2,y2,mandelpar[],RGBfactors,maxiter)
-        mandelbrot(bmp, 0, 0, mx, my,
-                mandelpar['maxeqdim'],
-                cf['brightgreen'], 255)
-        file = 'mandelbrot.bmp' # random file name
+        f(bmp, 0, 0, mx, my,
+          mandelpar['maxeqdim'],
+          cf['yellow'], 255)
+        file = f'hello{fname}.bmp' # random file name
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user something happened
