ea22bc2345c73b70ec6b2276e0c84fd1d63598d4
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 18:08:59 +0800

    add docstring to sample code

--- Hello_TV_Scanlines.py
+++ Hello_TV_Scanlines.py
@@ -14,7 +14,7 @@ notice = """
 
 from Python_BMP.BITMAPlib import(
         loadBMP,
-        erasealternatehorizontallines,
+        erasealternatehorizontallines as f,
         saveBMP
         )
 
@@ -24,16 +24,17 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames[0:3]}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         bmp = loadBMP(f'{rootdir}/assets/earth.bmp')
         eraseeverynline, eraseNthline = 4, 3 # control line erase
         #if i%int_eraseeverynline==int_eraseNthline: bmp[s2:s2+bufsize]=blank
         bytepattern = 0 # byte pattern for line erase
-        erasealternatehorizontallines(
-                bmp, eraseeverynline,
-                eraseNthline, bytepattern)
-        file = 'HelloTVEarth.bmp' #file name
+        f(bmp, eraseeverynline,
+          eraseNthline, bytepattern)
+        file = f'Hello{fname}.bmp' #file name
         saveBMP(file, bmp)
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
