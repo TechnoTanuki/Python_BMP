7a64aadceb0834bb90a8557167860fb827c2cb54
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 12:25:58 +0800

    add docstring printing to sample code

--- Hello_3D_surfaceplot.py
+++ Hello_3D_surfaceplot.py
@@ -19,7 +19,7 @@ from Python_BMP.BITMAPlib import(
         plot3Dsolid,
         rotvec3D,
         saveBMP,
-        surfplot3Dvertandsurface,
+        surfplot3Dvertandsurface as f,
         )
 
 import subprocess as proc
@@ -28,10 +28,12 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames[0:6]}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         mx = my = 500
-        file = 'Hello_3D_surfplot.bmp'
+        file = f'Hello{fname}.bmp'
         bmp = newBMP(mx, my, 24)
         cenpt = centercoord(bmp) # bitmap dependent coords
         cf = getRGBfactors() # color info
@@ -41,13 +43,12 @@ def main():
         fnxy = lambda x, y: sin(x / 50 * _2pi ) * 4 + \
                             cos(y / 50 * _2pi ) * 4
         plot3Dsolid(bmp,
-                surfplot3Dvertandsurface (
-                -50, -50, 50, 50, 5, fnxy),
-                  True, cf['brightcyan'],
-                  True, 0, rotvec3D(30, 27, 30),
-                  tvect, d, cenpt)
+                f(-50, -50, 50, 50, 5, fnxy),
+                True, cf['brightcyan'],
+                True, 0, rotvec3D(30, 27, 30),
+                tvect, d, cenpt)
 
-        saveBMP(file,bmp) # dump bytes to file
+        saveBMP(file, bmp) # dump bytes to file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
         ret = proc.call([imgedt, file])
