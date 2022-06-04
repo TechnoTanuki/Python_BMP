2c8eca8ce73f337357d6099f40f5573accd38942
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 13:08:02 +0800

    add docstring printing to sample code

--- Hello_BezierCurve.py
+++ Hello_BezierCurve.py
@@ -16,7 +16,7 @@ from Python_BMP.BITMAPlib import(
         newBMP,
         centercoord,
         regpolygonvert,
-        beziercurve,
+        beziercurve as f,
         saveBMP
         )
 
@@ -26,22 +26,23 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames[0:4]}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         bmp = newBMP(250, 250, 4) # 250 x 250  16 color bitmap
         (x, y) = centercoord(bmp) # How to get center of the bitmap
-        polyrad=80 # radius or circle that circumscribes the regular polygon
-        sides=5 # a pentagon
-        degrot=30 # no rotation
-        controlpointlist = \
+        polyrad = 80 # radius or circle that circumscribes the regular polygon
+        sides = 5 # a pentagon
+        degrot = 30 # no rotation
+        ctrlpntlst = \
                 regpolygonvert(x, y, polyrad,
                                sides, degrot)
         penradius = 3 # uint radius of pen in pixels
         color = 12 # unit color
-        beziercurve(bmp, controlpointlist,
-                penradius, color)
-        file = 'HelloBezierCurve.bmp' #file name
-        saveBMP(file,bmp) # save file
+        f(bmp, ctrlpntlst, penradius, color)
+        file = f'Hello{fname}.bmp' #file name
+        saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
         ret = proc.call([imgedt, file])
