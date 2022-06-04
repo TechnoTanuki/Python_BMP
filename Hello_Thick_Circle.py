e86762ff6d636373f2607e99770f48156c75665b
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 20:19:14 +0800

    add docstring printing to sample code

--- Hello_Thick_Circle.py
+++ Hello_Thick_Circle.py
@@ -15,7 +15,7 @@ notice = """
 from Python_BMP.BITMAPlib import(
         newBMP,
         centercoord,
-        thickcircle,
+        thickcircle as f,
         saveBMP
         )
 
@@ -25,17 +25,19 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames[0:5]}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         mx = my = 256 # bitmap size y = x square bitmap
         bmp = newBMP(mx, my, 4) # 4 bit = 16 color
         (x, y) = centercoord(bmp) # How to get center of the bitmap
-        r=x-12  # set radius = x - 12
-        color = 10 # set the color
-        penradius = 10 # control the thickness
-        thickcircle(bmp, x, y, r, penradius, color) # all unsigned
+        r = x - 20  # set radius = x - 12
+        color = 6 # set the color
+        penradius = 20 # control the thickness
+        f(bmp, x, y, r, penradius, color) # all unsigned
         #Python_BMP.BITMAPlib.thickcircle(bmp bytyearray,x int,y int ,r int,penradius int,color int)
-        file = 'HelloThickCircle.bmp' #file name
+        file = f'Hello{fname}.bmp' #file name
         saveBMP(file, bmp) #dump the bytearray to disk
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
