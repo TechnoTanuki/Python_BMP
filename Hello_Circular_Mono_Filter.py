a6b2cc15f89d431782cc7f5902eacb743b507fde
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 16:18:53 +0800

    add docstring printout to sample code

--- Hello_Circular_Mono_Filter.py
+++ Hello_Circular_Mono_Filter.py
@@ -1,5 +1,5 @@
 notice = """
-   Circular monochrome filter Demo
+   Circular Monochrome Filter Demo
  -----------------------------------
 | Copyright 2022 by Joel C. Alcarez |
 | [joelalcarez1975@gmail.com]       |
@@ -15,7 +15,7 @@ notice = """
 from Python_BMP.BITMAPlib import(
         loadBMP,
         centercoord,
-        monocircle,
+        monocircle as f,
         saveBMP
         )
 
@@ -25,14 +25,16 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         bmp = loadBMP(f'{rootdir}/assets/earth.bmp')
         (x, y) = centercoord(bmp) # How to get center of the bitmap
         r = x - 20 # radius set to x - 20
-        monocircle(bmp, x, y, r)
+        f(bmp, x, y, r) # call monocircle to apply the filter
         # Python_BMP.BITMAPlib.monocircle(bmp bytearray,x int,y int ,r int)
-        file='HelloCircularMonoFilter.bmp' #file name
+        file = f'Hello{fname}.bmp' #file name
         saveBMP(file, bmp)
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
