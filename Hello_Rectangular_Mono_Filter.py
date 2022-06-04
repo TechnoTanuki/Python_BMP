71631296da4d9bbe39f1007099899595711e725b
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 22:46:24 +0800

    add docstring printing to  sample code

--- Hello_Rectangular_Mono_Filter.py
+++ Hello_Rectangular_Mono_Filter.py
@@ -15,7 +15,7 @@ notice = """
 
 from Python_BMP.BITMAPlib import(
         loadBMP,
-        monofilterto24bitregion,
+        monofilterto24bitregion as f,
         saveBMP
         )
 
@@ -25,11 +25,13 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         bmp = loadBMP(f'{rootdir}/assets/earth.bmp')
-        monofilterto24bitregion(bmp, 30, 30, 138, 138) # monofilterto24bitregion(bmp,x1,y1,x2,y2)
-        file = 'HelloRectangularMonoFilter.bmp' # file name
+        f(bmp, 30, 30, 138, 138) # monofilterto24bitregion(bmp,x1,y1,x2,y2)
+        file = f'Hello{fname}.bmp' # file name
         saveBMP(file, bmp) # save the modified earth image to a file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
