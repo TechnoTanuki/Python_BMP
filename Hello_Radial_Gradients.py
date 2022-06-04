e3b7a8c621e251a36579d2c79f13fa9079dc1885
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 21:22:12 +0800

    add docstring printing to code sample

--- Hello_Radial_Gradients.py
+++ Hello_Radial_Gradients.py
@@ -17,7 +17,7 @@ from Python_BMP.BITMAPlib import(
         newBMP,
         distance,
         centercoord,
-        plotRGBxybit,
+        plotRGBxybit as f,
         getmaxxy,
         saveBMP
         )
@@ -28,21 +28,25 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames[0:4]}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        mx, my = 1024, 768 # x and y dim of bmp
-        bmp = newBMP(mx,my,24) # (x,y,bit depth)  24 bit BMP
+        mx = 1024
+        my = 768 # x and y dim of bmp
+        bmp = newBMP(mx, my, 24) # (x,y,bit depth)  24 bit BMP
         end = getmaxxy(bmp) # get max x and y
         cen = centercoord(bmp) # get x,y of center
         ori = (0, 0) # define as origin
         for y in range(my):
                 for x in range(mx):
                         pt = (x, y) # pacc x,y to tuple
-                        r = int(distance(ori, pt)) % 256 # red gradient
-                        g = int(distance(end, pt)) % 256 # green gradient
-                        b = int(distance(cen, pt)) % 256 # blue gradient
-                        plotRGBxybit(bmp, x, y, (r, g, b)) # make a rainbow
-        file='HelloRadialGradients.bmp' #file name
+                        f(bmp, x, y,
+                        (int(distance(ori, pt)) % 256, # red gradient
+                         int(distance(end, pt)) % 256, # green gradient
+                         int(distance(cen, pt)) % 256) # blue gradient
+                         ) # make a rainbow
+        file = f'Hello{fname}.bmp' #file name
         saveBMP(file, bmp)
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
