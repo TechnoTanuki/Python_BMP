c2c40953f2fb438739193b1f4fe9488a3cc4c925
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-04 18:17:57 +0800

    add docstring printing to sample code

--- Hello_Mirror_BottomRight.py
+++ Hello_Mirror_BottomRight.py
@@ -1,5 +1,5 @@
 notice = """
-      Mirror bottom-right demo
+      Mirror bottom right demo
  -----------------------------------
 | Copyright 2022 by Joel C. Alcarez |
 | [joelalcarez1975@gmail.com]       |
@@ -14,7 +14,7 @@ notice = """
 
 from Python_BMP.BITMAPlib import(
         loadBMP,
-        mirrorbottomright,
+        mirrorbottomright as f,
         saveBMP
         )
 
@@ -23,11 +23,13 @@ from os import path
 
 def main():
         print(notice)
+        fname = f.__name__
+        print(f'def {fname}{f.__code__.co_varnames[0:1]}\n\t{f.__doc__}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         bmp = loadBMP(f'{rootdir}/assets/tanuki.bmp')
-        mirrorbottomright(bmp)
-        file = 'HelloMirrorBottomRight.bmp' # file name
+        f(bmp) # call mirrorbottomright
+        file = f'Hello{fname}.bmp' # file name
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
