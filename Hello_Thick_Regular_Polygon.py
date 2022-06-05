9bceaf1af5adbecdc077d5b5a39e18a4bee1d502
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-05 22:26:37 +0800

    make sample code moar interesting + metadata fn

--- Hello_Thick_Regular_Polygon.py
+++ Hello_Thick_Regular_Polygon.py
@@ -15,30 +15,28 @@ from Python_BMP.BITMAPlib import(
         newBMP,
         regpolygonvert,
         thickplotpoly as f,
+        getfuncmetastr as meta,
         saveBMP
         )
 
 import subprocess as proc
 from os import path
 
+
 def main():
-        print(notice)
-        fname = f.__name__
-        print(f'def {fname}{f.__code__.co_varnames[0:4]}\n\t{f.__doc__}')
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        mx = my = 200 # bitmap size
+        mx = my = 360 # bitmap size
         bmp =  newBMP(mx, my, 4) # 2^4=16 color bitmap black background
-        x = y = mx // 2 # centerpoint
-        r = x - 20 # radius of a circle that contains all the vertices
-        sides = 5 # for a pentagon
-        angle = 30 # for rotation in degrees
-        polygonvertexlist = \
-                regpolygonvert(x, y, r, sides, angle) # generate vertices
-        color = 11 # color in 4 bit mode (min 0 - max 15)
-        penradius = 5 # radius of pen
-        f(bmp, polygonvertexlist, penradius, color)  # plot the polygon
-        file = f'Hello{fname}.bmp' # file name
+        x = y = mx >> 1 # centerpoint
+        r = x - 20
+        for c in range(1, 16):# plot the polygons
+                f(bmp, regpolygonvert(
+                x, y, r, 5, 30), 3, c)
+                r -= 10
+
+        file = f'Hello{f.__name__}.bmp' # file name
         saveBMP(file, bmp) # save the bitmap
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
