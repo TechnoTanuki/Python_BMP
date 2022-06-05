bce46acc6ebc628277f8c0423cc81f3a3da53dcf
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-05 21:14:07 +0800

    make the sample code moar interesting+metadata fn

--- Hello_FilledCircle.py
+++ Hello_FilledCircle.py
@@ -15,27 +15,31 @@ from Python_BMP.BITMAPlib import(
         newBMP,
         centercoord,
         filledcircle as f,
+        getfuncmetastr as meta,
         saveBMP
-)
+        )
 
 import subprocess as proc
 from os import path
 
 
 def main():
-        print(notice)
-        fname = f.__name__
-        print(f'def {fname}{f.__code__.co_varnames[0:5]}\n\t{f.__doc__}')
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) #get path of running script
-        my = mx = 256 # bitmap size y = x square bitmap
+        my = mx = 200 # bitmap size y = x square bitmap
         bmp = newBMP(mx, my, 4) # 4 bit = 16 color
         (x, y) = centercoord(bmp) # How to get center of the bitmap
-        r = x - 12 # radius
-        c = 11 # color
-        f(bmp, x, y, r, c) # draw filled circle
+        d = 40
+        r = d >> 1
+        for y in range(0, 4):
+                c = 4 * y
+                y1 = y * d
+                for x in range(0, 4):
+                        c += 1
+                        f(bmp, d + x * d, d + y1, r, c) # draw filled circle
         # Python_BMP.BITMAPlib.filledcircle(bmp bytearray,x int ,y int, r int, c int)
-        file = f'Hello{fname}.bmp' # some random file name
+        file = f'Hello{f.__name__}.bmp' # some random file name
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user something happened
