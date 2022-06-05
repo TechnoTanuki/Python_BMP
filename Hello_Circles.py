54c2c19f5836635b712cf6a8cb74553206a48c6f
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-05 22:46:14 +0800

    minor edit + add metadata fn

--- Hello_Circles.py
+++ Hello_Circles.py
@@ -16,6 +16,7 @@ from Python_BMP.BITMAPlib import(
         newBMP,
         centercoord,
         circle as f,
+        getfuncmetastr as meta,
         saveBMP
         )
 
@@ -24,19 +25,16 @@ from os import path
 
 
 def main():
-        print(notice)
-        fname = f.__name__
-        print(f'def {fname}{f.__code__.co_varnames[0:6]}\n\t{f.__doc__}')
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         mx = my = 512 # bitmap size y = x square bitmap
         bmp = newBMP(mx, my, 8) #8 bit = 256 color
         (x, y) = centercoord(bmp) # How to get center of the bitmap
         for r in range(x): # increase the radius of the circle from 0 to x
-                c = r # set color equal to radius
-                f(bmp, x, y, c, c, False) # draw circles
-                        # Python_BMP.BITMAPlib.circle(bmp bytearray,x int ,y int, r int, c int,isfilled bool)
-        file = f'Hello{fname}.bmp' # file name
+                f(bmp, x, y, r, r, False) # draw circles
+        #circle(bmp bytearray, x int, y int, r int, c int, isfilled bool)
+        file = f'Hello{f.__name__}.bmp' # file name
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
