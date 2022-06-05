9e0aa8349db2d5de29211bda619d91dae2e8fe46
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-05 20:11:59 +0800

    add metadata func to sample code

--- Hello_Benzene.py
+++ Hello_Benzene.py
@@ -16,16 +16,16 @@ from Python_BMP.BITMAPlib import(
         centercoord,
         gradvert as f,
         regpolygonvert,
+        getfuncmetastr as meta,
         saveBMP
-)
+        )
 
 import subprocess as proc
 from os import path
 
+
 def main():
-        print(notice)
-        fname = f.__name__
-        print(f'def {fname}{f.__code__.co_varnames[0:5]}\n\t{f.__doc__}')
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) #get path of running script
         mx = my = 250 #bitmap size
@@ -40,7 +40,7 @@ def main():
         atomrad = 55 # make radius of carbon atoms 55 pixels
         f(bmp, polyvertlist, atomrad,
           lumrange, rgbfactors) # call gradvert
-        file = f'Hello{fname}.bmp' #file name
+        file = f'Hello{f.__name__}.bmp' #file name
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
