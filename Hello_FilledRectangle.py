c9f58878a7b699eb544cb3da903026c610daf91b
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-05 17:33:08 +0800

    add meta data func + moar interesting sample code

--- Hello_FilledRectangle.py
+++ Hello_FilledRectangle.py
@@ -25,13 +25,21 @@ def main():
         print(notice)
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) #get path of running script
-        x, y = 320, 200 # bitmap size
-        bmp= newBMP(x, y, 4) # 2^4 = 16 colors -> black background by default
-        j = 19 # border
-        color = 10 # change this 0 to 15 -> if 4 bits... 0 black thou lmao
-        x1 = y1 = j # define top left (x,y)
-        x2, y2 = x - j, y - j # define bottom right (x,y)
-        filledrect(bmp, x1, y1, x2, y2, color) # draw filled rectangle
+        x, y = 320, 320 # bitmap size
+        bmp = newBMP(x, y, 4) # 2^4 = 16 colors -> black background by default
+        c = 0
+        d = 32
+        f = 25
+        xf = x >> 2
+        yf = y >> 2
+        for y in range(0, 4):
+                c = y * 4
+                for x in range(0, 4):
+                        c += x
+                        a = f + x * xf
+                        b = f + y * yf
+                        filledrect(bmp, a, b,
+                             a + d, b + d, c) # draw filled rectangle
         file = 'HelloFilledRectangle.bmp' #file name
         saveBMP(file, bmp) # bmp <byte array> to file
         print('Saved to %s in %s\nAll done close %s to finish' % \
