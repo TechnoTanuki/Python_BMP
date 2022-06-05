b75003d2c79e55e7dd5fc342fc191eb2ab529669
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-05 19:27:33 +0800

    add metadata func to sample code

--- Hello_Icosahedron_Outline.py
+++ Hello_Icosahedron_Outline.py
@@ -18,20 +18,20 @@ from Python_BMP.BITMAPlib import(
         rotvec3D,
         plot3Dsolid,
         icosahedvertandsurface as f,
+        getfuncmetastr as meta,
         saveBMP
         )
 
 import subprocess as proc
 from os import path
 
+
 def main():
-        print(notice)
-        fname = f.__name__
-        print(f'def {fname}{f.__code__.co_varnames[0:1]}\n\t{f.__doc__}')
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         mx = my = 250 # x=y square bmp
-        file = f'Hello{fname}1.bmp' # some random file name as string
+        file = f'Hello{f.__name__}1.bmp' # some random file name as string
         bmp = newBMP(mx, my, 4) # 16 color
         cenpt = centercoord(bmp) # helper method to get center of a bitmap
         cf = getRGBfactors() # color info with presets
