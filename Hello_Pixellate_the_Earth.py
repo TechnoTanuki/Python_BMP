31c57e1905a63737e8226a5aac31e9f32c7e244c
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-05 16:26:47 +0800

    add metadata printing  to sample code

--- Hello_Pixellate_the_Earth.py
+++ Hello_Pixellate_the_Earth.py
@@ -15,22 +15,20 @@ notice = """
 from Python_BMP.BITMAPlib import(
         loadBMP,
         pixelizenxn as f,
+        getfuncmetastr as meta,
         saveBMP
         )
 
 import subprocess as proc
 from os import path
 
-
 def main():
-        print(notice)
-        fname = f.__name__
-        print(f'def {fname}{f.__code__.co_varnames}\n\t{f.__doc__}')
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         bmp = loadBMP(f'{rootdir}/assets/earth.bmp')
         bmp = f(bmp, 4) # 4x4 pix unit to blend for blur effect
-        file = f'Hello{fname}.bmp' # file name
+        file = f'Hello{f.__name__}.bmp' # file name
         saveBMP(file, bmp) # save in-memory bitmap to file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
