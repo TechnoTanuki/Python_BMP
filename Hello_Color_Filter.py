c296fcaf35384fe7de55ee23b9534f166d492d21
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-05 20:28:14 +0800

    add metadata func to sample code

--- Hello_Color_Filter.py
+++ Hello_Color_Filter.py
@@ -14,7 +14,9 @@ notice = """
 
 from Python_BMP.BITMAPlib import(
         loadBMP,
+        getX11RGBfactors as c,
         colorfilterto24bitimage as f,
+        getfuncmetastr as meta,
         saveBMP
         )
 
@@ -23,15 +25,12 @@ from os import path
 
 
 def main():
-        print(notice)
-        fname = f.__name__
-        print(f'def {fname}{f.__code__.co_varnames}\n\t{f.__doc__}')
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) #get path of running script
         bmp = loadBMP(f'{rootdir}/assets/tanuki.bmp')
-        rf, gf, bf = .3, 0.8, .5 #RGB factors 0 to 1 float unsigned
-        f(bmp, [rf, gf, bf])
-        file = f'Hello{fname}.bmp' #file name
+        f(bmp, c()['greenyellow'])
+        file = f'Hello{f.__name__}.bmp' #file name
         saveBMP(file,bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
