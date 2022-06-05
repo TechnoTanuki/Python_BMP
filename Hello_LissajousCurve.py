cbf4c63d4d1d79e9e1a849bc4adb8469c57b913c
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-06 04:41:46 +0800

    add metadata func to sample code

--- Hello_LissajousCurve.py
+++ Hello_LissajousCurve.py
@@ -18,6 +18,7 @@ from Python_BMP.BITMAPlib import(
         centercoord,
         plotlines,
         lissajouscurvevert as f,
+        getfuncmetastr as meta,
         saveBMP
         )
 
@@ -26,25 +27,22 @@ from os import path
 
 
 def main():
-        print(notice)
-        fname = f.__name__
-        print(f'def {fname}{f.__code__.co_varnames}\n\t{f.__doc__}')
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         mx = my = 500 # bitmap size
         bmp = newBMP(mx, my, 4) # 16 color
         (x, y) = centercoord(bmp) # How to get center of the bitmap
-        file = f'Hello{fname}.bmp' # file name
+        file = f'Hello{f.__name__}.bmp' # file name
         d = 1/120
         lim = pi * 25 + d
         color = 10
-        plotlines(bmp,
-                f(x, y, # position the curve
+        plotlines(bmp, f(x, y, # position the curve
                  200, 200, # axis mulitipliers
                  1/3, .25, # frequency multipliers
                  pi / 2, # phase shift
-                d, lim), # angle increment + limit
-                color) # connect the dots with lines
+                 d, lim), # angle increment + limit
+                 color) # connect the dots with lines
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
