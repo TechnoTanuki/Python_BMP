83bdbcbd313efb4fff902d7e28d4ae5e73f490bb
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-05 22:39:40 +0800

    make sample code moar interesting + metadata func

--- Hello_Lines.py
+++ Hello_Lines.py
@@ -14,36 +14,27 @@ notice = """
 from Python_BMP.BITMAPlib import(
         newBMP,
         bottomrightcoord,
-        centercoord,
         line as f,
+        getfuncmetastr as meta,
         saveBMP
-)
+        )
 
 import subprocess as proc
 from os import path
 
+
 def main():
-        print(notice)
-        fname = f.__name__
-        print(f'def {fname}{f.__code__.co_varnames[0:6]}\n\t{f.__doc__}')
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) #get path of running script
-        mx = my= 300 # bitmap size
-        bmp = newBMP(mx,my,4) # 4 bit = 16 color
-        (mx, my) = bottomrightcoord(bmp) #max-1 for screen
-        (cx, cy) = centercoord(bmp) # How to get center of the bitmap
-        #Python_BMP.BITMAPlib.line(bmp bytearray,x1 int,y1 int,x2 int, y2 int,color int) all unsigned
-        f(bmp, 0, 0, mx, my, 15)
-        f(bmp, mx, 0, 0, my, 14)
-        f(bmp, 0, 0, mx, 0, 13)
-        f(bmp, 0, 0, 0, my, 11)
-        f(bmp, mx, my, mx, 0, 10)
-        f(bmp, mx, my, 0, my, 9)
-        f(bmp, cx, cy, cx, 0, 8)
-        f(bmp, cx, cy, mx, cy, 7)
-        f(bmp, cx, cy, cx, my, 6)
-        f(bmp, cx, cy, 0, cy, 5)
-        file = f'Hello{fname}.bmp' # file name
+        mx = my = 512 # bitmap size
+        bmp = newBMP(mx, my, 8) # 8 bit = 256 color
+        (mx, my) = bottomrightcoord(bmp)
+        my >>= 1
+        for y in range(0, my):
+                l = y * 2
+                f(bmp, 0, l, mx, l, y) # line(bmp bytearray,x1 int,y1 int,x2 int, y2 int,color int) all unsigned
+        file = f'Hello{f.__name__}.bmp' # file name
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user something happened
