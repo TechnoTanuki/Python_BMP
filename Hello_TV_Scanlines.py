1772fb97e553440a278dce351e457ad04e079d40
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-05 18:19:40 +0800

    add metadata func to sample code

--- Hello_TV_Scanlines.py
+++ Hello_TV_Scanlines.py
@@ -15,6 +15,7 @@ notice = """
 from Python_BMP.BITMAPlib import(
         loadBMP,
         erasealternatehorizontallines as f,
+        getfuncmetastr as meta,
         saveBMP
         )
 
@@ -23,9 +24,7 @@ from os import path
 
 
 def main():
-        print(notice)
-        fname = f.__name__
-        print(f'def {fname}{f.__code__.co_varnames[0:3]}\n\t{f.__doc__}')
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         bmp = loadBMP(f'{rootdir}/assets/earth.bmp')
@@ -34,7 +33,7 @@ def main():
         bytepattern = 0 # byte pattern for line erase
         f(bmp, eraseeverynline,
           eraseNthline, bytepattern)
-        file = f'Hello{fname}.bmp' #file name
+        file = f'Hello{f.__name__}.bmp' #file name
         saveBMP(file, bmp)
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
