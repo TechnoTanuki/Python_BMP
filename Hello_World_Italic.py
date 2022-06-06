fad9383bd79201d441c86c9193e7561e91adb110
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-06 23:59:48 +0800

    add metadata func to sample code

--- Hello_World_Italic.py
+++ Hello_World_Italic.py
@@ -14,9 +14,10 @@ notice = """
 
 from Python_BMP.BITMAPlib import(
         newBMP,
-        plotitalicstring,
+        plotitalicstring as f,
         getcolorname2RGBdict,
         font8x14,
+        getfuncmetastr as meta,
         saveBMP
         )
 
@@ -25,21 +26,20 @@ from os import path
 
 
 def main():
-        print(notice)
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         mx, my = 600, 80 # bitmap size
         bmp = newBMP(mx, my, 24) # RGB bitmap
-        plotitalicstring(bmp,
-                50, 10, # position the text
-                'Hello World!!', # random text
-                5, # uint font size multiplier
-                1, # uint space between pixels
-                0, # uint default space between char
-                getcolorname2RGBdict()['yellow'],
-                font8x14)
-        file = 'HelloWorldItalic.bmp' #file name
-        saveBMP(file,bmp)
+        f(bmp, 50, 10, # position the text
+          'Hello World!!', # random text
+           5, # uint font size multiplier
+           1, # uint space between pixels
+           0, # uint default space between char
+           getcolorname2RGBdict()['yellow'],
+           font8x14)
+        file = f'HelloWorld{f.__name__}.bmp' #file name
+        saveBMP(file, bmp)
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
         ret = proc.call([imgedt, file])
