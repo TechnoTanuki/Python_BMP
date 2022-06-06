9875388767f3e9281c63ad6d38d38884b4a138c3
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-06 11:10:59 +0800

    add metadata func to sample code

--- Hello_World_rainbowreversedItalic.py
+++ Hello_World_rainbowreversedItalic.py
@@ -1,5 +1,5 @@
 notice = """
- Hello World Reversed rainbow Italic
+ Hello World Reversed Rainbow Italic
  -----------------------------------
 | Copyright 2022 by Joel C. Alcarez |
 | [joelalcarez1975@gmail.com]       |
@@ -14,9 +14,10 @@ notice = """
 
 from Python_BMP.BITMAPlib import(
         newBMP,
-        plotreverseditalicstring,
+        plotreverseditalicstring as f,
         getcolorname2RGBdict,
         font8x8,
+        getfuncmetastr as meta,
         saveBMP
         )
 
@@ -25,29 +26,27 @@ from os import path
 
 
 def main():
-        print(notice)
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         mx, my = 1000, 90 # bitmap size
-        bmp = newBMP(mx,my,24) # RGB bitmap
+        bmp = newBMP(mx, my, 24) # RGB bitmap
         c = getcolorname2RGBdict()
-        colors = (c['brightred'],
-                  c['brightorange'],
-                  c['brightyellow'],
-                  c['brightgreen'],
-                  c['cyan'],
-                  c['brightblue'],
-                  c['brightmagenta'])
-        plotreverseditalicstring(bmp,
-                25, 10, # position the text
-                'Hello World!!', # random text
-                9, # uint font size multiplier
-                1, # uint space between pixels
-                0, # uint default space between char
-                colors,
-                font8x8)
-        file = 'HelloWorldrainbowrevital.bmp' #file name
-        saveBMP(file,bmp)
+        f(bmp, 25, 10, # position the text
+         'Hello World!!', # random text
+          9, # uint font size multiplier
+          1, # uint space between pixels
+          0, # uint default space between char
+          (c['brightred'],
+           c['brightorange'],
+           c['brightyellow'],
+           c['brightgreen'],
+           c['cyan'],
+           c['brightblue'],
+           c['brightmagenta']),
+          font8x8)
+        file = f'HelloWorld{f.__name__}rainbow.bmp' #file name
+        saveBMP(file, bmp)
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
         ret = proc.call([imgedt, file])
