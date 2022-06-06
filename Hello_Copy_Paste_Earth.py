1e8c4cdf13b1a1d18281992cd21175c9c6b0be15
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-06 22:52:35 +0800

    add metadata func to sample code

--- Hello_Copy_Paste_Earth.py
+++ Hello_Copy_Paste_Earth.py
@@ -13,22 +13,24 @@ notice = """
 """
 from Python_BMP.BITMAPlib import(
         loadBMP,
-        copyrect,
-        pasterect,
+        copyrect as c,
+        pasterect as p,
+        getfuncmetastr as meta,
         saveBMP
         )
 
 import subprocess as proc
 from os import path
 
+
 def main():
-        print(notice)
+        print(f'{notice}\n{meta(c)}\n{meta(p)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         bmp = loadBMP(f'{rootdir}/assets/earth.bmp')
-        buf = copyrect(bmp, 3, 3, 60, 60) # copy a rectangular region (x1,y1,x2,y2) to a buffer
-        pasterect(bmp,buf,40,40) # paste buffer to a rectangular region at (x1,y1)
-        file = 'HelloEarthCopyPaste.bmp' #file name
+        buf = c(bmp, 3, 3, 60, 60) # copy a rectangular region (x1,y1,x2,y2) to a buffer
+        p(bmp, buf, 40, 40) # paste buffer to a rectangular region at (x1,y1)
+        file = f'HelloEarth{c.__name__}{p.__name__}.bmp' #file name
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
