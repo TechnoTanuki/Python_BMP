a8aeab64d86371ea1bcab675953379604f7e2dca
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-07 08:05:36 +0800

    add metadata func to sample code

--- Hello_Elliptical_Gradient.py
+++ Hello_Elliptical_Gradient.py
@@ -14,7 +14,8 @@ notice = """
 from Python_BMP.BITMAPlib import(
         newBMP,
         centercoord,
-        gradellipse,
+        gradellipse as f,
+        getfuncmetastr as meta,
         saveBMP
         )
 
@@ -23,17 +24,17 @@ from os import path
 
 
 def main():
-        print(notice)
+        print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) #get path of running script
-        bmp = newBMP(640, 480, 24) # 24 bit 300 x 200 bmp
+        bmp = newBMP(350, 300, 24) # 24 bit 300 x 200 bmp
         (x, y) = centercoord(bmp) # How to get center of the bitmap
         b = y # b axis = y
         a = x # a axis = x
         lumrange = (255, 0) # decreasing gradient from center uint bytes
         rgbfactors = (.3, .9, .5) # rgb triplet as values 0 to 1 unsigned float
-        gradellipse(bmp, a, b, b, a, lumrange, rgbfactors)
-        file = 'HelloEllipticalGradent.bmp' #file name
+        f(bmp, a, b, b, a, lumrange, rgbfactors)  # call gradellipse
+        file = f'Hello{f.__name__}.bmp' #file name
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
