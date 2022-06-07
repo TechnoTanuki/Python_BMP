c9021bdd614ddce1ed0da7cb4f0734334122610a
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-07 14:02:56 +0800

    make sample code moar interesting + metadata print

--- Hello_Cardioid.py
+++ Hello_Cardioid.py
@@ -1,5 +1,5 @@
 notice = """
-            Cardiod demo
+        Hello Hypotrochoid Demo
  -----------------------------------
 | Copyright 2022 by Joel C. Alcarez |
 | [joelalcarez1975@gmail.com]       |
@@ -11,34 +11,41 @@ notice = """
 |   to a bitmap file.               |
  -----------------------------------
 """
+
 from Python_BMP.BITMAPlib import(
         newBMP,
+        pi,
         centercoord,
-        plotflower,
+        plotpoly as p,
+        flowervert as f,
+        getfuncmetastr as meta,
         saveBMP
         )
 
 import subprocess as proc
 from os import path
 
+
 def main():
-        print(notice)
+        print(f'{notice}\n{meta(f)}\n{meta(p)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        x = y = 256 # square bitmap
-        bmp = newBMP(x, y, 24) # (x,y,bit depth)
-        (x, y) = centercoord(bmp) # helper method to get center of a bitmap
-        r = x - 10 # radius of flower
-        petals = 1# for cardioid
-        angrot = 45 # angle of rotation in degrees
-        lumrange = (255, 240) # for brightness gradient
-        rgbfactors = (1, 1, 0) # color as rgb of ufloat 0 to 1
-        file = 'HelloCardioid.bmp' #file name
-        plotflower(bmp, x, y, r, petals, angrot,
-                lumrange, rgbfactors)
-        saveBMP(file, bmp) # dump bytearray to file
+        mx = my = 500 # bitmap size
+        bmp = newBMP(mx, my, 4) # 16 color
+        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        file = f'Hello{f.__name__}.bmp' # file name
+        r = 200
+        for c in range(1, 16):
+                p(bmp, f(x, y, # position the flower
+                  r, # radius
+                  1, # petals for cardioid
+                  100, # angle of rotation
+                  ),# angle step and limit
+                  c) # connect the dots with lines
+                r -= 8
+        saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
-                (file, rootdir, imgedt))
+                (file, rootdir, imgedt)) # tell user we are done
         ret = proc.call([imgedt, file])
 
 if __name__=="__main__":
