aa4735784f79edfcfbcf3282b9ba5c8f2bac6680
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-07 08:21:55 +0800

    add metadata func to sample code

--- Hello_Earth_Sideways_text.py
+++ Hello_Earth_Sideways_text.py
@@ -13,10 +13,11 @@ notice = """
 """
 
 from Python_BMP.BITMAPlib import(
-        loadBMP,
+        loadBMP as l,
         getcolorname2RGBdict,
-        plotstringsideway,
+        plotstringsideway as f,
         font8x14,
+        getfuncmetastr as meta,
         saveBMP
         )
 
@@ -25,21 +26,23 @@ from os import path
 
 
 def main():
-        print(notice)
+        print(f'{notice}\n{meta(l)}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        bmp = loadBMP(f'{rootdir}/assets/earth.bmp')
+        bmp = l(f'{rootdir}/assets/earth.bmp') # call loadBMP
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        bmp = l(f'{rootdir}/assets/earth.bmp')
         c = getcolorname2RGBdict() #friendly color names 2 rgb
         x = y = 150
         fontsize =2
         pixspace = 1 # space between bitmap font pixels (0 = default)
         charspace = 0 # space bitmap font characters (0 = default)
-        plotstringsideway(bmp, x, y,
-                'Hello!!!',
-                fontsize, pixspace, charspace,
-                c['brightyellow'], font8x14)
-        file = 'HelloEarthSidewayText.bmp' #file name
-        saveBMP(file,bmp) # save file
+        f(bmp, x, y, 'Hello!!!', # call plotstringsideway
+          fontsize, pixspace, charspace,
+          c['brightyellow'], font8x14)
+        file = f'Hello{l.__name__}{f.__name__}.bmp' #file name
+        saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
         ret = proc.call([imgedt, file])
