2456eeaf7cc80202ea037574703f93e035137dd7
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-07 10:14:36 +0800

    add metadata func to sample code + minor edits

--- Hello_NonLinearRadialMultichannel_Gradients.py
+++ Hello_NonLinearRadialMultichannel_Gradients.py
@@ -15,30 +15,32 @@ Non Linear Radial Multichannel Gradient
 
 from Python_BMP.BITMAPlib import(
         newBMP,
-        centercoord,
-        plotRGBxybit,
-        distance,
+        centercoord as c,
+        plotRGBxybit as p,
+        distance as d,
+        getfuncmetastr as meta,
         saveBMP
         )
 
 import subprocess as proc
 from os import path
 
+
 def main():
-        print(notice)
+        print(f'{notice}\n{meta(d)}\n{meta(c)}\n{meta(p)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        mx, my = 1024, 768 # dimensions of the bmp
+        mx, my = 800, 600 # dimensions of the bmp
         bmp = newBMP(mx, my, 24) # (x,y,bit depth) 24 bit BMP
-        cen = centercoord(bmp) # get x,y of center
-        f = lambda d: d ** 1.8 # simple non linear func try different funcs
+        cen = c(bmp) # get x,y of center
+        f = lambda d: d ** 3 # simple non linear func try different funcs
         for y in range(my): # row sweep
                 for x in range(mx): # column sweep
-                        g = int(f(distance(cen, (x, y)) * (1 / 8))) % 256 # green gradient
+                        g = int(f(d(cen, (x, y)) * (1 / 8))) % 256 # green gradient
                         b = 255 - g # blue gradient
                         r = (abs(b - g)) % 256 # red gradient
-                        plotRGBxybit(bmp, x, y, (r, g, b)) # make a rainbow
-        file = 'HelloNonLinearRadialMultichannelGradients.bmp' # file name
+                        p(bmp, x, y, (r, g, b)) # make a rainbow
+        file = f'Hello{d.__name__}{c.__name__}{p.__name__}1.bmp' # file name
         saveBMP(file, bmp) # dump bytearray to file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
