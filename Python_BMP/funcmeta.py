3cce6d7c1dc6c95036d7c20ab83afb72bc064f4b
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-09 19:04:42 +0800

    add docstring

--- Python_BMP/funcmeta.py
+++ Python_BMP/funcmeta.py
@@ -5,4 +5,12 @@ _d= '"'*3
 
 
 def getfuncmetastr(f: Callable):
+    """Gets Function Metadata in Python Code Format
+
+    Args:
+        f: function
+
+    Returns:
+        string function signature and docstring
+    """
     return f'def {f.__name__}{signature(f)}:\n    {_d}{f.__doc__}{_d}\n'
