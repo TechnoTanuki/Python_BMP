f95ae2ae618d24cbcc74f30ac45292e11779b938
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-13 23:17:04 +0800

    make sample moar interesting

--- Hello_LissajousCurve.py
+++ Hello_LissajousCurve.py
@@ -31,18 +31,18 @@ def main():
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         mx = my = 500 # bitmap size
-        bmp = newBMP(mx, my, 4) # 16 color
+        bmp = newBMP(mx, my, 8) # 256 color
         (x, y) = centercoord(bmp) # How to get center of the bitmap
         file = f'Hello{f.__name__}.bmp' # file name
         d = 1/120
         lim = pi * 25 + d
-        color = 10
-        plotlines(bmp, f(x, y, # position the curve
-                 200, 200, # axis mulitipliers
+        for c in range(10, 200, 15):
+                plotlines(bmp, f(x, y, # position the curve
+                 c, c, # axis mulitipliers
                  1/3, .25, # frequency multipliers
                  pi / 2, # phase shift
                  d, lim), # angle increment + limit
-                 color) # connect the dots with lines
+                 c) # connect the dots with lines
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
