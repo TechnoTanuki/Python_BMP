568c4cfda9a860dbb10480568bc3d6366d0d092b
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-14 22:06:48 +0800

    moar interesting sample code

--- Hello_Epicycloid.py
+++ Hello_Epicycloid.py
@@ -30,18 +30,20 @@ def main():
         print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        mx = my = 500 # bitmap size
+        mx = my = 770 # bitmap size
         bmp = newBMP(mx, my, 4) # 16 color
         (x, y) = centercoord(bmp) # How to get center of the bitmap
         file = f'Hello{f.__name__}.bmp' # file name
         d = 1/120 # angle increment
         lim = pi * 10 + d # angle limit
         color = 10
-        plotlines(bmp, f(x, y, # position the epicycloid
-                 80, # radius of fixed circle
-                 50, # radius of rolling circle
+        for c in range(1, 16):
+                j = c * 8
+                plotlines(bmp, f(x, y, # position the epicycloid
+                 160 / c, # radius of fixed circle
+                 100 / c, # radius of rolling circle
                  d, lim), # angle step and limit
-                 color) # connect the dots with lines
+                 c, 2 if c < 3 else 1) # connect the dots with lines
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
