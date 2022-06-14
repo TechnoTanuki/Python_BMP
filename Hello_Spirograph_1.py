577173b07421156694cc02f2ccf26933d730f85d
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-14 19:05:36 +0800

    updated sample

--- Hello_Spirograph_1.py
+++ Hello_Spirograph_1.py
@@ -16,8 +16,9 @@ from Python_BMP.BITMAPlib import(
         newBMP,
         pi,
         centercoord,
-        plotlines,
+        gradplotlines as g,
         spirographvert as f,
+        getX11RGBfactors as c,
         getfuncmetastr as meta,
         saveBMP
         )
@@ -27,21 +28,23 @@ from os import path
 
 
 def main():
-        print(f'{notice}\n{meta(f)}')
+        print(f'{notice}\n{meta(f)}\n{meta(g)}\n')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         mx = my = 500 # bitmap size
-        bmp = newBMP(mx, my, 4) # 16 color
+        bmp = newBMP(mx, my, 24) # RGB
         (x, y) = centercoord(bmp) # How to get center of the bitmap
-        file = f'Hello{f.__name__}1.bmp' # file name
+        file = f'Hello{f.__name__}{g.__name__}.bmp' # file name
         d = 1/120 # angle increment
         lim = pi * 10 + d # angle limit
         color = 10
-        plotlines(bmp, f(x, y, # control spirograph location
+        g(bmp, f(x, y, # control spirograph location
                   200, # control spirograph size
                   1, .3, # controls spirograph shape
                   d, lim), # angle step and limit
-                 color, 5) # connect the dots with lines
+                  7, # pen radius
+                  [255, 0], # luminosity range
+                  c()['teal']) # connect the dots with lines
         saveBMP(file, bmp) # save file
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
