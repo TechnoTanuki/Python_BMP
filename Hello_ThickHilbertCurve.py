a705dadaf54bb292044dc891e34ccc91ea342215
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-06-23 16:14:54 +0800

    simplify sample code

--- Hello_ThickHilbertCurve.py
+++ Hello_ThickHilbertCurve.py
@@ -13,11 +13,8 @@ notice = """
 """
 
 from Python_BMP.BITMAPlib import(
-        newBMP,
-        plotlines,
-        hilbertvert as f,
+        savehilbertcurve2file as f,
         getfuncmetastr as meta,
-        saveBMP
         )
 
 import subprocess as proc
@@ -29,15 +26,8 @@ def main():
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
         mx = my = 512 # bitmap size
-        bmp = newBMP(mx, my, 4) #  monochrome
         file = f'Hello{f.__name__}.bmp' # file name
-        l = [] # empty list to pass to func
-        f(l, # list is returned byref by hilbertvert
-        (0, 0), # origin point
-        (511, 0, 0, 511), # set extents and orientation
-        4) # number of recursions
-        plotlines(bmp, l, 12, 5) # plot the Hilbert curve
-        saveBMP(file, bmp) # save file
+        f(file, mx, my, 4, 4, 15, 2, 5, 1)
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
         ret = proc.call([imgedt, file])
