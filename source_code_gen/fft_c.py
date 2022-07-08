4bd6bb7a70415dd45d1074905714f15d853f70c1
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-07-08 20:43:27 +0800

    experimental pure python fft

---
+++ source_code_gen/fft_c.py
@@ -0,0 +1,94 @@
+#Factored discrete Fourier transform, or FFT, and its inverse iFFT */
+
+
+from math import sin, cos, pi
+q = 3
+N = 1 << q
+_2pi = 2 * pi
+
+"""
+   fft(v,N):
+   [0] If N==1 then return.
+   [1] For k = 0 to N/2-1, let ve[k] = v[2*k]
+   [2] Compute fft(ve, N/2);
+   [3] For k = 0 to N/2-1, let vo[k] = v[2*k+1]
+   [4] Compute fft(vo, N/2);
+   [5] For m = 0 to N/2-1, do [6] through [9]
+   [6]   Let w.re = cos(2*PI*m/N)
+   [7]   Let w.im = -sin(2*PI*m/N)
+   [8]   Let v[m] = ve[m] + w*vo[m]
+   [9]   Let v[m+N/2] = ve[m] - w*vo[m]
+"""
+
+def fft(v: list[complex], n: int, tmp: list[complex]):
+    if n > 1:
+        halfn = n >> 1
+        vo = [0] * halfn
+        ve = [0] * halfn
+        for k in range(0, halfn):
+            _2k = k << 1
+            ve[k] = v[_2k]
+            vo[k] = v[_2k + 1]
+        fft( ve, halfn, v )
+        fft( vo, halfn, v )
+        for m in range(0, halfn):
+            a = _2pi * m / n
+            w = complex(cos(a), -sin(a)) * vo[m]
+            vm = ve[m]
+            v[m] = vm + w
+            v[m + halfn] = vm - w
+
+"""
+   [0] If N==1 then return.
+   [1] For k = 0 to N/2-1, let ve[k] = v[2*k]
+   [2] Compute ifft(ve, N/2);
+   [3] For k = 0 to N/2-1, let vo[k] = v[2*k+1]
+   [4] Compute ifft(vo, N/2);
+   [5] For m = 0 to N/2-1, do [6] through [9]
+   [6]   Let w.re = cos(2*PI*m/N)
+   [7]   Let w.im = sin(2*PI*m/N)
+   [8]   Let v[m] = ve[m] + w*vo[m]
+   [9]   Let v[m+N/2] = ve[m] - w*vo[m]
+ */
+"""
+def ifft(v: list[complex], n: int,  tmp: list[complex]):
+    if n > 1:
+        halfn = n >> 1
+        vo = [0] * halfn
+        ve = [0] * halfn
+        for k in range(0, halfn):
+            _2k = k << 1
+            ve[k] = v[_2k]
+            vo[k] = v[_2k + 1]
+        ifft( ve, halfn, v )
+        ifft( vo, halfn, v )
+        for m in range(0, halfn):
+            a = _2pi * m / n
+            w = complex(cos(a), sin(a)) * vo[m]
+            vm = ve[m]
+            v[m] = vm + w
+            v[m + halfn] = vm - w
+
+
+def main():
+    v = [0] * N
+    v1 = [0] * N
+    scratch = [0] * N
+    for  k in range(0, N):
+        a = _2pi * k / N
+        v[k] = complex(0.125*cos(a), 0.125*sin(a))
+        v1[k] = complex(0.3*cos(a), -0.3*sin(a))
+
+    print("Orig", v, N)
+    fft(v, N, scratch)
+    print(" FFT", v, N)
+    ifft( v, N, scratch)
+    print("iFFT", v, N)
+
+    print("Orig", v1, N)
+    fft( v1, N, scratch)
+    print(" FFT", v1, N)
+    ifft( v1, N, scratch)
+    print("iFFT", v1, N)
+
+main()
