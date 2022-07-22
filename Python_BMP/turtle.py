1e31215053ecb7312df4e7625c3d2215b9703f43
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-07-22 10:24:06 +0800

    ongoing turtle api clone from scratch

--- Python_BMP/turtle.py
+++ Python_BMP/turtle.py
@@ -1,24 +1,56 @@
 
 from typing import Number
+_x = 0
+_y = 1
+_h = 2
+_default_state = [0, 0, 0]
+_state = [0, 0, 0]
 
-
-_position = [0, 0]
 
 def position():
-    return _position
+    return _state[0:2]
+
+
+def heading():
+    return _state[_h]
+
 
 def goto(x, y = None):
     if len(x) == 2 and type(x) == list:
-        _position = x
+         _state[0:2] = x
     else:
         if type(y) == Number:
-            _position = (x, y)
+             _state[0:2] = (x, y)
         else:
-            _position[0] = x
+            _state[_x] = x
+
+
+def setpos(x, y = None):
+    goto(x, y)
+
+
+def setposition(x, y = None):
+    goto(x, y)
+
 
 def setx(x):
-    _position[0] = x
+    _state[_x] = x
 
 
 def setx(y):
-    _position[1] = y
+    _state[_y] = y
+
+
+def setheading(to_angle):
+    _state[_h] = to_angle
+
+
+def seth(to_angle):
+    _state[_h] = to_angle
+
+def home():
+    _state = _default_state
+
+
+def circle(radius, extent=None, steps=None):
+    pass
