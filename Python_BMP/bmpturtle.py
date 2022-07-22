c4a86a859a79686ed2ac671a4140cf57e132aeb4
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-07-22 22:02:43 +0800

    use complex number as vectors

--- Python_BMP/bmpturtle.py
+++ Python_BMP/bmpturtle.py
@@ -1,9 +1,7 @@
-from turtle import distance
-from .mathlib import (subvect, vmag)
-import inspect
+
 import functools
 from typing import Number
-
+from math import degrees, radians
 
 def logaction(f):
     @functools.wraps(f)
@@ -12,69 +10,81 @@ def logaction(f):
         return f(*args, **kwargs)
     return wrapper
 
-_x = 0
-_y = 1
-_h = 2
-_cmd = 3
-_default_state = [0, 0, 0 , []]
-_state = [0, 0, 0, []]
+_p = 0
+_h = 1
+_cmd = 2
+_default_state = [complex(0, 0), 0 , []]
+_state = [complex(0, 0), 0, []]
 
 def _goto(x, y = None):
     if len(x) == 2 and type(x) == list:
-         _state[0:2] = x
+         _state[_p] = complex(x[0], x[1])
     else:
         if type(y) == Number:
-            _state[0:2] = (x, y)
+            _state[_p] = complex(x, y)
         else:
-            _state[_x] = x
+            _state[_p].real = x
+
 
 @logaction
 def position():
-    return _state[0:2]
+    return (_state[_p].real, _state[_p].imag)
+
 
 @logaction
 def heading():
     return _state[_h]
 
+
 @logaction
 def goto(x, y = None):
     _goto(x, y)
+
 
 @logaction
 def setpos(x, y = None):
     _goto(x, y)
 
+
 @logaction
 def setposition(x, y = None):
     _goto(x, y)
 
+
 @logaction
 def setx(x):
-    _state[_x] = x
+    _state[_p].real = x
+
 
 @logaction
 def setx(y):
-    _state[_y] = y
+    _state[_p].imag = y
+
 
 @logaction
 def setheading(to_angle):
     _state[_h] = to_angle
 
+
 @logaction
 def seth(to_angle):
     _state[_h] = to_angle
+
 
 @logaction
 def home():
     _state = _default_state
 
+
 @logaction
 def circle(radius, extent=None, steps=None):
     pass
 
+
 @logaction
 def dot(size=None, *color):
     pass
+
 
 @logaction
 def undo():
@@ -84,21 +94,40 @@ def undo():
 @logaction
 def towards(x, y=None):
     if len(x) == 2 and type(x) == list:
-         (x0, y0) = x[0:2]
+        p = complex(*x)
     else:
         if type(y) == Number:
-            (x0, y0) = _state[0:2]
+            p = complex(x, y)
         else:
-            x0 = _state[_x]
+            p = (x, _state[_p].imag)
+    d = p - _state[_p]
+    return degrees(d.phase)
+
 
 @logaction
 def xcor():
-    return _state[_x]
+    return _state[_p].real
+
 
 @logaction
 def ycor():
-    return _state[_y]
+    return _state[_p].imag
+
 
 @logaction
 def heading():
     return _state[_h]
+
+
+@logaction
+def distance(x, y=None):
+    if len(x) == 2 and type(x) == list:
+        p = complex(*x)
+    else:
+        if type(y) == Number:
+            p = complex(x, y)
+        else:
+            p = (x, _state[_p].imag)
+    d = p - _state[_p]
+    return abs(d)
+
