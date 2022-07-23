995529b7811a87f9028b4f587513d702b810f9da
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-07-23 13:59:54 +0800

    ongoing turtle cloning

---
+++ Hello_Turtle.py
@@ -0,0 +1,23 @@
+notice = """
+          Hello World Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+
+import Python_BMP.bmpturtle as bt
+import subprocess as proc
+from os import path
+
+def main():
+    
+
+if __name__=="__main__":
+        main()
