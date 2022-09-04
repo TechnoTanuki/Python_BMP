e2e1c76d136a834895e559921ffb55097eeb0233
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-09-04 23:15:57 +0800

    use file oriented func for shorter sample code

--- Hello_Monochrome.py
+++ Hello_Monochrome.py
@@ -13,10 +13,8 @@ Hello Monochromatic Image Filter Demo
 """
 
 from Python_BMP.BITMAPlib import(
-        loadBMP,
-        monofilterto24bitimage as f,
-        getfuncmetastr as meta,
-        saveBMP
+        monochrome2file as f,
+        getfuncmetastr as meta
         )
 
 import subprocess as proc
@@ -27,10 +25,9 @@ def main():
         print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        bmp = loadBMP(f'{rootdir}/assets/tanuki.bmp')
-        f(bmp)
+        origfile = f'{rootdir}/assets/tanuki.bmp'
         file = f'Hello{f.__name__}.bmp' # file name
-        saveBMP(file, bmp) # save file
+        f(origfile, file) # convert to monochrome
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
         ret = proc.call([imgedt, file])
