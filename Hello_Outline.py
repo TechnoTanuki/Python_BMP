77193890c487f38c09f7e8088e32c3f1536b46ce
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-09-04 23:09:04 +0800

    use file oriented func to shorten sample code

--- Hello_Outline.py
+++ Hello_Outline.py
@@ -13,10 +13,8 @@ notice = """
 """
 
 from Python_BMP.BITMAPlib import(
-        loadBMP,
-        outline as f,
-        getfuncmetastr as meta,
-        saveBMP
+        outline2file as f,
+        getfuncmetastr as meta
         )
 
 import subprocess as proc
@@ -27,10 +25,9 @@ def main():
         print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        bmp = loadBMP(f'{rootdir}/assets/tanuki.bmp')
-        f(bmp) # call outline
+        origfile = f'{rootdir}/assets/tanuki.bmp'
         file = f'Hello{f.__name__}.bmp' # file name
-        saveBMP(file, bmp) #dump the bytearray to disk
+        f(origfile, file) # call outline
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
         ret = proc.call([imgedt, file])
