9755e49099656ccb61f0031563d176159d9fd299
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2022-09-06 10:09:47 +0800

    use file oriernted func for less code in sample

--- Hello_Circular_Pixellate.py
+++ Hello_Circular_Pixellate.py
@@ -13,11 +13,8 @@ notice = """
 """
 
 from Python_BMP.BITMAPlib import(
-        loadBMP,
-        centercoord,
-        pixelizenxncircregion as f,
+        pixelizenxncircregion2file as f,
         getfuncmetastr as meta,
-        saveBMP
         )
 
 import subprocess as proc
@@ -28,13 +25,12 @@ def main():
         print(f'{notice}\n{meta(f)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) #get path of running script
-        bmp = loadBMP(f'{rootdir}/assets/earth.bmp')
-        (x, y) = centercoord(bmp) # How to get center of the bitmap
+        origfile = f'{rootdir}/assets/earth.bmp'
+        file = f'Hello{f.__name__}.bmp' #file name
+        x, y = 87, 83 # center of the bitmap
         r = x - 30 # radius set to x - 30
         n = 5 # blursize n = 5x5 pix
-        f(bmp, x, y, r, n) # pixelizenxncircregion(bmp bytarray,x int ,y int ,r int ,n int)
-        file = f'Hello{f.__name__}.bmp' #file name
-        saveBMP(file, bmp) # save the image
+        f(origfile, file, x, y, r, n) # pixelizenxncircregion(bmp bytarray,x int ,y int ,r int ,n int)
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt))
         ret = proc.call([imgedt, file])
