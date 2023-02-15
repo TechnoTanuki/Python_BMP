18b189f5496f60a84c1d0cb5554ddd8c4dbab1fc
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2023-02-15 21:12:22 +0800

    preliminary external raster font reader

---
+++ source_code_gen/loadfont.py
@@ -0,0 +1,45 @@
+from os import path
+def plot8bitpatternastext(
+        bitpattern: list[int],
+        onechar: str,
+        zerochar: str):
+    """Outputs the bits of a list
+        of bytes to console
+
+    Args:
+        bitpattern: list of bytes
+        onechar   : char to display
+                    if bit is 1
+        zerochar : char to display
+                    if bit is 0
+
+    Returns:
+        console output
+    """
+    s = ""
+    for bits in bitpattern:
+        mask = 128
+        while mask > 0:
+            s += onechar if mask & bits > 0 else zerochar
+            mask >>= 1
+        s += '\n'
+    return s
+
+def getcharbmp(filename: str, ch: str):
+    a = ""
+    with open(filename, 'rb') as f:
+        f.seek(1626 + 8 * ord(ch))
+        a = f.read(8)
+    return a
+
+def  main():
+    scriptdir = path.dirname(__file__)
+    filename = scriptdir + "/Bm437_IBM_CGA.FON"
+    ch = ">"
+    a = getcharbmp(filename, ch)
+    print(a)
+    print(plot8bitpatternastext(a,"*"," "))
+
+
+if __name__ == "__main__":
+    main()
