bb0df45fcd88b714cf15e500fb714147300c8619
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2023-02-18 17:31:38 +0800

    full EGA ASCII 8x14 bit font update

--- source_code_gen/loadfont.py
+++ source_code_gen/loadfont.py
@@ -28,33 +28,37 @@ def plot8bitpatternastext(
 def getcharbmp(filename: str, ch: str):
     a = ""
     with open(filename, 'rb') as f:
-        f.seek(1626 + 8 * ord(ch))
-        a = f.read(8)
+        f.seek(1626 + 14 * ord(ch))
+        #f.seek(1626 + 8 * ord(ch))
+        a = f.read(14)
     return a
 
-def getcharset(filename: str):
+def getcharset(filename: str, dataptr: int = 1626, databytes: int = 8):
     a = ""
+    datasz = databytes * 256
     with open(filename, 'rb') as f:
-        f.seek(1626)
-        a = f.read(2048)
+        f.seek(dataptr)
+        a = f.read(datasz)
     return a
 
 
 
 def  main():
     scriptdir = path.dirname(__file__)
-    filename = scriptdir + "/Bm437_IBM_CGA.FON"
+    filename = scriptdir + "/Bm437_IBM_EGA_8x14.FON" #"/Bm437_IBM_CGA.FON"
     ch = ">"
     a = getcharbmp(filename, ch)
     print(a)
     print(plot8bitpatternastext(a,"*"," "))
-    s = getcharset(filename)
-    b = '[8,\n'
-    for i in range(2048):
+    c = 14
+    sz = 256 * c
+    s = getcharset(filename, 1626, c)
+    b = '[{:01d},\n'.format(c)
+    for i in range(sz):
         b += "0x{:02x}".format(s[i])
-        b += ", " if i< 2047 else "] "
-        if (i + 1) % 8 == 0:
-            b += "#0x{:02x}\n".format(i // 8)
+        b += ", " if i < sz else "] "
+        if (i + 1) % c == 0:
+            b += "#0x{:02x}\n".format(i // c)
     print(b)
 
 
