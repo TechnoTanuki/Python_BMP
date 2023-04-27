167b3dfc26932a2e6573ee5f523024fb3b46368e
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2023-04-27 21:00:11 +0800

    trim black margin

--- Hello_Superellipse.py
+++ Hello_Superellipse.py
@@ -29,7 +29,7 @@ def main():
         print(f'{notice}\n{meta(f)}\n{meta(p)}')
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        mx = my = 500 # bitmap size
+        mx = my = 420 # bitmap size
         bmp = newBMP(mx, my, 4) # 16 color
         (x, y) = centercoord(bmp) # How to get center of the bitmap
         file = f'Hello{f.__name__}.bmp' # file name
