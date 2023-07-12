ee837968733cbfa69a992a9cab0a03d60cb8d7fb
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2023-07-12 16:00:35 +0800

    Making a text logo for my other project

---
+++ TetraPlex_Rainbow_Dots.py
@@ -0,0 +1,55 @@
+notice = """
+  Hello World Rainbow Dot Font Demo
+ -----------------------------------
+| Copyright 2022 by Joel C. Alcarez |
+| [joelalcarez1975@gmail.com]       |
+|-----------------------------------|
+|    We make absolutely no warranty |
+| of any kind, expressed or implied |
+|-----------------------------------|
+|   This graphics library outputs   |
+|   to a bitmap file.               |
+ -----------------------------------
+"""
+
+from Python_BMP.BITMAPlib import(
+        newBMP,
+        plotstringasdots as f,
+        getcolorname2RGBdict,
+        font8x8thin,
+        getfuncmetastr as meta,
+        saveBMP
+        )
+
+import subprocess as proc
+from os import path
+
+
+def main():
+        print(f'{notice}\n{meta(f)}')
+        imgedt = 'mspaint'  # replace with another editor if Unix
+        rootdir = path.dirname(__file__) # get path of this script
+        mx, my = 550, 70 # bitmap size
+        bmp = newBMP(mx, my, 24) # RGB bitmap
+        c = getcolorname2RGBdict()
+        f(bmp, 20, 10, # position the text
+          'TetraPlex', # random text
+           7, # uint font size multiplier
+           1, # uint space between pixels
+           0, # uint default space between char
+           (c['brightred'],
+            c['brightorange'],
+            c['brightyellow'],
+            c['brightgreen'],
+            c['cyan'],
+            c['brightblue'],
+            c['brightmagenta']),
+           font8x8thin)
+        file = f'HelloWorldRainbow{f.__name__}.bmp' #file name
+        saveBMP(file, bmp)
+        print('Saved to %s in %s\nAll done close %s to finish' % \
+                (file, rootdir, imgedt)) # tell user we are done
+        ret = proc.call([imgedt, file])
+
+if __name__=="__main__":
+        main()
