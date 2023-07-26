dbcc6fec09e8868ef27c599be737db7e08efae95
TechnoTanuki <97581084+TechnoTanuki@users.noreply.github.com>
2023-07-26 22:16:40 +0800

    minor edit

--- samples/fonts/TetraPlex_logo.py
+++ samples/fonts/TetraPlex_logo.py
@@ -1,5 +1,5 @@
 notice = """
-  Hello World Rainbow Dot Font Demo
+           Text Logo Demo
  -----------------------------------
 | Copyright 2022 by Joel C. Alcarez |
 | [joelalcarez1975@gmail.com]       |
@@ -27,20 +27,21 @@ from os import path
 
 def main():
         print(f'{notice}\n{meta(f)}')
+        logostr = 'TetraPlex' #change text as needed
         imgedt = 'mspaint'  # replace with another editor if Unix
         rootdir = path.dirname(__file__) # get path of this script
-        mx, my = 550, 70 # bitmap size
+        mx, my = 540, 70 # bitmap size
         bmp = newBMP(mx, my, 24) # RGB bitmap
         c = getcolorname2RGBdict()
-        f(bmp, 20, 10, # position the text
-          'TetraPlex', # random text
+        f(bmp, 0, 10, # position the text
+           logostr, # random text
            7, # uint font size multiplier
            0, # uint space between pixels
            0, # uint default space between char
            (c['yellow'],
             c['brightyellow']),
            font8x8sanserif)
-        file = f'HelloWorldRainbow{f.__name__}.bmp' #file name
+        file = f'{logostr}.bmp' #file name
         saveBMP(file, bmp)
         print('Saved to %s in %s\nAll done close %s to finish' % \
                 (file, rootdir, imgedt)) # tell user we are done
